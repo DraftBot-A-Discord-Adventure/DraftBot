d8df12216dcc69220c015619065d0dfdb79ba41a
BastLast <bastien.last87@gmail.com>
2018-12-22 15:42:21 +0100

    Ajout de la commande invite

--- modules/CommandTable.js
+++ modules/CommandTable.js
@@ -1,11 +1,13 @@
 const Help = require('./commands/Help');
 const Ping = require('./commands/Ping');
+const Invite = require('./commands/Invite');
 
 const CommandTable = new Map(
     [
         ["help", Help.HelpCommand],
         ["aide", Help.HelpCommand],
-        ["ping", Ping.PingCommand]
+        ["ping", Ping.PingCommand],
+        ["invite", Invite.InviteCommand]
     ]
 );
 
---
+++ modules/commands/Invite.js
@@ -0,0 +1,14 @@
+const Text = require('../text/Francais');
+
+/**
+ * Display the ping of the bot and allow user to check if the bot is online
+ * @param message - The message that caused the function to be called. Used to retrieve the author of the message
+ */
+const inviteCommand = function (message) {
+
+    let inviteMessage = Text.commands.invite.main;
+    message.channel.send(inviteMessage);
+
+};
+
+module.exports.InviteCommand = inviteCommand;
