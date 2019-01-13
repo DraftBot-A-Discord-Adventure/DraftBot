044dc3f25a4a37d5bd68d09a5899fbcda4deba1c
BastLast <bastien.last87@gmail.com>
2019-01-13 17:54:12 +0100

    Ajout des getters et setters pour la classe item

--- modules/classes/Item.js
+++ modules/classes/Item.js
@@ -14,4 +14,42 @@ class Item {
         this.power = power;
     }
 
+
+
+
+    /**
+     * Returns the name of the item
+     * @returns {String} - The name
+     */
+    getName() {
+        return this.name;
+    }
+
+
+    /**
+     * Returns the rareness of the item
+     * @returns {number} - The rareness
+     */
+    getRareness() {
+        return this.rareness;
+    }
+
+
+    /**
+     * Returns the power of the item
+     * @returns {number} - The power 
+     */
+    getPower() {
+        return this.power;
+    }
+
+    /**
+     * Save a weapon in the weapon slot of the inventory
+     * @param {number} weapon - The weapon that has to be saved
+     */
+    upgrade(powerToAdd) {
+        if (Tools.isAPositiveNumber && this.power + powerToAdd < DefaultValues.item.maxPower)
+            this.power = this.power + powerToAdd;
+    }
+
 }
