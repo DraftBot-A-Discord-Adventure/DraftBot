3204c3a742d6f439101ff0f06e8a96144fc104e3
Bast <bastien.last87@gmail.com>
2019-04-02 14:07:55 +0200

    Finalisation de la classe object

--- modules/classes/Object.js
+++ modules/classes/Object.js
@@ -1,7 +1,5 @@
 const Item = require('./Item');
-const sql = require("sqlite");
-
-sql.open("./modules/data/database.sqlite");
+const ItemNames = require('../utils/items/Francais');
 
 /**
  * Represent an object
@@ -13,7 +11,7 @@ class Object extends Item {
         super(id, rareness, power);
         this.natureEffect = natureEffect
     }
-    
+
 
     /**
      * a number that match with an id of the effect applied by the object
@@ -21,6 +19,30 @@ class Object extends Item {
      */
     getNatureEffect() {
         return this.natureEffect;
+    }
+
+    /**
+   * The amount of power of the potion
+   * @returns {number} - The power of the potion.
+   */
+    getPower() {
+        return this.power;
+    }
+
+    /**
+     * Return the name of the object
+     * @returns {String} - The name
+     */
+    getName() {
+        return ItemNames.object[this.id];
+    }
+
+    /**
+     * return the emoji that correspond to the object
+     * @returns {String} - The emoji
+     */
+    getEmoji() {
+        return ItemNames.object[this.id].split(" ")[0];
     }
 }
 
