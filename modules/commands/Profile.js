18d11fec483b49ff0e3de795fd5622bff95abd26
Bast <bastien.last87@gmail.com>
2019-04-11 16:44:21 +0300

    Correction d'un bug d'affichage sur le profile

--- modules/commands/Profile.js
+++ modules/commands/Profile.js
@@ -22,15 +22,20 @@ const profileCommand = async function (message) {
  */
 const generateProfileMessage = function (message, player, numberOfPlayer) {
     let playerManager = new PlayerManager();
-    let profileMessage = player.getEffect() + Text.commands.profile.main + message.author.username +
-        Text.commands.profile.level + player.getLevel() + 
-        Text.commands.profile.xp + player.getExperience() + Text.commands.profile.separator + player.getExperienceToLevelUp() +
-        Text.commands.profile.health + player.getHealth() + Text.commands.profile.separator + player.getMaxHealth() +
-        Text.commands.profile.statsAttack + player.getAttack() + Text.commands.profile.statsDefense + player.getDefense() + Text.commands.profile.statsSpeed + player.getSpeed() +
-        Text.commands.profile.rank + player.getRank() + Text.commands.profile.separator + numberOfPlayer +
-        Text.commands.profile.money + player.getMoney() +
-        Text.commands.profile.score + player.getScore() + 
-        playerManager.displayTimeLeft(player, message);
+    let profileMessage;
+    if (player.getEffect() == ":baby:") {
+        profileMessage = player.getEffect() + Text.commands.profile.main + message.author.username + Text.commands.profile.notAPlayer;
+    } else {
+        profileMessage = player.getEffect() + Text.commands.profile.main + message.author.username +
+            Text.commands.profile.level + player.getLevel() +
+            Text.commands.profile.xp + player.getExperience() + Text.commands.profile.separator + player.getExperienceToLevelUp() +
+            Text.commands.profile.health + player.getHealth() + Text.commands.profile.separator + player.getMaxHealth() +
+            Text.commands.profile.statsAttack + player.getAttack() + Text.commands.profile.statsDefense + player.getDefense() + Text.commands.profile.statsSpeed + player.getSpeed() +
+            Text.commands.profile.rank + player.getRank() + Text.commands.profile.separator + numberOfPlayer +
+            Text.commands.profile.money + player.getMoney() +
+            Text.commands.profile.score + player.getScore() +
+            playerManager.displayTimeLeft(player, message);
+    }
     return profileMessage;
 };
 
