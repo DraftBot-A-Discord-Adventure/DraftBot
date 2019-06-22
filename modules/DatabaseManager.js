27673982089e1b539660e7d97f614a91b48264a8
Bast <bastien.last87@gmail.com>
2019-06-22 17:17:24 +0200

    Reset des badges - début

--- modules/DatabaseManager.js
+++ modules/DatabaseManager.js
@@ -22,6 +22,7 @@ class DatabaseManager {
      */
     setEverybodyAsUnOccupied() {
         console.log("Updating everybody ...");
+        sql.run(`UPDATE player SET badges = ""`).catch(console.error);
         sql.run(`UPDATE entity SET effect = ":smiley:" WHERE effect = ":clock10:"`).catch(console.error);
         console.log("everybody updated !");
     }
