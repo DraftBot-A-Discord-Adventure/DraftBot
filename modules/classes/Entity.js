6223aa135c9732354c5c247a8eff247f796ce476
Dogeek <Dogeek@users.noreply.github.com>
2020-02-18 22:33:22 +0100

    remove chargeText from Entity

--- modules/classes/Entity.js
+++ modules/classes/Entity.js
@@ -4,20 +4,6 @@ const Config = require('../utils/Config');
 let Text;
 
 /**
- * Allow to charge the correct text file
- * @param message - The message that caused the function to be called. Used to retrieve the author of the message.
- */
-const chargeText = async function (message) {
-    let serverManager = new ServerManager();
-    let server = await serverManager.getServer(message);
-    if (message.channel.id == Config.ENGLISH_CHANNEL_ID) {
-        server.language = "en";
-    }
-    let address = '../text/' + server.language;
-    return require(address);
-}
-
-/**
  * Base class that shouldn't be instantiated. Instead, Entities are meant to extend this class.
  * Entities are things like Enemies, entitys...
  */
