bde7b37738c974832896214ff062860e447dd1e9
Max <maxence1604.minecraft@gmail.com>
2020-07-14 11:25:45 +0200

    At the speed of light

--- src/core/DraftBot.js
+++ src/core/DraftBot.js
@@ -2,109 +2,109 @@
  * @class
  */
 class DraftBot {
-  /**
-   * @return {Promise<DraftBot>}
-   */
-  static async init() {
-    await (require('core/JsonReader')).init({
-      folders: ['ressources/text/commands', 'ressources/text/models'],
-      files: [
-        'config/app.json',
-        'draftbot/package.json',
-        'ressources/text/error.json',
-        'ressources/text/bot.json',
-        'ressources/text/values.json',
-        'ressources/text/items.json',
-      ],
-    });
-    await (require('core/Database')).init();
-    await (require('core/Command')).init();
+    /**
+     * @return {Promise<DraftBot>}
+     */
+    static async init() {
+        await (require('core/JsonReader')).init({
+            folders: ['ressources/text/commands', 'ressources/text/models'],
+            files: [
+                'config/app.json',
+                'draftbot/package.json',
+                'ressources/text/error.json',
+                'ressources/text/bot.json',
+                'ressources/text/values.json',
+                'ressources/text/items.json',
+            ],
+        });
+        await (require('core/Database')).init();
+        await (require('core/Command')).init();
 
-    // TODO 2.1
-    // draftbot.checkEasterEggsFile();
+        // TODO 2.1
+        // draftbot.checkEasterEggsFile();
 
-    DraftBot.programTopWeekTimeout();
+        DraftBot.programTopWeekTimeout();
 
-    return this;
-  }
-
-  /**
-   * Programs a timeout for the next sunday midnight
-   */
-  static programTopWeekTimeout() {
-    let millisTill = getNextSundayMidnight() - new Date();
-    if (millisTill === 0) { //Case at 0:00:00
-      setTimeout(DraftBot.programTopWeekTimeout, 1);
-      return;
+        return this;
     }
-    setTimeout(DraftBot.topWeekEnd, millisTill);
-  }
 
-  /**
-   * Handle the top week reward and reset
-   * @return {Promise<void>}
-   */
-  static async topWeekEnd() {
-    let winner = await Entities.findOne({
-      defaults: {
-        Player: {
-          Inventory: {}
+    /**
+     * Programs a timeout for the next sunday midnight
+     */
+    static programTopWeekTimeout() {
+        let millisTill = getNextSundayMidnight() - new Date();
+        if (millisTill === 0) { //Case at 0:00:00
+            setTimeout(DraftBot.programTopWeekTimeout, 1);
+            return;
         }
-      },
-      include: [{
-        model: Players,
-        as: 'Player',
-        where: {
-          weeklyScore: {
-            [(require('sequelize/lib/operators')).gt]: 100,
-          },
-        },
-      }],
-      order: [
-        [{model: Players, as: 'Player'}, 'weeklyScore', 'DESC'],
-        [{model: Players, as: 'Player'}, 'level', 'DESC']
-      ],
-      limit: 1
-    });
-    if (winner !== null) {
-      (await client.channels.fetch(JsonReader.app.FRENCH_ANNOUNCEMENT_CHANNEL_ID)).send(format(JsonReader.bot.getTranslation("fr").topWeekAnnouncement, {mention: winner.getMention()}));
-      (await client.channels.fetch(JsonReader.app.ENGLISH_ANNOUNCEMENT_CHANNEL_ID)).send(format(JsonReader.bot.getTranslation("en").topWeekAnnouncement, {mention: winner.getMention()}));
-      winner.Player.addBadge("🎗️");
-      winner.Player.save();
+        setTimeout(DraftBot.topWeekEnd, millisTill);
     }
-    Players.update({ weeklyScore: 0}, { where: {}});
-    console.log("# WARNING # Weekly leaderboard has been reset !");
-    DraftBot.programTopWeekTimeout();
-  }
 
-  /**
-   * TODO 2.1
-   * Checks if the easter eggs file exists and copy the default one if not
-   */
-  // checkEasterEggsFile() {
-  //
-  //   let EasterEggs = require("./src/utils/eastereggs/EasterEggs");
-  //   EasterEggs.init();
-  //
-  //   const fs = require('fs');
-  //   if (!fs.existsSync('./src/utils/eastereggs/EasterEggs.js')) {
-  //     fs.copyFileSync('./src/utils/eastereggs/EasterEggs.js.default',
-  //         './src/utils/eastereggs/EasterEggs.js', (err) => {
-  //           if (err) throw err;
-  //         });
-  //     console.warn(
-  //         './src/utils/eastereggs/EasterEggs.js not found. ./src/utils/eastereggs/EasterEggs.js.default copied to be used.');
-  //     console.warn(
-  //         'Ignore this message if you don\'t have the key to decrypt the file.');
-  //   }
-  // }
+    /**
+     * Handle the top week reward and reset
+     * @return {Promise<void>}
+     */
+    static async topWeekEnd() {
+        let winner = await Entities.findOne({
+            defaults: {
+                Player: {
+                    Inventory: {}
+                }
+            },
+            include: [{
+                model: Players,
+                as: 'Player',
+                where: {
+                    weeklyScore: {
+                        [(require('sequelize/lib/operators')).gt]: 100,
+                    },
+                },
+            }],
+            order: [
+                [{ model: Players, as: 'Player' }, 'weeklyScore', 'DESC'],
+                [{ model: Players, as: 'Player' }, 'level', 'DESC']
+            ],
+            limit: 1
+        });
+        if (winner !== null) {
+            (await client.channels.fetch(JsonReader.app.FRENCH_ANNOUNCEMENT_CHANNEL_ID)).send(format(JsonReader.bot.getTranslation("fr").topWeekAnnouncement, { mention: winner.getMention() }));
+            (await client.channels.fetch(JsonReader.app.ENGLISH_ANNOUNCEMENT_CHANNEL_ID)).send(format(JsonReader.bot.getTranslation("en").topWeekAnnouncement, { mention: winner.getMention() }));
+            winner.Player.addBadge("🎗️");
+            winner.Player.save();
+        }
+        Players.update({ weeklyScore: 0 }, { where: {} });
+        console.log("# WARNING # Weekly leaderboard has been reset !");
+        DraftBot.programTopWeekTimeout();
+    }
+
+    /**
+     * TODO 2.1
+     * Checks if the easter eggs file exists and copy the default one if not
+     */
+    // checkEasterEggsFile() {
+    //
+    //   let EasterEggs = require("./src/utils/eastereggs/EasterEggs");
+    //   EasterEggs.init();
+    //
+    //   const fs = require('fs');
+    //   if (!fs.existsSync('./src/utils/eastereggs/EasterEggs.js')) {
+    //     fs.copyFileSync('./src/utils/eastereggs/EasterEggs.js.default',
+    //         './src/utils/eastereggs/EasterEggs.js', (err) => {
+    //           if (err) throw err;
+    //         });
+    //     console.warn(
+    //         './src/utils/eastereggs/EasterEggs.js not found. ./src/utils/eastereggs/EasterEggs.js.default copied to be used.');
+    //     console.warn(
+    //         'Ignore this message if you don\'t have the key to decrypt the file.');
+    //   }
+    // }
 }
 
 /**
  * @type {{init: (function(): DraftBot)}}
  */
 module.exports = {
-  init: DraftBot.init,
+    init: DraftBot.init,
 };
 /**
  * @type {module:"discord.js"}
@@ -113,4 +113,4 @@ global.discord = (require('discord.js'));
 /**
  * @type {module:"discord.js".Client}
  */
-global.client = new (require('discord.js')).Client({restTimeOffset: 100});
+global.client = new(require('discord.js')).Client({ restTimeOffset: 0 });
