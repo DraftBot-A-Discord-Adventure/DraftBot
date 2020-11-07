4f5fc2baf1bf03ad7f6b5b7cf4cf29975f6a0990
BastLast <bastien.last87@gmail.com>
2020-11-07 18:53:08 +0100

    Bugfix class command #243

--- src/commands/player/ClassCommand.js
+++ src/commands/player/ClassCommand.js
@@ -42,14 +42,6 @@ async function ClassCommand(language, message, args) {
 
     const collector = classMessage.createReactionCollector(filterConfirm, { time: 120000, max: 1 });
 
-    //Adding reactions
-    let classEmojis = new Map();
-    for (let k = 0; k < allClasses.length; k++) {
-        await classMessage.react(allClasses[k].emoji);
-        classEmojis.set(allClasses[k].emoji, k);
-    }
-    classMessage.react(MENU_REACTION.DENY)
-
     //Fetch the choice from the user
     collector.on("end", async (reaction) => {
         if (!reaction.first()) { //the user is afk
@@ -62,19 +54,17 @@ async function ClassCommand(language, message, args) {
             return;
         }
 
-        if (canBuy(CLASS.PRICE, entity.Player)) {
-            const selectedClass = await Classes.getByEmojy(reaction.first().emoji.name);
-            confirmPurchase(message, language, selectedClass, entity);
-        } else {
-            sendErrorMessage(message.author, message.channel, language, format(
-                JsonReader.commands.class.getTranslation(language).error.cannotBuy,
-                {
-                    missingMoney: CLASS.PRICE - entity.Player.money,
-                }
-            ));
-            removeBlockedPlayer(entity.discordUser_id);
-        }
+        const selectedClass = await Classes.getByEmojy(reaction.first().emoji.name);
+        confirmPurchase(message, language, selectedClass, entity);
     });
+
+    //Adding reactions
+    let classEmojis = new Map();
+    for (let k = 0; k < allClasses.length; k++) {
+        await classMessage.react(allClasses[k].emoji);
+        classEmojis.set(allClasses[k].emoji, k);
+    }
+    classMessage.react(MENU_REACTION.DENY)
 }
 
 /**
@@ -97,7 +87,7 @@ async function confirmPurchase(message, language, selectedClass, entity) {
             "\n\u200b\n" +
             format(JsonReader.commands.class.getTranslation(language).display, {
                 name: selectedClass.toString(language, entity.Player.level),
-                price: CLASS.PRICE,
+                price: selectedClass.price,
                 description: selectedClass.getDescription(language)
             })
         );
@@ -117,6 +107,14 @@ async function confirmPurchase(message, language, selectedClass, entity) {
         removeBlockedPlayer(entity.discordUser_id);
         if (reaction.first()) {
             if (reaction.first().emoji.name === MENU_REACTION.ACCEPT) {
+                if (!canBuy(selectedClass.price, entity.Player)) {
+                    return sendErrorMessage(message.author, message.channel, language, format(
+                        JsonReader.commands.class.getTranslation(language).error.cannotBuy,
+                        {
+                            missingMoney: selectedClass.price - entity.Player.money,
+                        }
+                    ));
+                }
                 if (selectedClass.id === playerClass.id) {
                     return sendErrorMessage(message.author, message.channel, language, JsonReader.commands.class.getTranslation(language).error.sameClass);
                 }
@@ -126,7 +124,7 @@ async function confirmPurchase(message, language, selectedClass, entity) {
                 await entity.setHealth(Math.round(
                     (entity.health / await playerClass.getMaxHealthValue(entity.Player.level))
                     * await newClass.getMaxHealthValue(entity.Player.level)))
-                entity.Player.addMoney(-CLASS.PRICE);
+                entity.Player.addMoney(-selectedClass.price);
                 await Promise.all([
                     entity.save(),
                     entity.Player.save()
