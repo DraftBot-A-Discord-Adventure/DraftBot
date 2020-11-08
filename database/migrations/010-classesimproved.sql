187e6e1522e6c975055675399b9d957234a6e02d
Bast <bastien.last87@gmail.com>
2020-11-08 22:35:04 +0100

    hotfixMigration bis

--- database/migrations/010-classesimproved.sql
+++ database/migrations/010-classesimproved.sql
@@ -6,8 +6,8 @@ ALTER TABLE classes ADD classgroup INTEGER;
 update players set badges = players.badges||"-🔖" where players.class != 0 and players.badges is not null;
 update players set badges = "🔖" where badges is null and players.class != 0;
 
-UPDATE players SET players.class = 5 WHERE players.class = 2;
-UPDATE players SET players.class = 9 WHERE players.class = 1;
+UPDATE players SET class = 5 WHERE players.class = 2;
+UPDATE players SET class = 9 WHERE players.class = 1;
 
 -- Down
 
