22bbf8bbdeefb69b1ee7aa3da376306326f5167e
BastLast <bastien.last87@gmail.com>
2020-11-09 16:41:25 +0100

    migration fix

--- database/migrations/010-classesimproved.sql
+++ database/migrations/010-classesimproved.sql
@@ -3,9 +3,6 @@
 ALTER TABLE classes ADD price INTEGER;
 ALTER TABLE classes ADD classgroup INTEGER;
 
-update players set badges = players.badges||"-🔖" where players.class != 0 and players.badges is not null;
-update players set badges = "🔖" where badges is null and players.class != 0;
-
 UPDATE players SET class = 5 WHERE players.class = 2;
 UPDATE players SET class = 9 WHERE players.class = 1;
 
