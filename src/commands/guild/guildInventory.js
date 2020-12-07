23cbf86fd8b0fb33d4f33b5a0ccad28f2518b11c
eagles666 <eagles.paladin@gmail.com>
2020-12-07 19:46:07 +0100

    edit variables #262

--- src/commands/guild/guildInventory.js
+++ src/commands/guild/guildInventory.js
@@ -5,134 +5,148 @@
  * @param {String[]} args=[] - Additional arguments sent with the command
  */
 const GuildInventoryCommand = async (language, message, args) => {
-    [entity] = await Entities.getOrRegister(message.author.id);
+  [entity] = await Entities.getOrRegister(message.author.id);
 
-    if (
-        (await canPerformCommand(
-            message,
-            language,
-            PERMISSION.ROLE.ALL,
-            [EFFECT.BABY, EFFECT.DEAD],
-            entity
-        )) !== true
-    ) {
-        return;
-    }
+  if (
+    (await canPerformCommand(
+      message,
+      language,
+      PERMISSION.ROLE.ALL,
+      [EFFECT.BABY, EFFECT.DEAD],
+      entity
+    )) !== true
+  ) {
+    return;
+  }
 
-    // search for a user's guild
-    let guild;
-    try {
-        guild = await Guilds.getById(entity.Player.guild_id);
-    } catch (error) {
-        guild = null;
-    }
+  // search for a user's guild
+  let guild;
+  try {
+    guild = await Guilds.getById(entity.Player.guild_id);
+  } catch (error) {
+    guild = null;
+  }
 
-    if (guild == null) {
-        // not in a guild
-        return sendErrorMessage(
-            message.author,
-            message.channel,
-            language,
-            JsonReader.commands.guildAdd.getTranslation(language).notInAguild
-        );
-    }
-
-    let inventoryEmbed = new discord.MessageEmbed();
-
-    inventoryEmbed.setTitle(
-        format(
-            JsonReader.commands.guildInventory.getTranslation(language)
-                .embedTitle,
-            {
-                guild: guild.name,
-            }
-        )
+  if (guild == null) {
+    // not in a guild
+    return sendErrorMessage(
+      message.author,
+      message.channel,
+      language,
+      JsonReader.commands.guildAdd.getTranslation(language).notInAguild
     );
+  }
 
-    inventoryEmbed.setThumbnail(JsonReader.commands.guild.icon);
+  let inventoryEmbed = new discord.MessageEmbed();
 
-    inventoryEmbed.addField(
-        "**Nourriture** : ",
-        "La nourriture est utile pour nourrir tous vos familiers. Si vous en avez plus, achetez en avec la commande `guildshop`"
-    );
-    inventoryEmbed.addField(
-        format(
-            JsonReader.commands.guildInventory.getTranslation(language)
-                .foodTitle,
-            {
-                foodType:
-                    language === "fr"
-                        ? FOOD.COMMON_FOOD.TRANSLATIONS.fr
-                        : FOOD.COMMON_FOOD.TRANSLATIONS.en,
-            }
-        ),
-        format(
-            JsonReader.commands.guildInventory.getTranslation(language)
-                .foodField,
-            {
-                guildFood: guild.commonPetFood,
-                maxFood: GUILD.MAX_COMMON_PETFOOD,
-                emote: FOOD.COMMON_FOOD.EMOTE,
-                addLovePoints: FOOD.COMMON_FOOD.EFFECT,
-            }
-        ),
-        true
-    );
-    inventoryEmbed.addField(
-        format(
-            JsonReader.commands.guildInventory.getTranslation(language)
-                .foodTitle,
-            {
-                foodType:
-                    language === "fr"
-                        ? FOOD.RARE_FOOD.TRANSLATIONS.fr
-                        : FOOD.RARE_FOOD.TRANSLATIONS.en,
-            }
-        ),
-        format(
-            JsonReader.commands.guildInventory.getTranslation(language)
-                .foodField,
-            {
-                guildFood: guild.commonPetFood,
-                maxFood: GUILD.MAX_RARE_PETFOOD,
-                emote: FOOD.RARE_FOOD.EMOTE,
-                addLovePoints: FOOD.RARE_FOOD.EFFECT,
-            }
-        ),
-        true
-    );
-    inventoryEmbed.addField(
-        format(
-            JsonReader.commands.guildInventory.getTranslation(language)
-                .foodTitle,
-            {
-                foodType:
-                    language === "fr"
-                        ? FOOD.UNIQUE_FOOD.TRANSLATIONS.fr
-                        : FOOD.UNIQUE_FOOD.TRANSLATIONS.en,
-            }
-        ),
-        format(
-            JsonReader.commands.guildInventory.getTranslation(language)
-                .foodField,
-            {
-                guildFood: guild.commonPetFood,
-                maxFood: GUILD.MAX_UNIQUE_PETFOOD,
-                emote: FOOD.UNIQUE_FOOD.EMOTE,
-                addLovePoints: FOOD.UNIQUE_FOOD.EFFECT,
-            }
-        ),
-        true
-    );
-    await message.channel.send(inventoryEmbed);
+  inventoryEmbed.setTitle(
+    format(
+      JsonReader.commands.guildInventory.getTranslation(language).embedTitle,
+      {
+        guild: guild.name,
+      }
+    )
+  );
+
+  inventoryEmbed.setThumbnail(JsonReader.commands.guild.icon);
+
+  inventoryEmbed.addField(
+    "**Nourriture** : ",
+    "La nourriture est utile pour nourrir tous vos familiers. Si vous en avez plus, achetez en avec la commande `guildshop`"
+  );
+  inventoryEmbed.addField(
+    format(
+      JsonReader.commands.guildInventory.getTranslation(language).foodTitle,
+      {
+        foodType:
+          language === "fr"
+            ? FOOD.COMMON_FOOD.TRANSLATIONS.fr
+            : FOOD.COMMON_FOOD.TRANSLATIONS.en,
+      }
+    ),
+    format(
+      JsonReader.commands.guildInventory.getTranslation(language).foodField,
+      {
+        guildFood: guild.commonFood,
+        maxFood: GUILD.MAX_COMMON_PETFOOD,
+        emote: FOOD.COMMON_FOOD.EMOTE,
+        addLovePoints: FOOD.COMMON_FOOD.EFFECT,
+      }
+    ),
+    true
+  );
+  inventoryEmbed.addField(
+    format(
+      JsonReader.commands.guildInventory.getTranslation(language).foodTitle,
+      {
+        foodType:
+          language === "fr"
+            ? FOOD.RARE_FOOD.TRANSLATIONS.fr
+            : FOOD.RARE_FOOD.TRANSLATIONS.en,
+      }
+    ),
+    format(
+      JsonReader.commands.guildInventory.getTranslation(language).foodField,
+      {
+        guildFood: guild.rareFood,
+        maxFood: GUILD.MAX_RARE_PETFOOD,
+        emote: FOOD.RARE_FOOD.EMOTE,
+        addLovePoints: FOOD.RARE_FOOD.EFFECT,
+      }
+    ),
+    true
+  );
+  inventoryEmbed.addField(
+    format(
+      JsonReader.commands.guildInventory.getTranslation(language).foodTitle,
+      {
+        foodType:
+          language === "fr"
+            ? FOOD.UNIQUE_FOOD.TRANSLATIONS.fr
+            : FOOD.UNIQUE_FOOD.TRANSLATIONS.en,
+      }
+    ),
+    format(
+      JsonReader.commands.guildInventory.getTranslation(language).foodField,
+      {
+        guildFood: guild.uniqueFood,
+        maxFood: GUILD.MAX_UNIQUE_PETFOOD,
+        emote: FOOD.UNIQUE_FOOD.EMOTE,
+        addLovePoints: FOOD.UNIQUE_FOOD.EFFECT,
+      }
+    ),
+    true
+  );
+  inventoryEmbed.addField(
+    format(
+      JsonReader.commands.guildInventory.getTranslation(language).foodTitle,
+      {
+        foodType:
+          language === "fr"
+            ? FOOD.GROW_FOOD.TRANSLATIONS.fr
+            : FOOD.GROW_FOOD.TRANSLATIONS.en,
+      }
+    ),
+    format(
+      JsonReader.commands.guildInventory.getTranslation(language).growFoodField,
+      {
+        guildFood: guild.growFood,
+        maxFood: GUILD.MAX_GROW_PETFOOD,
+        emote: FOOD.GROW_FOOD.EMOTE,
+        addGrowPoints: FOOD.GROW_FOOD.EFFECT,
+      }
+    ),
+    true
+  );
+  await message.channel.send(inventoryEmbed);
 };
 
 module.exports = {
-    commands: [
-        {
-            name: "guildinventory",
-            func: GuildInventoryCommand,
-            aliases: ["guildinventory", "ginv"],
-        },
-    ],
+  commands: [
+    {
+      name: "guildinventory",
+      func: GuildInventoryCommand,
+      aliases: ["guildinventory", "ginv"],
+    },
+  ],
 };
