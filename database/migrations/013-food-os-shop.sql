ff238017c9650bd13c6e7fc234600ad358e08f81
eagles666 <eagles.paladin@gmail.com>
2020-12-25 17:21:10 +0100

    remove growFood in migration #270

--- database/migrations/013-food-os-shop.sql
+++ database/migrations/013-food-os-shop.sql
@@ -3,7 +3,6 @@
 ALTER TABLE guilds ADD commonFood INTEGER;
 ALTER TABLE guilds ADD rareFood INTEGER;
 ALTER TABLE guilds ADD uniqueFood INTEGER;
-ALTER TABLE guilds ADD growFood INTEGER;
 
 ALTER TABLE possibilities ADD oneshot Boolean;
 UPDATE possibilities SET oneshot = false;
