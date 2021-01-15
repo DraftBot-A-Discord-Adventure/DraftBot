bfca1624f5f6919f5edea4912f914cdc5acc93d2
Quentin <quenquentin@gmx.fr>
2021-01-15 18:05:03 +0100

    Ajout de la commande vote + new version dans le package

---
+++ src/commands/player/VoteCommand.js
@@ -0,0 +1,23 @@
+/**
+ * Displays the changelog of the bot
+ * @param {("fr"|"en")} language - Language to use in the response
+ * @param {module:"discord.js".Message} message - Message from the discord server
+ * @param {String[]} args=[] - Additional arguments sent with the command
+ */
+async function VoteCommand(language, message, args) {
+    const voteEmbed = new discord.MessageEmbed()
+        .setDescription(JsonReader.commands.vote.getTranslation(language).text)
+        .setTitle(JsonReader.commands.vote.getTranslation(language).title)
+        .setColor(JsonReader.bot.embed.default);
+    message.channel.send(voteEmbed);
+}
+
+module.exports = {
+    commands: [
+        {
+            name: 'vote',
+            func: VoteCommand,
+            aliases: ['votes']
+        }
+    ]
+};
