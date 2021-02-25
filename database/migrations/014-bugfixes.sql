6f3b74d3df03f6b588f04e716deaf11d49099c82
BastLast <bastien.last87@gmail.com>
2021-02-25 01:03:03 +0100

    debug guildes affectées connues #310

--- database/migrations/014-bugfixes.sql
+++ database/migrations/014-bugfixes.sql
@@ -5,4 +5,8 @@ UPDATE guilds SET herbivorousFood = 0 WHERE herbivorousFood is null;
 UPDATE guilds SET carnivorousFood = 0 WHERE carnivorousFood is null;
 UPDATE guilds SET ultimateFood = 0 WHERE ultimateFood is null;
 
+UPDATE guilds SET chief_id = 6 WHERE id = 2
+UPDATE guilds SET chief_id = 7446 WHERE id = 173
+UPDATE guilds SET chief_id = 7435 WHERE id = 163
+
 -- Down 
