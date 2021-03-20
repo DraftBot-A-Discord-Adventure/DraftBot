a11e162c260d366f0f4183d48ad8ea49dbebffa8
eagles <eagles.paladin@gmail.com>
2021-03-20 18:43:26 +0100

    new schema of daily #329

--- src/commands/player/DailyCommand.js
+++ src/commands/player/DailyCommand.js
@@ -5,78 +5,109 @@
  */
 const DailyCommand = async function (language, message) {
 	const [entity] = await Entities.getOrRegister(message.author.id);
-	if ((await canPerformCommand(message, language, PERMISSION.ROLE.ALL,
-		[EFFECT.BABY, EFFECT.DEAD], entity)) !== true) {
+	if ((await canPerformCommand(message, language, PERMISSION.ROLE.ALL, [EFFECT.BABY, EFFECT.DEAD], entity)) !== true) {
 		return;
 	}
 
-	const now = new Date();
 	const activeObject = await entity.Player.Inventory.getActiveObject();
 
-	const lastDailyDay = new Date(await entity.Player.Inventory.lastDailyAt);
-
 	const embed = new discord.MessageEmbed();
-	if (lastDailyDay.getDate() === now.getDate() &&
-		lastDailyDay.getMonth() === now.getMonth() &&
-		lastDailyDay.getFullYear() === now.getFullYear()) {
-		return sendErrorMessage(message.author, message.channel, language, JsonReader.commands.daily.getTranslation(language).alreadyClaimedError);
-	}
+
+	let time = millisecondsToHours(message.createdAt.getTime() - entity.Player.Inventory.lastDailyAt.valueOf());
 
 	if (activeObject.nature === NATURE.NONE) {
 		if (activeObject.id !== JsonReader.models.inventories.object_id) {
 			// there is a object that do nothing in the inventory
-			sendErrorMessage(message.author, message.channel, language, JsonReader.commands.daily.getTranslation(language).objectDoNothingError);
+			sendErrorMessage(
+				message.author,
+				message.channel,
+				language,
+				JsonReader.commands.daily.getTranslation(language).objectDoNothingError
+			);
 		} else {
 			// there is no object in the inventory
-			sendErrorMessage(message.author, message.channel, language, JsonReader.commands.daily.getTranslation(language).noActiveObjectdescription);
+			sendErrorMessage(
+				message.author,
+				message.channel,
+				language,
+				JsonReader.commands.daily.getTranslation(language).noActiveObjectdescription
+			);
 		}
 		return;
 	}
+
+	if (time < JsonReader.commands.daily.timeBetweenDailys) {
+		return sendErrorMessage(
+			message.author,
+			message.channel,
+			language,
+			format(JsonReader.commands.daily.getTranslation(language).coolDown, {
+				coolDownTime: JsonReader.commands.daily.timeBetweenDailys,
+				time: minutesToString(
+					millisecondsToMinutes(
+						JsonReader.commands.daily.timeBetweenDailys * 3600000 - message.createdAt.getTime() + entity.Player.Inventory.lastDailyAt.valueOf()
+					)
+				),
+			})
+		);
+	}
+
 	if (activeObject.nature === NATURE.HEALTH) {
-		embed.setColor(JsonReader.bot.embed.default)
-			.setAuthor(format(
-				JsonReader.commands.daily.getTranslation(language).dailySuccess,
-				{pseudo: message.author.username}),
-				message.author.displayAvatarURL())
-			.setDescription(format(
-				JsonReader.commands.daily.getTranslation(language).healthDaily,
-				{value: activeObject.power}));
+		embed
+			.setColor(JsonReader.bot.embed.default)
+			.setAuthor(
+				format(JsonReader.commands.daily.getTranslation(language).dailySuccess, { pseudo: message.author.username }),
+				message.author.displayAvatarURL()
+			)
+			.setDescription(
+				format(JsonReader.commands.daily.getTranslation(language).healthDaily, { value: activeObject.power })
+			);
 		await entity.addHealth(activeObject.power);
 		entity.Player.Inventory.updateLastDailyAt();
 	}
-	if (activeObject.nature === NATURE.SPEED || activeObject.nature === NATURE.DEFENSE || activeObject.nature === NATURE.ATTACK) { // Those objects are active only during fights
-		return sendErrorMessage(message.author, message.channel, language, JsonReader.commands.daily.getTranslation(language).objectIsActiveDuringFights);
+	if (
+		activeObject.nature === NATURE.SPEED ||
+		activeObject.nature === NATURE.DEFENSE ||
+		activeObject.nature === NATURE.ATTACK
+	) {
+		// Those objects are active only during fights
+		return sendErrorMessage(
+			message.author,
+			message.channel,
+			language,
+			JsonReader.commands.daily.getTranslation(language).objectIsActiveDuringFights
+		);
 	}
 	if (activeObject.nature === NATURE.HOSPITAL) {
-		embed.setColor(JsonReader.bot.embed.default)
-			.setAuthor(format(
-				JsonReader.commands.daily.getTranslation(language).dailySuccess,
-				{pseudo: message.author.username}),
-				message.author.displayAvatarURL())
-			.setDescription(format(
-				JsonReader.commands.daily.getTranslation(language).hospitalBonus,
-				{value: minutesToString(activeObject.power * 60)}));
+		embed
+			.setColor(JsonReader.bot.embed.default)
+			.setAuthor(
+				format(JsonReader.commands.daily.getTranslation(language).dailySuccess, { pseudo: message.author.username }),
+				message.author.displayAvatarURL()
+			)
+			.setDescription(
+				format(JsonReader.commands.daily.getTranslation(language).hospitalBonus, {
+					value: minutesToString(activeObject.power * 60),
+				})
+			);
 		await entity.Player.fastForward(activeObject.power);
 		entity.Player.Inventory.updateLastDailyAt();
 	}
 	if (activeObject.nature === NATURE.MONEY) {
-		embed.setColor(JsonReader.bot.embed.default)
-			.setAuthor(format(
-				JsonReader.commands.daily.getTranslation(language).dailySuccess,
-				{pseudo: message.author.username}),
-				message.author.displayAvatarURL())
-			.setDescription(format(
-				JsonReader.commands.daily.getTranslation(language).moneyBonus,
-				{value: activeObject.power}));
+		embed
+			.setColor(JsonReader.bot.embed.default)
+			.setAuthor(
+				format(JsonReader.commands.daily.getTranslation(language).dailySuccess, { pseudo: message.author.username }),
+				message.author.displayAvatarURL()
+			)
+			.setDescription(
+				format(JsonReader.commands.daily.getTranslation(language).moneyBonus, { value: activeObject.power })
+			);
 		entity.Player.addMoney(activeObject.power);
 		entity.Player.Inventory.updateLastDailyAt();
 	}
 
-	await Promise.all([
-		entity.save(),
-		entity.Player.save(),
-		entity.Player.Inventory.save(),
-	]);
+	await Promise.all([entity.save(), entity.Player.save(), entity.Player.Inventory.save()]);
 	log(entity.discordUser_id + " used his daily item " + activeObject.en);
 	return await message.channel.send(embed);
 };
@@ -84,9 +115,9 @@ const DailyCommand = async function (language, message) {
 module.exports = {
 	commands: [
 		{
-			name: 'daily',
+			name: "daily",
 			func: DailyCommand,
-			aliases: ['da']
-		}
-	]
+			aliases: ["da"],
+		},
+	],
 };
