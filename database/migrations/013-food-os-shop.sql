75f188cec949ff20a353c26bc3fc1998761a0f43
romain22222 <romrom22222@hotmail.fr>
2021-06-07 13:40:10 +0200

    #427 fix wrong edit migrations

--- database/migrations/013-food-os-shop.sql
+++ database/migrations/013-food-os-shop.sql
@@ -8,8 +8,8 @@ ALTER TABLE guilds ADD ultimateFood INTEGER;
 ALTER TABLE possibilities ADD oneshot BOOLEAN;
 UPDATE possibilities SET oneshot = false;
 
-CREATE TABLE IF NOT EXISTS shop(shopPotionId INTEGER, updatedAt DATETIME, createdAt DATETIME);
-INSERT INTO shop(shopPotionId, updatedAt, createdAt) VALUES(5, DATETIME('now'), DATETIME('now'));
+CREATE TABLE IF NOT EXISTS shop(shop_potion_id INTEGER, updatedAt DATETIME, createdAt DATETIME);
+INSERT INTO shop(shop_potion_id, updatedAt, createdAt) VALUES(5, DATETIME('now'), DATETIME('now'));
 
 UPDATE players SET nextEvent = null;
 
---
+++ database/migrations/018-refractor-names.sql
@@ -0,0 +1,13 @@
+-- Up
+
+ALTER TABLE "shop" RENAME COLUMN "shop_potion_id" TO "shopPotionId";
+ALTER TABLE "armors" RENAME COLUMN "french_masculine" TO "frenchMasculine";
+ALTER TABLE "armors" RENAME COLUMN "french_plural" TO "frenchPlural";
+ALTER TABLE "weapons" RENAME COLUMN "french_masculine" TO "frenchMasculine";
+ALTER TABLE "weapons" RENAME COLUMN "french_plural" TO "frenchPlural";
+ALTER TABLE "potions" RENAME COLUMN "french_masculine" TO "frenchMasculine";
+ALTER TABLE "potions" RENAME COLUMN "french_plural" TO "frenchPlural";
+ALTER TABLE "objects" RENAME COLUMN "french_masculine" TO "frenchMasculine";
+ALTER TABLE "objects" RENAME COLUMN "french_plural" TO "frenchPlural";
+
+-- Down
