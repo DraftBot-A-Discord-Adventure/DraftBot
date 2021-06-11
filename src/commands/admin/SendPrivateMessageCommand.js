79d6d53a39d04e6ad1f4723f082ec5e029da9617
eagles <eagles.paladin@gmail.com>
2021-06-11 20:57:42 +0200

    refact sendprivatemessage command name #448

---
+++ src/commands/admin/SendPrivateMessageCommand.js
@@ -0,0 +1,50 @@
+module.exports.help = {
+	name: "sendprivatemessage",
+	aliases: ["dm"]
+};
+
+/**
+ * Allow an admin to change the prefix the bot use in a specific server
+ * @param {("fr"|"en")} language - Language to use in the response
+ * @param {module:"discord.js".Message} message - Message from the discord server
+ * @param {String[]} args=[] - Additional arguments sent with the command
+ */
+const SendPrivateMessageCommand = async (message, language, args) => {
+	if (await canPerformCommand(message, language,
+		PERMISSION.ROLE.SUPPORT) !== true) {
+		return;
+	}
+
+	const userId = args[0];
+	const messageToSend = args.join(" ").replace(userId, "") +
+		format(JsonReader.commands.sendPrivateMessage.getTranslation(language).signature, {
+			username: message.author.username
+		});
+	const user = client.users.cache.get(userId);
+
+	if (userId === undefined || args[1] === undefined) {
+		return sendErrorMessage(user, message.channel, language, JsonReader.commands.sendPrivateMessage.getTranslation(language).descError);
+	}
+	if (user === undefined) {
+		return sendErrorMessage(message.author, message.channel, language, JsonReader.commands.sendPrivateMessage.getTranslation(language).personNotExists);
+	}
+	const embed = new discord.MessageEmbed();
+	embed.setColor(JsonReader.bot.embed.default)
+		.setTitle(format(JsonReader.commands.sendPrivateMessage.getTranslation(language).title, {
+			username: user.username
+		}))
+		.setDescription(messageToSend)
+		.setImage(message.attachments.size > 0 ? [...message.attachments.values()][0].url : "");
+
+	message.delete();
+	try {
+		await user.send(messageToSend);
+		sendMessageAttachments(message, user);
+		return await message.channel.send(embed);
+	}
+	catch {
+		return sendErrorMessage(message.author, message.channel, language, JsonReader.commands.sendPrivateMessage.getTranslation(language).errorCannotSend);
+	}
+};
+
+module.exports.execute = SendPrivateMessageCommand;
