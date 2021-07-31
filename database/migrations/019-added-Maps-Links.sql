2cf35cfdb496e0c019be6148ecea29d72a9ba234
BastLast <bastien.last87@gmail.com>
2021-07-31 19:40:05 +0200

    déplacement des maplinks et ajout des oublis #436

--- database/migrations/019-added-Maps-Links.sql
+++ database/migrations/019-added-Maps-Links.sql
@@ -1,6 +1,6 @@
 -- Up
 
-CREATE TABLE IF NOT EXISTS map_links( id INTEGER PRIMARY KEY, firstMap INTEGER, endMap INTEGER, tripDuration INTEGER, updatedAt DATETIME, createdAt DATETIME);
+CREATE TABLE IF NOT EXISTS map_links( id INTEGER PRIMARY KEY, startMap INTEGER, endMap INTEGER, tripDuration INTEGER, updatedAt DATETIME, createdAt DATETIME);
 
 CREATE TEMPORARY TABLE map_locations_backup( id INTEGER PRIMARY KEY, type TEXT NOT NULL, nameFr TEXT NOT NULL, nameEn TEXT NOT NULL, descFr TEXT NOT NULL, descEn TEXT NOT NULL, particleFr TEXT NOT NULL,particleEn TEXT NOT NULL, updatedAt DATETIME, createdAt DATETIME);
 INSERT INTO map_locations_backup SELECT id, type, nameFr, nameEn, descFr, descEn, particleFr, particleEn, updatedAt, createdAt FROM map_locations;
