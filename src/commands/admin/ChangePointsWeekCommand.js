01a0060f60e9bb3d83e85fc7f125e5d53428c604
romain22222 <romrom22222@hotmail.fr>
2021-08-26 15:08:23 +0200

    #296 commande pointsWeek refaite, juste besoin des traductions anglaises

--- src/commands/admin/ChangePointsWeekCommand.js
+++ src/commands/admin/ChangePointsWeekCommand.js
@@ -5,7 +5,7 @@ module.exports.commandInfo = {
 };
 
 /**
- * Allow the bot owner to give an item to somebody
+ * Allow the bot owner to give weekly points to 1 or more people
  * @param {module:"discord.js".Message} message - Message from the discord server
  * @param {("fr"|"en")} language - Language to use in the response
  * @param {String[]} args=[] - Additional arguments sent with the command
@@ -13,15 +13,100 @@ module.exports.commandInfo = {
 import {DraftBotEmbed} from "../../core/messages/DraftBotEmbed";
 
 const ChangePointsWeekCommand = async (message, language, args) => {
-	const playerId = message.mentions.users.last().id;
-	const [entity] = await Entities.getOrRegister(playerId);
-	entity.Player.weeklyScore = parseInt(args[1]);
-	await entity.Player.save();
+	if (args.length < 3) {
+		return await sendErrorMessage(
+			message.author,
+			message.channel,
+			language,
+			JsonReader.commands.pointsWeek.getTranslation(language).errors.invalidNumberOfArgs
+		);
+	}
+	if (args.length > 68) {
+		return await sendErrorMessage(
+			message.author,
+			message.channel,
+			language,
+			JsonReader.commands.pointsWeek.getTranslation(language).errors.tooMuchPeople
+		);
+	}
+	const amount = parseInt(args[1]);
+	if (isNaN(amount) || amount > 10**17) {
+		return await sendErrorMessage(
+			message.author,
+			message.channel,
+			language,
+			JsonReader.commands.pointsWeek.getTranslation(language).errors.invalidAmountFormat
+		);
+	}
+	const users = new Set();
+	for (let i = 2; i < args.length; i++) {
+		let mention = args[i];
+		if (!isAMention(mention) && (parseInt(mention) < 10 ** 17 || parseInt(mention) >= 10 ** 18)) {
+			return await sendErrorMessage(
+				message.author,
+				message.channel,
+				language,
+				format(JsonReader.commands.pointsWeek.getTranslation(language).errors.invalidIdOrMention, {
+						position: i-1,
+						wrongText: args[i]
+					}
+				)
+			);
+		}
+		users.add(isAMention(mention) ? getIdFromMention(mention) : mention);
+	}
 
+	let descString = "";
+	for (const user of users) {
+		let entityToEdit;
+		try {
+			[entityToEdit] = await Entities.getOrRegister(user);
+		} catch (e) {
+			return await sendErrorMessage(
+				message.author,
+				message.channel,
+				language,
+				format(JsonReader.commands.pointsWeek.getTranslation(language).errors.invalidIdOrMentionDoesntExist, {
+						position: (args.indexOf(user) - 1),
+						wrongText: user
+					}
+				)
+			);
+		}
+		try {
+			giveWeeklyPointsTo(entityToEdit, amount, args);
+		} catch (e) {
+			if (e.message === "mauvais paramètre don points hebdo") {
+				return await sendErrorMessage(
+					message.author,
+					message.channel,
+					language,
+					JsonReader.commands.pointsWeek.getTranslation(language).errors.invalidDonationParameter
+				);
+			}
+			else {
+				console.error(e.stack);
+			}
+		}
+		entityToEdit.Player.save();
+		descString += format(JsonReader.commands.pointsWeek.getTranslation(language).desc, {
+			player: entityToEdit.getMention(),
+			points: entityToEdit.Player.weeklyScore
+		});
+	}
 	return await message.channel.send(new DraftBotEmbed()
 		.formatAuthor(JsonReader.commands.pointsWeek.getTranslation(language).title, message.author)
-		.setDescription(format(JsonReader.commands.pointsWeek.getTranslation(language).desc,
-			{player: args[0], points: args[1]})));
+		.setDescription(descString));
 };
+
+function giveWeeklyPointsTo(entityToEdit, amount, args) {
+	if (args[0] === "set") {
+		entityToEdit.Player.weeklyScore = amount;
+	} else if (args[0] === "add") {
+		entityToEdit.Player.weeklyScore += amount;
+	} else {
+		throw new Error("mauvais paramètre don points hebdo")
+	}
+}
 
 module.exports.execute = ChangePointsWeekCommand;
