ae95729cda9e589ef9f77b640b1ecb8189e263f3
romain22222 <romrom22222@hotmail.fr>
2021-09-09 15:08:05 +0200

    #529 création se advanceTime

---
+++ src/core/smallEvents/advanceTimeSmallEvent.js
@@ -0,0 +1,33 @@
+/**
+ * Main function of small event
+ * @param {module:"discord.js".Message} message
+ * @param {"fr"|"en"} language
+ * @param {Entities} entity
+ * @param {module:"discord.js".MessageEmbed} seEmbed - The template embed to send. The description already contains the emote so you have to get it and add your text
+ * @returns {Promise<>}
+ */
+import {Translations} from "../Translations";
+const Maps = require("../../core/Maps");
+
+const executeSmallEvent = async function(message, language, entity, seEmbed) {
+
+	const timeAdvanced = draftbotRandom.integer(10,50);
+
+	Maps.advanceTime(entity.Player, timeAdvanced);
+	entity.Player.save();
+
+	seEmbed.setDescription(
+		JsonReader.smallEvents.advanceTime.emote +
+		Translations.getModule("smallEventsIntros",language).getRandom("intro") +
+		format(Translations.getModule("smallEvents.advanceTime",language).getRandom("stories"),{
+			time: timeAdvanced
+		})
+	);
+
+	await message.channel.send(seEmbed);
+	log(entity.discordUserId + " advanced " + timeAdvanced + "minutes from a small event.");
+};
+
+module.exports = {
+	executeSmallEvent: executeSmallEvent
+};
