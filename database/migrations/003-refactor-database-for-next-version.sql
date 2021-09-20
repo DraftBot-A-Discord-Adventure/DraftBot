008e3888d12f15a1d6a09be8535f83cf79d805d2
BastLast <bastien.last87@gmail.com>
2021-09-20 22:25:36 +0200

    Bugfix des mirgations

--- database/migrations/003-refactor-database-for-next-version.sql
+++ database/migrations/003-refactor-database-for-next-version.sql
@@ -2,7 +2,6 @@
 
 DROP TABLE IF EXISTS database;
 CREATE TABLE IF NOT EXISTS databases (id INTEGER PRIMARY KEY, lastResetAt DATETIME, updatedAt DATETIME, createdAt DATETIME);
-
 CREATE TEMPORARY TABLE server_backup (id TEXT, prefix TEXT, language TEXT);
 INSERT INTO server_backup SELECT DISTINCT id, prefix, language FROM server;
 DROP TABLE server;
@@ -10,7 +9,6 @@ CREATE TABLE servers (id INTEGER PRIMARY KEY, prefix CHARACTER(10) NOT NULL, lan
 INSERT INTO servers (prefix, language, discordGuild_id, updatedAt, createdAt) SELECT prefix, language, id, DATETIME('now'), DATETIME('now') FROM server_backup;
 DROP TABLE server_backup;
 CREATE UNIQUE INDEX IF NOT EXISTS ius ON servers (discordGuild_id);
-
 CREATE TEMPORARY TABLE entity_backup (id TEXT, maxHealth INTEGER, health INTEGER, attack INTEGER, defense INTEGER, speed INTEGER, effect TEXT);
 INSERT INTO entity_backup SELECT DISTINCT id, maxHealth, health, attack, defense, speed, effect FROM entity;
 DROP TABLE entity;
@@ -18,16 +16,13 @@ CREATE TABLE entities (id INTEGER PRIMARY KEY, maxHealth INTEGER NOT NULL, healt
 INSERT INTO entities (maxHealth, health, attack, defense, speed, effect, discordUser_id, updatedAt, createdAt) SELECT maxHealth, health, attack, defense, speed, effect, id, DATETIME('now'), DATETIME('now') FROM entity_backup;
 DROP TABLE entity_backup;
 CREATE UNIQUE INDEX IF NOT EXISTS iue ON entities (discordUser_id);
-
 CREATE TEMPORARY TABLE player_backup (discordId TEXT, score INTEGER, weeklyScore INTEGER, level INTEGER, experience INTEGER, money INTEGER, lastReport INTEGER, badges TEXT, guildId TEXT);
 INSERT INTO player_backup SELECT DISTINCT discordId, score, weeklyScore, level, experience, money, lastReport, badges, guildId FROM player;
 DROP TABLE player;
 CREATE TABLE players (id INTEGER PRIMARY KEY, score INTEGER NOT NULL, weeklyScore INTEGER NOT NULL, level INTEGER NOT NULL, experience INTEGER NOT NULL, money INTEGER NOT NULL, badges TEXT, lastReportAt DATETIME, entity_id INTEGER NOT NULL, guild_id VARCHAR(64), updatedAt DATETIME, createdAt DATETIME, oldDiscordId VARCHAR(64), oldGuildId VARCHAR(64));
 INSERT INTO players (score, weeklyscore, level, experience, money, badges, entity_id, updatedAt, createdAt, oldDiscordId, oldGuildId) SELECT pb.score, pb.weeklyScore, pb.level, pb.experience, pb.money, pb.badges, e.id, DATETIME('now'), DATETIME('now'), pb.discordId, pb.guildId FROM player_backup as pb JOIN entities as e ON pb.discordId = e.discordUser_id;
-
 CREATE TEMPORARY TABLE inventory_backup (playerId TEXT, weaponId TEXT, armorId TEXT, potionId TEXT, objectId TEXT, backupItemId TEXT, lastDaily INTEGER);
 INSERT INTO inventory_backup SELECT DISTINCT playerId, weaponId, armorId, potionId, objectId, backupItemId, lastDaily FROM inventory;
-
 DROP TABLE inventory;
 CREATE TABLE inventories (id INTEGER PRIMARY KEY, lastDailyAt DATETIME, player_id INTEGER NOT NULL, weapon_id INTEGER NOT NULL, armor_id INTEGER NOT NULL, potion_id INTEGER NOT NULL, object_id INTEGER NOT NULL, backup_id INTEGER NOT NULL, updatedAt DATETIME, createdAt DATETIME);
 INSERT INTO inventories (player_id, weapon_id, armor_id, potion_id, object_id, backup_id, updatedAt, createdAt) SELECT p.id, ib.weaponId, ib.armorId, ib.potionId, ib.objectId, ib.backupItemId, DATETIME('now'), DATETIME('now') FROM inventory_backup as ib JOIN players as p ON p.oldDiscordId = ib.playerId;
@@ -39,16 +34,13 @@ UPDATE inventories SET object_id = 0 WHERE object_id = 'default';
 UPDATE inventories SET backup_id = 0 WHERE backup_id = 'default';
 DROP TABLE inventory_backup;
 CREATE UNIQUE INDEX IF NOT EXISTS iui ON inventories (player_id);
-
 CREATE TEMPORARY TABLE guild_backup (guildId TEXT, name TEXT, chief TEXT, score INTEGER, level INTEGER, experience INTEGER, lastInvocation INTEGER);
 INSERT INTO guild_backup SELECT guildId, name, chief, score, level, experience, lastInvocation FROM guild;
 DROP TABLE guild;
 CREATE TABLE guilds (id INTEGER PRIMARY KEY, name VARCHAR(32) NOT NULL, score INTEGER NOT NULL, level INTEGER NOT NULL, experience INTEGER NOT NULL, lastDailyAt DATETIME, chief_id INTEGER NOT NULL, updatedAt DATETIME, createdAt DATETIME, oldGuildId TEXT);
 INSERT INTO guilds (name, score, level, experience, chief_id, updatedAt, createdAt, oldGuildId) SELECT gb.name, gb.score, gb.level, gb.experience, p.id, DATETIME('now'), DATETIME('now'), gb.guildId FROM guild_backup as gb JOIN players as p ON gb.chief = p.oldDiscordId;
-
 DROP TABLE guild_backup;
 DROP TABLE player_backup;
-
 CREATE TEMPORARY TABLE player_backup (id INTEGER PRIMARY KEY, score INTEGER NOT NULL, weeklyScore INTEGER NOT NULL, level INTEGER NOT NULL, experience INTEGER NOT NULL, money INTEGER NOT NULL, badges TEXT, lastReportAt DATETIME, entity_id INTEGER NOT NULL, guild_id VARCHAR(64), updatedAt DATETIME, createdAt DATETIME, oldDiscordId VARCHAR(64), oldGuildId VARCHAR(64));
 INSERT INTO player_backup SELECT id, score, weeklyScore, level, experience, money, badges, lastReportAt, entity_id, guild_id, updatedAt, createdAt, oldGuildId, oldGuildId FROM players;
 DROP TABLE players;
@@ -61,7 +53,6 @@ CREATE INDEX IF NOT EXISTS ip ON players (score);
 CREATE INDEX IF NOT EXISTS ip1 ON players (weeklyscore);
 CREATE INDEX IF NOT EXISTS ip2 ON players (guild_id);
 DROP TABLE player_backup;
-
 CREATE TEMPORARY TABLE guild_backup (id INTEGER PRIMARY KEY, name VARCHAR(32) NOT NULL, score INTEGER NOT NULL, level INTEGER NOT NULL, experience INTEGER NOT NULL, lastDailyAt DATETIME, chief_id INTEGER NOT NULL, updatedAt DATETIME, createdAt DATETIME);
 INSERT INTO guild_backup SELECT id, name, score, level, experience, lastDailyAt, chief_id, updatedAt, createdAt FROM guilds;
 DROP TABLE guilds;
@@ -71,7 +62,6 @@ UPDATE guilds SET lastDailyAt = DATETIME(0, 'unixepoch');
 CREATE UNIQUE INDEX IF NOT EXISTS iug2 ON guilds (chief_id);
 CREATE INDEX IF NOT EXISTS ig ON guilds (score);
 DROP TABLE guild_backup;
-
 CREATE TABLE IF NOT EXISTS armors (id INTEGER PRIMARY KEY, rarity INTEGER NOT NULL, rawAttack INTEGER NOT NULL, rawDefense INTEGER NOT NULL, rawSpeed INTEGER NOT NULL, attack INTEGER NOT NULL, defense INTEGER NOT NULL, speed INTEGER NOT NULL, fr TEXT NOT NULL, en TEXT NOT NULL, updatedAt DATETIME, createdAt DATETIME);
 CREATE TABLE IF NOT EXISTS weapons (id INTEGER PRIMARY KEY, rarity INTEGER NOT NULL, rawAttack INTEGER NOT NULL, rawDefense INTEGER NOT NULL, rawSpeed INTEGER NOT NULL, attack INTEGER NOT NULL, defense INTEGER NOT NULL, speed INTEGER NOT NULL, fr TEXT NOT NULL, en TEXT NOT NULL, updatedAt DATETIME, createdAt DATETIME);
 CREATE TABLE IF NOT EXISTS potions (id INTEGER PRIMARY KEY, rarity INTEGER NOT NULL, power INTEGER NOT NULL, nature INTEGER NOT NULL, fr TEXT NOT NULL, en TEXT NOT NULL, updatedAt DATETIME, createdAt DATETIME);
