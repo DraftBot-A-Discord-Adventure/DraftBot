8dd248f9e358a4daba49f04e15d2b86ae3b860d1
niqore <nicolas.v1999@hotmail.fr>
2021-10-18 01:12:17 +0200

    Hotfixes

--- src/core/DraftBot.js
+++ src/core/DraftBot.js
@@ -311,11 +311,13 @@ class DraftBot {
 				fs.appendFileSync(
 					global.currLogsFile,
 					dateStr +
-					message.replace(
+					message/*
+					 // TODO sera remplacé par un vrai système de logs next maj
+					 .replace(
 						// eslint-disable-next-line no-control-regex
 						/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
 						""
-					) +
+					)*/ +
 					"\n",
 					new function(err) {
 						if (err !== undefined) {
