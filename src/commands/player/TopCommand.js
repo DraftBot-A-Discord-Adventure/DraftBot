e42be60b76cdf17731b4becbf8d2e18d6066a346
Pietagorh <60353007+Pietagorh@users.noreply.github.com>
2022-01-07 18:37:47 +0100

    fixes (#819)
    * fix nerf display #816 
    
    * fix top display #793
    
    + rename topCommand.json -> top.json pour que ça colle avec les autres noms
    
    * fix item 11 emoji display
    
    * fix ma connerie sur 9ea96f67424a19345f3fe391ed4e578dd623c7cb
    
    * balance petlovepoints lost in pettrade #700
    
    Tout -> 5 * rareté
    
    * fix fight draw summarize #731
    
    * fix gd full heal #822
    
    Donc la colonne maxHealth n'est plus du tout utilisée
    
    * hotfix map image display
    
    ça marchait pas à de temps en temps, j'espère avoir réglé le problème en faisant ça (en tout cas j'ai rien empiré, ça marche toujours x))
    
    * fix indent problem on 50f7e45e705bfad10f3ac136f02e7a72e4ad7e25
    
    * Revert "hotfix map image display"
    
    This reverts commit d79ffb50cff01b00a1c6f4ee4ef87b5e2ffa72c7.

--- src/commands/player/TopCommand.js
+++ src/commands/player/TopCommand.js
@@ -18,6 +18,7 @@ const TopCommand = async function(message, language, args) {
 	const [entity] = await Entities.getOrRegister(message.author.id);
 
 	let rankCurrentPlayer = 0;
+	let scoreTooLow = 0;
 
 	let page = parseInt(args[args.length - 1]);
 	if (page < 1) {
@@ -26,18 +27,14 @@ const TopCommand = async function(message, language, args) {
 	if (isNaN(page)) {
 		page = 1;
 	}
+	if (entity.Player.score <= 100) {
+		scoreTooLow = 1;
+	}
 
 	// top of the serv
 	if (args[0] === "serv" || args[0] === "s") {
-
-		if (entity.Player.score <= 100) {
-			return await errorScoreTooLow(message, language);
-		}
-
 		// get all discordID on the server
 		const listId = Array.from((await message.guild.members.fetch()).keys());
-
-		rankCurrentPlayer = (await Entities.getServerRank(message.author.id, listId))[0].rank;
 
 		const numberOfPlayer = await Entities.count({
 			defaults: {
@@ -85,16 +82,15 @@ const TopCommand = async function(message, language, args) {
 			offset: (page - 1) * 15
 		});
 
-		await displayTop(message, language, numberOfPlayer, allEntities, rankCurrentPlayer, JsonReader.commands.topCommand.getTranslation(language).server, page);
+		if (scoreTooLow === 0) {
+			rankCurrentPlayer = (await Entities.getServerRank(message.author.id, listId))[0].rank;
+		}
+
+		await displayTop(message, language, numberOfPlayer, allEntities, rankCurrentPlayer, JsonReader.commands.top.getTranslation(language).server, page, scoreTooLow);
 	}
 
 	// top general of the week
 	else if (args[0] === "week" || args[0] === "w") {
-
-		if (entity.Player.weeklyScore <= 100) {
-			return await errorScoreTooLow(message, language);
-		}
-
 		// rank of the user
 		const rankCurrentPlayer = (await Players.getById(entity.Player.id))[0].weeklyRank;
 		const numberOfPlayer = await Players.count({
@@ -127,15 +123,12 @@ const TopCommand = async function(message, language, args) {
 			offset: (page - 1) * 15
 		});
 
-		await displayTop(message, language, numberOfPlayer, allEntities, rankCurrentPlayer, JsonReader.commands.topCommand.getTranslation(language).generalWeek, page);
+		await displayTop(message, language, numberOfPlayer, allEntities, rankCurrentPlayer, JsonReader.commands.top.getTranslation(language).generalWeek, page, scoreTooLow);
 	}
 
 	// top general by a page number
 	else {
 		// rank of the user
-		if (entity.Player.score <= 100) {
-			return await errorScoreTooLow(message, language);
-		}
 		const rankCurrentPlayer = (await Players.getById(entity.Player.id))[0].rank;
 
 		const numberOfPlayer = await Players.count({
@@ -169,19 +162,9 @@ const TopCommand = async function(message, language, args) {
 			offset: (page - 1) * 15
 		});
 
-		await displayTop(message, language, numberOfPlayer, allEntities, rankCurrentPlayer, JsonReader.commands.topCommand.getTranslation(language).general, page);
+		await displayTop(message, language, numberOfPlayer, allEntities, rankCurrentPlayer, JsonReader.commands.top.getTranslation(language).general, page, scoreTooLow);
 	}
 };
-
-/**
- * Send an error message saying that the score is too low
- * @param {module:"discord.js".Message} message
- * @param {"fr"|"en"} language
- * @return {Promise<Message>}
- */
-async function errorScoreTooLow(message, language) {
-	return await sendErrorMessage(message.author, message.channel, language, JsonReader.commands.topCommand.getTranslation(language).lowScore);
-}
 
 /**
  * Sends a message with the top
@@ -193,10 +176,11 @@ async function errorScoreTooLow(message, language) {
  * @param {Number} rankCurrentPlayer
  * @param {string} topTitle
  * @param {Number} page
+ * @param {*} scoreTooLow
  * @return {Promise<Message>}
  */
 
-async function displayTop(message, language, numberOfPlayer, allEntities, rankCurrentPlayer, topTitle, page) { // eslint-disable-line max-params
+async function displayTop(message, language, numberOfPlayer, allEntities, rankCurrentPlayer, topTitle, page, scoreTooLow) { // eslint-disable-line max-params
 	const embedError = new DraftBotEmbed();
 	const embed = new DraftBotEmbed();
 	const actualPlayer = message.author.username;
@@ -209,11 +193,11 @@ async function displayTop(message, language, numberOfPlayer, allEntities, rankCu
 	}
 	if (page > pageMax || page < 1) {
 		embedError
-			.setTitle(format(JsonReader.commands.topCommand.getTranslation(language).maxPageTitle, {
+			.setTitle(format(JsonReader.commands.top.getTranslation(language).maxPageTitle, {
 				pseudo: actualPlayer,
 				pageMax: pageMax
 			}))
-			.setDescription(format(JsonReader.commands.topCommand.getTranslation(language).maxPageDesc, {pageMax: pageMax}));
+			.setDescription(format(JsonReader.commands.top.getTranslation(language).maxPageDesc, {pageMax: pageMax}));
 		return await message.channel.send({ embeds: [embedError] });
 	}
 	const fin = page * 15;
@@ -235,28 +219,28 @@ async function displayTop(message, language, numberOfPlayer, allEntities, rankCu
 		// badge depending on the rank
 		if (page === 1) {
 			if (k === 0) {
-				badge = JsonReader.commands.topCommand.first;
+				badge = JsonReader.commands.top.first;
 			}
 			else if (k === 1) {
-				badge = JsonReader.commands.topCommand.second;
+				badge = JsonReader.commands.top.second;
 			}
 			else if (k === 2) {
-				badge = JsonReader.commands.topCommand.third;
+				badge = JsonReader.commands.top.third;
 			}
 			else if (k > 2 && k <= 4) {
-				badge = JsonReader.commands.topCommand.military;
+				badge = JsonReader.commands.top.military;
 			}
 		}
 		if (page !== 1 || k > 4) {
 			if (message.guild.members.cache.find(val => val.id === allEntities[k].discordUserId)) {
-				badge = JsonReader.commands.topCommand.blue;
+				badge = JsonReader.commands.top.blue;
 			}
 			else {
-				badge = JsonReader.commands.topCommand.black;
+				badge = JsonReader.commands.top.black;
 			}
 		}
 		if (message.author.id === allEntities[k].discordUserId) {
-			badge = JsonReader.commands.topCommand.white;
+			badge = JsonReader.commands.top.white;
 		}
 
 		// badgeState depending on last report
@@ -275,19 +259,19 @@ async function displayTop(message, language, numberOfPlayer, allEntities, rankCu
 		else {
 			badgeState = ":smiley:";
 		}
-		messages += format(JsonReader.commands.topCommand.getTranslation(language).playerRankLine, {
+		messages += format(JsonReader.commands.top.getTranslation(language).playerRankLine, {
 			badge: badge,
 			rank: debut + k,
 			pseudo: pseudo,
 			badgeState: badgeState !== ":smiley:" ? badgeState + " | " : "",
-			score: topTitle === JsonReader.commands.topCommand.getTranslation(language).generalWeek ? allEntities[k].Player.weeklyScore : allEntities[k].Player.score,
+			score: topTitle === JsonReader.commands.top.getTranslation(language).generalWeek ? allEntities[k].Player.weeklyScore : allEntities[k].Player.score,
 			level: allEntities[k].Player.level
 		});
 	}
-	if (topTitle === JsonReader.commands.topCommand.getTranslation(language).generalWeek) {
+	if (topTitle === JsonReader.commands.top.getTranslation(language).generalWeek) {
 		embed.setFooter(
 			format(
-				JsonReader.commands.topCommand.getTranslation(language).nextReset, {time: parseTimeDifference(new Date(), getNextSundayMidnight(), language)}
+				JsonReader.commands.top.getTranslation(language).nextReset, {time: parseTimeDifference(new Date(), getNextSundayMidnight(), language)}
 			), "https://i.imgur.com/OpL9WpR.png"
 		);
 	}
@@ -309,10 +293,18 @@ async function displayTop(message, language, numberOfPlayer, allEntities, rankCu
 	else {
 		badge = ":black_circle:";
 	}
-
+	// test if the user has 100 points
+	if (scoreTooLow === 1) {
+		embed.addField(JsonReader.commands.top.getTranslation(language).yourRanking, format(JsonReader.commands.top.getTranslation(language).lowScore, {
+			badge: badge,
+			pseudo: actualPlayer,
+			totalPlayer: numberOfPlayer,
+			pageMax: pageMax
+		}));
+	}
 	// test if user is in the current page displayed to indicate(or not) the page where he can find himself
-	if ((rankCurrentPlayer > fin || rankCurrentPlayer < debut) && rankCurrentPlayer !== 1) {
-		embed.addField(JsonReader.commands.topCommand.getTranslation(language).yourRanking, format(JsonReader.commands.topCommand.getTranslation(language).end1, {
+	else if ((rankCurrentPlayer > fin || rankCurrentPlayer < debut) && rankCurrentPlayer !== 1) {
+		embed.addField(JsonReader.commands.top.getTranslation(language).yourRanking, format(JsonReader.commands.top.getTranslation(language).end1, {
 			badge: badge,
 			pseudo: actualPlayer,
 			rank: rankCurrentPlayer,
@@ -322,7 +314,7 @@ async function displayTop(message, language, numberOfPlayer, allEntities, rankCu
 		}));
 	}
 	else if ((rankCurrentPlayer > fin || rankCurrentPlayer < debut) && rankCurrentPlayer === 1) {
-		embed.addField(JsonReader.commands.topCommand.getTranslation(language).yourRanking, format(JsonReader.commands.topCommand.getTranslation(language).end1Top, {
+		embed.addField(JsonReader.commands.top.getTranslation(language).yourRanking, format(JsonReader.commands.top.getTranslation(language).end1Top, {
 			pseudo: actualPlayer,
 			rank: rankCurrentPlayer,
 			totalPlayer: numberOfPlayer,
@@ -331,7 +323,7 @@ async function displayTop(message, language, numberOfPlayer, allEntities, rankCu
 		}));
 	}
 	else if ((rankCurrentPlayer <= fin || rankCurrentPlayer >= debut) && rankCurrentPlayer !== 1) {
-		embed.addField(JsonReader.commands.topCommand.getTranslation(language).yourRanking, format(JsonReader.commands.topCommand.getTranslation(language).end2, {
+		embed.addField(JsonReader.commands.top.getTranslation(language).yourRanking, format(JsonReader.commands.top.getTranslation(language).end2, {
 			badge: badge,
 			pseudo: actualPlayer,
 			rank: rankCurrentPlayer,
@@ -339,7 +331,7 @@ async function displayTop(message, language, numberOfPlayer, allEntities, rankCu
 		}));
 	}
 	else if ((rankCurrentPlayer <= fin || rankCurrentPlayer >= debut) && rankCurrentPlayer === 1) {
-		embed.addField(JsonReader.commands.topCommand.getTranslation(language).yourRanking, format(JsonReader.commands.topCommand.getTranslation(language).end2Top, {
+		embed.addField(JsonReader.commands.top.getTranslation(language).yourRanking, format(JsonReader.commands.top.getTranslation(language).end2Top, {
 			pseudo: actualPlayer,
 			rank: rankCurrentPlayer,
 			totalPlayer: numberOfPlayer
