d453d34ea3ca78e774924cf419d08d68c502868d
Pietagorh <solal.hara@yahoo.com>
2022-01-08 17:38:00 +0100

    fix ident problems on 27052a04907eed2869c06ac7aa95dcfb5d13fbfe

--- src/commands/player/DrinkCommand.js
+++ src/commands/player/DrinkCommand.js
@@ -21,7 +21,7 @@ const DrinkCommand = async (message, language, args) => {
 	if (await sendBlockedError(message.author, message.channel, language)) {
 		return;
 	}
-	
+
 	const potion = await entity.Player.getMainPotionSlot().getItem();
 	const embed = new DraftBotEmbed()
 		.formatAuthor(tr.get("drinkSuccess"), message.author);
@@ -34,38 +34,38 @@ const DrinkCommand = async (message, language, args) => {
 	}
 	const drinkPotion = async (validateMessage, potion) => {
 		removeBlockedPlayer(entity.discordUserId);
-			if (args[0] === "force" || args[0] === "f" || validateMessage.isValidated()) {
-				if (potion.nature === NATURE.NONE) {
-					await entity.Player.drinkPotion();
-					return sendErrorMessage(message.author, message.channel, language, tr.get("objectDoNothingError"));
-				}
-				if (potion.nature === NATURE.HEALTH) {
-					embed.setDescription(format(tr.get("healthBonus"), {value: potion.power}));
-					await entity.addHealth(potion.power);
-					await entity.Player.drinkPotion();
-				}
-				if (potion.nature === NATURE.HOSPITAL) {
-					embed.setDescription(format(tr.get("hospitalBonus"), {value: potion.power}));
-					Maps.advanceTime(entity.Player, potion.power * 60);
-					entity.Player.save();
-					await entity.Player.drinkPotion();
-				}
-				if (potion.nature === NATURE.MONEY) {
-					embed.setDescription(format(tr.get("moneyBonus"), {value: potion.power}));
-					entity.Player.addMoney(potion.power);
-					await entity.Player.drinkPotion();
-				}
-	
-				await Promise.all([
-					entity.save(),
-					entity.Player.save()
-				]);
-				log(entity.discordUserId + " drank " + potion.en);
-				return await message.channel.send({ embeds: [embed] });
-			};
-			removeBlockedPlayer(entity.discordUserId);
-			return sendErrorMessage(message.author, message.channel, language, tr.get("drinkCanceled"));
-		}
+		if (args[0] === "force" || args[0] === "f" || validateMessage.isValidated()) {
+			if (potion.nature === NATURE.NONE) {
+				await entity.Player.drinkPotion();
+				return sendErrorMessage(message.author, message.channel, language, tr.get("objectDoNothingError"));
+			}
+			if (potion.nature === NATURE.HEALTH) {
+				embed.setDescription(format(tr.get("healthBonus"), {value: potion.power}));
+				await entity.addHealth(potion.power);
+				await entity.Player.drinkPotion();
+			}
+			if (potion.nature === NATURE.HOSPITAL) {
+				embed.setDescription(format(tr.get("hospitalBonus"), {value: potion.power}));
+				Maps.advanceTime(entity.Player, potion.power * 60);
+				entity.Player.save();
+				await entity.Player.drinkPotion();
+			}
+			if (potion.nature === NATURE.MONEY) {
+				embed.setDescription(format(tr.get("moneyBonus"), {value: potion.power}));
+				entity.Player.addMoney(potion.power);
+				await entity.Player.drinkPotion();
+			}
+
+			await Promise.all([
+				entity.save(),
+				entity.Player.save()
+			]);
+			log(entity.discordUserId + " drank " + potion.en);
+			return await message.channel.send({ embeds: [embed] });
+		};
+		removeBlockedPlayer(entity.discordUserId);
+		return sendErrorMessage(message.author, message.channel, language, tr.get("drinkCanceled"));
+	}
 	
 	if (args[0] === "force" || args[0] === "f") {
 		drinkPotion(null, potion)
