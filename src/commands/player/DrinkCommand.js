27052a04907eed2869c06ac7aa95dcfb5d13fbfe
Pietagorh <solal.hara@yahoo.com>
2022-01-08 14:46:18 +0100

    drink #838: better confirmation message, add en translates & refactor
    J'en ai profité pour enlever tous les jsonReader

--- src/commands/player/DrinkCommand.js
+++ src/commands/player/DrinkCommand.js
@@ -26,32 +26,32 @@ const DrinkCommand = async (message, language, args) => {
 	const embed = new DraftBotEmbed()
 		.formatAuthor(tr.get("drinkSuccess"), message.author);
 
-	if (potion.nature === NATURE.NONE && potion.id === JsonReader.models.inventories.potionId) {
+	if (potion.nature === NATURE.NONE && potion.id === Translations.getModule("models.inventories", language).get("potionId")) {
 		return sendErrorMessage(message.author, message.channel, language, tr.get("noActiveObjectdescription"));
 	}
 	if (potion.nature === NATURE.SPEED || potion.nature === NATURE.DEFENSE || potion.nature === NATURE.ATTACK) { // Those objects are active only during fights
-		return sendErrorMessage(message.author, message.channel, language, JsonReader.commands.drink.getTranslation(language).objectIsActiveDuringFights);
+		return sendErrorMessage(message.author, message.channel, language, tr.get("objectIsActiveDuringFights"));
 	}
 	const drinkPotion = async (validateMessage, potion) => {
 		removeBlockedPlayer(entity.discordUserId);
 			if (args[0] === "force" || args[0] === "f" || validateMessage.isValidated()) {
 				if (potion.nature === NATURE.NONE) {
 					await entity.Player.drinkPotion();
-					return sendErrorMessage(message.author, message.channel, language, JsonReader.commands.drink.getTranslation(language).objectDoNothingError);
+					return sendErrorMessage(message.author, message.channel, language, tr.get("objectDoNothingError"));
 				}
 				if (potion.nature === NATURE.HEALTH) {
-					embed.setDescription(format(JsonReader.commands.drink.getTranslation(language).healthBonus, {value: potion.power}));
+					embed.setDescription(format(tr.get("healthBonus"), {value: potion.power}));
 					await entity.addHealth(potion.power);
 					await entity.Player.drinkPotion();
 				}
 				if (potion.nature === NATURE.HOSPITAL) {
-					embed.setDescription(format(JsonReader.commands.drink.getTranslation(language).hospitalBonus, {value: potion.power}));
+					embed.setDescription(format(tr.get("hospitalBonus"), {value: potion.power}));
 					Maps.advanceTime(entity.Player, potion.power * 60);
 					entity.Player.save();
 					await entity.Player.drinkPotion();
 				}
 				if (potion.nature === NATURE.MONEY) {
-					embed.setDescription(format(JsonReader.commands.drink.getTranslation(language).moneyBonus, {value: potion.power}));
+					embed.setDescription(format(tr.get("moneyBonus"), {value: potion.power}));
 					entity.Player.addMoney(potion.power);
 					await entity.Player.drinkPotion();
 				}
@@ -75,7 +75,9 @@ const DrinkCommand = async (message, language, args) => {
 		.formatAuthor(tr.get("confirmationTitle"), message.author)
 		.setDescription(tr.format("confirmation", {
 			potion: potion[language],
+			effect: potion.getNatureTranslation(language)
 		}))
+		.setFooter(tr.get("confirmationFooter"))
 		.send(message.channel, (collector) => addBlockedPlayer(entity.discordUserId, "drink", collector));
 	}
 }
