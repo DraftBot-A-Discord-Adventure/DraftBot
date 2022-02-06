2e22cfad2747763c45644cb2462abdcac2e33bd8
BastLast <bastien.last87@gmail.com>
2022-02-06 23:13:38 +0100

    v1 amélioration du profil du joueur #153
    v1 amélioration du profil du joueur #153

--- src/commands/player/ProfileCommand.js
+++ src/commands/player/ProfileCommand.js
@@ -3,6 +3,7 @@ import {Classes} from "../../core/models/Class";
 import {Entities} from "../../core/models/Entity";
 import {Guilds} from "../../core/models/Guild";
 import Player, {Players} from "../../core/models/Player";
+import {Campaign} from "../../core/missions/Campaign";
 
 module.exports.commandInfo = {
 	name: "profile",
@@ -49,21 +50,32 @@ const ProfileCommand = async (message, language, args) => {
 			})
 		},
 		{
+			name: JsonReader.commands.profile.getTranslation(language).mission.fieldName,
+			value: format(JsonReader.commands.profile.getTranslation(language).mission.fieldValue, {
+				gems: entity.Player.PlayerMissionsInfo.gems,
+				campaign: Math.round((entity.Player.PlayerMissionsInfo.campaignProgression - 1) / Campaign.getMaxCampaignNumber() * 100)
+			}
+			),
+			inline: false
+		},
+		{
 			name: JsonReader.commands.profile.getTranslation(language).classement.fieldName,
-			value: format(JsonReader.commands.profile.getTranslation(
-				language).classement.fieldValue, {
-				rank: (await Players.getById(entity.Player.id))[0].rank,
-				numberOfPlayer: await Player.count({
-					where: {
-						score: {
-							[require("sequelize/lib/operators").gt]: 100
-						}
-					}
-				}),
-				score: entity.Player.score
-			})
+			value:
+					format(JsonReader.commands.profile.getTranslation(
+						language).classement.fieldValue, {
+						rank: (await Players.getById(entity.Player.id))[0].rank,
+						numberOfPlayer: await Player.count({
+							where: {
+								score: {
+									[require("sequelize/lib/operators").gt]: 100
+								}
+							}
+						}),
+						score: entity.Player.score
+					})
 		}
-	];
+	]
+	;
 
 	if (!entity.Player.checkEffect()) {
 		if (message.createdAt.getTime() >= entity.Player.effectEndDate) {
@@ -147,16 +159,17 @@ const ProfileCommand = async (message, language, args) => {
 	catch (error) {
 		console.log(error);
 	}
-
-	const msg = await message.channel.send({ embeds: [
-		new DraftBotEmbed()
-			.setTitle(format(JsonReader.commands.profile.getTranslation(language).title, {
-				effect: titleEffect,
-				pseudo: await entity.Player.getPseudo(language),
-				level: entity.Player.level
-			}))
-			.addFields(fields)
-	] });
+	const msg = await message.channel.send({
+		embeds: [
+			new DraftBotEmbed()
+				.setTitle(format(JsonReader.commands.profile.getTranslation(language).title, {
+					effect: titleEffect,
+					pseudo: await entity.Player.getPseudo(language),
+					level: entity.Player.level
+				}))
+				.addFields(fields)
+		]
+	});
 
 	const filterConfirm = (reaction) => reaction.me && !reaction.users.cache.last().bot;
 
@@ -167,7 +180,7 @@ const ProfileCommand = async (message, language, args) => {
 	});
 
 	collector.on("collect", (reaction) => {
-		message.channel.send({ content: JsonReader.commands.profile.getTranslation(language).badges[reaction.emoji.name] }).then((msg) => {
+		message.channel.send({content: JsonReader.commands.profile.getTranslation(language).badges[reaction.emoji.name]}).then((msg) => {
 			setTimeout(() => msg.delete(), JsonReader.commands.profile.badgeDescriptionTimeout);
 		});
 	});
