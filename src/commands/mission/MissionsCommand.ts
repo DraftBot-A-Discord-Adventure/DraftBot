04f51c8db8445003ea9dd2b1b4a31506c6a09e6c
Romain NEGRO <romain.negro@etu.univ-smb.fr>
2022-03-04 23:59:56 +0100

    fix #993

--- src/commands/mission/MissionsCommand.ts
+++ src/commands/mission/MissionsCommand.ts
@@ -4,11 +4,13 @@ import {Entities} from "../../core/models/Entity";
 import {Campaign} from "../../core/missions/Campaign";
 import {MissionsController} from "../../core/missions/MissionsController";
 import {DraftBotMissionsMessageBuilder} from "../../core/messages/DraftBotMissionsMessage";
+import {draftBotClient} from "../../core/bot";
+
 declare function sendBlockedError(user: User, channel: TextChannel, language: string): Promise<boolean>;
 
 export const commandInfo = {
 	name: "missions",
-	aliases: ["m", "mission", "quests", "quest","q"],
+	aliases: ["m", "mission", "quests", "quest", "q"],
 	disallowEffects: [Constants.EFFECT.BABY, Constants.EFFECT.DEAD]
 };
 
@@ -16,23 +18,32 @@ const MissionsCommand = async (message: Message, language: string, args: string[
 	if (await sendBlockedError(message.author, <TextChannel>message.channel, language)) {
 		return;
 	}
-	let [entity] = await Entities.getByArgs(args, message);
-	if (!entity) {
-		[entity] = await Entities.getOrRegister(message.author.id);
+	let [entity] = await Entities.getOrRegister(message.author.id);
+	let [entityToLook] = await Entities.getByArgs(args, message);
+	let userToPrint = message.author;
+	if (entityToLook !== null) {
+		userToPrint = await draftBotClient.users.fetch(entityToLook.discordUserId);
+	}
+	else {
+		entityToLook = entity;
 	}
 
-	await MissionsController.update(entity.discordUserId, <TextChannel> message.channel, language, "commandMission");
+	await MissionsController.update(entity.discordUserId, <TextChannel>message.channel, language, "commandMission");
 	entity = await Entities.getById(entity.id);
 
-	await Campaign.updateCampaignAndSendMessage(entity, <TextChannel> message.channel, language);
-	[entity] = await Entities.getOrRegister(entity.discordUserId);
-	message.channel.send({ embeds: [
-		await new DraftBotMissionsMessageBuilder(
-			entity.Player,
-			message.author,
-			language
-		).build()
-	]});
+	await Campaign.updateCampaignAndSendMessage(entity, <TextChannel>message.channel, language);
+	if (entityToLook.discordUserId === entity.discordUserId) {
+		[entityToLook] = await Entities.getOrRegister(entityToLook.discordUserId);
+	}
+	message.channel.send({
+		embeds: [
+			await new DraftBotMissionsMessageBuilder(
+				entityToLook.Player,
+				userToPrint,
+				language
+			).build()
+		]
+	});
 };
 
 export const execute = MissionsCommand;
