5197c3c0a199562cc2c5e4f4f712151445e267c2
BastLast <bastien.last87@gmail.com>
2022-03-14 00:46:00 +0100

    Vote command refactoring slash command #331 + bugfix

---
+++ src/commands/player/VoteCommand.ts
@@ -0,0 +1,31 @@
+import {ICommand} from "../ICommand";
+import {SlashCommandBuilder} from "@discordjs/builders";
+import {CommandInteraction} from "discord.js";
+import {Translations} from "../../core/Translations";
+import {DraftBotEmbed} from "../../core/messages/DraftBotEmbed";
+
+async function executeCommand(interaction: CommandInteraction, language: string): Promise<void> {
+	const tr = Translations.getModule("commands.vote", language);
+	await interaction.reply({
+		embeds: [new DraftBotEmbed()
+			.setDescription(tr.get("text"))
+			.setTitle(tr.get("title"))]
+	});
+}
+
+export const commandInfo: ICommand = {
+	slashCommandBuilder: new SlashCommandBuilder()
+		.setName("vote")
+		.setDescription("Display a link that allow a user to vote for the bot."),
+	executeCommand,
+	requirements: {
+		allowEffects: null,
+		requiredLevel: null,
+		disallowEffects: null,
+		guildPermissions: null,
+		guildRequired: null,
+		userPermission: null
+	},
+	mainGuildCommand: false,
+	slashCommandPermissions: null
+};
