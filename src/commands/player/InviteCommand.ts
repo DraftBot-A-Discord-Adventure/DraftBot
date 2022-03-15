c05788cc09dcd02309b923fcf8cf8de858cc2cf3
BastLast <bastien.last87@gmail.com>
2022-03-16 00:26:35 +0100

    invite command to slash command #331

---
+++ src/commands/player/InviteCommand.ts
@@ -0,0 +1,32 @@
+
+import {SlashCommandBuilder} from "@discordjs/builders";
+import {CommandInteraction} from "discord.js";
+import {Translations} from "../../core/Translations";
+import {ICommand} from "../ICommand";
+
+/**
+ * Display the link to invite the bot to another server
+ * @param {CommandInteraction} interaction
+ * @param {("fr"|"en")} language - Language to use in the response
+ */
+async function executeCommand(interaction: CommandInteraction, language: string) {
+	const tr = Translations.getModule("commands.invite", language);
+	await interaction.reply({ content: tr.get("main") });
+}
+
+export const commandInfo: ICommand = {
+	slashCommandBuilder: new SlashCommandBuilder()
+		.setName("invite")
+		.setDescription("Display the link to invite the bot to another server"),
+	executeCommand,
+	requirements: {
+		allowEffects: null,
+		requiredLevel: null,
+		disallowEffects: null,
+		guildPermissions: null,
+		guildRequired: null,
+		userPermission: null
+	},
+	mainGuildCommand: false,
+	slashCommandPermissions: null
+};
