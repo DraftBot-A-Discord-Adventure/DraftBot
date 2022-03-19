8ae71db9695a548d131397ce2df88ff75fbc3f43
Romain NEGRO <romain.negro@etu.univ-smb.fr>
2022-03-19 13:13:12 +0100

    refactor slash command SendDataCommand.ts #331

---
+++ src/commands/admin/SendDataCommand.ts
@@ -0,0 +1,34 @@
+import {ICommand} from "../ICommand";
+import {SlashCommandBuilder} from "@discordjs/builders";
+import {Constants} from "../../core/Constants";
+import {CommandInteraction} from "discord.js";
+
+/**
+ * Send database
+ * @param interaction
+ */
+async function executeCommand(interaction: CommandInteraction): Promise<void> {
+	await interaction.reply({
+		files: [{
+			attachment: "database/database.sqlite",
+			name: "database.sqlite"
+		}]
+	});
+}
+
+export const commandInfo: ICommand = {
+	slashCommandBuilder: new SlashCommandBuilder()
+		.setName("senddata")
+		.setDescription("Sends the database (admin only)"),
+	executeCommand,
+	requirements: {
+		allowEffects: null,
+		requiredLevel: null,
+		disallowEffects: null,
+		guildPermissions: null,
+		guildRequired: null,
+		userPermission: Constants.ROLES.USER.BOT_OWNER
+	},
+	mainGuildCommand: true,
+	slashCommandPermissions: null
+};
