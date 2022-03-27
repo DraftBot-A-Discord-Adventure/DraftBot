776a38d44577affef6710851058b22fa1730fef1
Romain NEGRO <romain.negro@etu.univ-smb.fr>
2022-03-27 14:49:12 +0200

    GuildShelterCommand.ts slash command #331

---
+++ src/commands/guild/GuildShelterCommand.ts
@@ -0,0 +1,31 @@
+import {Entity} from "../../core/models/Entity";
+import {DraftBotShelterMessageBuilder} from "../../core/messages/DraftBotShelterMessage";
+import {Guilds} from "../../core/models/Guild";
+import {ICommand} from "../ICommand";
+import {Constants} from "../../core/Constants";
+import {CommandRegisterPriority} from "../CommandRegisterPriority";
+import {SlashCommandBuilder} from "@discordjs/builders";
+import {CommandInteraction} from "discord.js";
+
+async function executeCommand(interaction: CommandInteraction, language: string, entity: Entity): Promise<void> {
+	const guild = await Guilds.getById(entity.Player.guildId);
+	await interaction.reply({embeds: [await new DraftBotShelterMessageBuilder(guild, language).build()]});
+}
+
+export const commandInfo: ICommand = {
+	slashCommandBuilder: new SlashCommandBuilder()
+		.setName("guildshelter")
+		.setDescription("Displays the shelter of your guild"),
+	executeCommand,
+	requirements: {
+		allowEffects: null,
+		requiredLevel: null,
+		disallowEffects: [Constants.EFFECT.BABY, Constants.EFFECT.DEAD],
+		guildPermissions: null,
+		guildRequired: true,
+		userPermission: null
+	},
+	mainGuildCommand: false,
+	slashCommandPermissions: null,
+	registerPriority: CommandRegisterPriority.TESTING
+};
