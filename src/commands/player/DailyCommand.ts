a8766bce803f0beb1f3f309a697c450c4f75ab46
Romain NEGRO <romain.negro@etu.univ-smb.fr>
2022-04-15 18:58:50 +0200

    daily command first refactor #331

--- src/commands/player/DailyCommand.ts
+++ src/commands/player/DailyCommand.ts
@@ -9,40 +9,52 @@ import {sendBlockedErrorInteraction, sendErrorMessage} from "../../core/utils/Er
 import {hoursToMinutes, millisecondsToHours, minutesDisplay} from "../../core/utils/TimeUtils";
 import ObjectItem from "../../core/models/ObjectItem";
 import {Data} from "../../core/Data";
-import {Translations} from "../../core/Translations";
+import {TranslationModule, Translations} from "../../core/Translations";
 import {DailyConstants} from "../../core/constants/DailyConstants";
 import {SlashCommandBuilder} from "@discordjs/builders";
 
-
 /**
- * Allow to use the object if the player has one in the dedicated slot of his inventory
+ * Checks if you have a right object you can daily with, and if you don't, sends an error
+ * @param activeObject
  * @param interaction
- * @param {("fr"|"en")} language - Language to use in the response
- * @param entity
+ * @param language
+ * @param dailyModule
  */
-async function executeCommand(interaction: CommandInteraction, language: string, entity: Entity) {
-	if (await sendBlockedErrorInteraction(interaction, language)) {
-		return;
-	}
-	const inventoryModelData = Data.getModule("models.inventories");
-	const dailyModule = Translations.getModule("commands.daily", language);
-
-	const activeObject: ObjectItem = await entity.Player.getMainObjectSlot().getItem() as ObjectItem;
-
-	const time = millisecondsToHours(interaction.createdAt.valueOf() - entity.Player.InventoryInfo.lastDailyAt.valueOf());
-
+function isWrongObjectForDaily(activeObject: ObjectItem, interaction: CommandInteraction, language: string, dailyModule: TranslationModule) {
 	if (activeObject.nature === Constants.NATURE.NONE) {
-		if (activeObject.id !== inventoryModelData.getNumber("objectId")) {
-			// there is a object that do nothing in the inventory
+		if (activeObject.id !== Data.getModule("models.inventories").getNumber("objectId")) {
+			// there is an object that do nothing in the inventory
 			sendErrorMessage(interaction.user, interaction.channel, language, dailyModule.get("objectDoNothingError"), false, interaction);
-			return;
+			return true;
 		}
-
 		// there is no object in the inventory
 		sendErrorMessage(interaction.user, interaction.channel, language, dailyModule.get("noActiveObjectdescription"), false, interaction);
-		return;
+		return true;
 	}
+	if (activeObject.nature in [Constants.NATURE.SPEED, Constants.NATURE.DEFENSE, Constants.NATURE.ATTACK]) {
+		// Those objects are active only during fights
+		sendErrorMessage(
+			interaction.user,
+			interaction.channel,
+			language,
+			dailyModule.get("objectIsActiveDuringFights"),
+			false,
+			interaction
+		);
+		return true;
+	}
+	return false;
+}
 
+/**
+ * Checks if you can do your daily, and if you can't, sends an error
+ * @param interaction
+ * @param entity
+ * @param language
+ * @param dailyModule
+ */
+function dailyNotReady(interaction: CommandInteraction, entity: Entity, language: string, dailyModule: TranslationModule) {
+	const time = millisecondsToHours(interaction.createdAt.valueOf() - entity.Player.InventoryInfo.lastDailyAt.valueOf());
 	if (time < DailyConstants.TIME_BETWEEN_DAILIES) {
 		sendErrorMessage(
 			interaction.user,
@@ -55,52 +67,53 @@ async function executeCommand(interaction: CommandInteraction, language: string,
 			false,
 			interaction
 		);
+		return true;
+	}
+	return false;
+}
+
+/**
+ * Activate your daily item effect
+ * @param interaction
+ * @param {("fr"|"en")} language - Language to use in the response
+ * @param entity
+ */
+async function executeCommand(interaction: CommandInteraction, language: string, entity: Entity) {
+	if (await sendBlockedErrorInteraction(interaction, language)) {
+		return;
+	}
+	const dailyModule = Translations.getModule("commands.daily", language);
+
+	const activeObject: ObjectItem = await entity.Player.getMainObjectSlot().getItem() as ObjectItem;
+
+	if (isWrongObjectForDaily(activeObject, interaction, language, dailyModule) || dailyNotReady(interaction, entity, language, dailyModule)) {
 		return;
 	}
 
 	const embed = new DraftBotEmbed().formatAuthor(dailyModule.get("dailySuccess"), interaction.user);
 
-	if (activeObject.nature === Constants.NATURE.HEALTH) {
-		embed.setDescription(
-			dailyModule.format("healthDaily", {value: activeObject.power})
-		);
+	switch (activeObject.nature) {
+	case Constants.NATURE.HEALTH:
+		embed.setDescription(dailyModule.format("healthDaily", {value: activeObject.power}));
 		await entity.addHealth(activeObject.power, interaction.channel, language);
-		entity.Player.InventoryInfo.updateLastDailyAt();
-	}
-	if (
-		activeObject.nature === Constants.NATURE.SPEED ||
-		activeObject.nature === Constants.NATURE.DEFENSE ||
-		activeObject.nature === Constants.NATURE.ATTACK
-	) {
-		// Those objects are active only during fights
-		sendErrorMessage(
-			interaction.user,
-			interaction.channel,
-			language,
-			dailyModule.get("objectIsActiveDuringFights"),
-			false,
-			interaction
-		);
-		return;
-	}
-	if (activeObject.nature === Constants.NATURE.HOSPITAL) {
+		break;
+	case Constants.NATURE.HOSPITAL:
 		embed.setDescription(
 			dailyModule.format("hospitalBonus", {
 				value: minutesDisplay(hoursToMinutes(activeObject.power))
 			})
 		);
 		Maps.advanceTime(entity.Player, hoursToMinutes(activeObject.power));
-		await entity.Player.save();
-		entity.Player.InventoryInfo.updateLastDailyAt();
-	}
-	if (activeObject.nature === Constants.NATURE.MONEY) {
-		embed.setDescription(
-			dailyModule.format("moneyBonus", {value: activeObject.power})
-		);
+		break;
+	case Constants.NATURE.MONEY:
+		embed.setDescription(dailyModule.format("moneyBonus", {value: activeObject.power}));
 		await entity.Player.addMoney(entity, activeObject.power, interaction.channel, language);
-		entity.Player.InventoryInfo.updateLastDailyAt();
+		break;
+	default:
+		break;
 	}
 
+	entity.Player.InventoryInfo.updateLastDailyAt();
 	await Promise.all([entity.save(), entity.Player.save(), entity.Player.InventoryInfo.save()]);
 	// TODO REFACTOR LES LOGS
 	// log(entity.discordUserId + " used his daily item " + activeObject.en);
