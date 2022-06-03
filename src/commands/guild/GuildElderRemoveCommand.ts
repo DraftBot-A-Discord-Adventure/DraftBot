770e9d327e849627d474f59107b9d083d024452f
Romain NEGRO <romain.negro@etu.univ-smb.fr>
2022-06-03 19:04:54 +0200

    guildElderRemove command slash command #331

---
+++ src/commands/guild/GuildElderRemoveCommand.ts
@@ -0,0 +1,92 @@
+import {DraftBotEmbed} from "../../core/messages/DraftBotEmbed";
+import {Entity} from "../../core/models/Entity";
+import {Guild, Guilds} from "../../core/models/Guild";
+import {BlockingUtils} from "../../core/utils/BlockingUtils";
+import {ICommand} from "../ICommand";
+import {Constants} from "../../core/Constants";
+import {CommandInteraction} from "discord.js";
+import {SlashCommandBuilder} from "@discordjs/builders";
+import {sendErrorMessage} from "../../core/utils/ErrorUtils";
+import {TranslationModule, Translations} from "../../core/Translations";
+import {DraftBotValidateReactionMessage} from "../../core/messages/DraftBotValidateReactionMessage";
+
+/**
+ * @param entity
+ * @param guild
+ * @param guildElderRemoveModule
+ * @param interaction
+ */
+function getEndCallbackElderRemoveValidation(entity: Entity, guild: Guild, guildElderRemoveModule: TranslationModule, interaction: CommandInteraction) {
+	return async (msg: DraftBotValidateReactionMessage) => {
+		BlockingUtils.unblockPlayer(entity.discordUserId);
+		if (msg.isValidated()) {
+			guild.elderId = null;
+			await Promise.all([guild.save()]);
+
+			const confirmEmbed = new DraftBotEmbed()
+				.setAuthor(
+					guildElderRemoveModule.get("successElderRemoveTitle"),
+					interaction.user.displayAvatarURL()
+				)
+				.setDescription(
+					guildElderRemoveModule.get("successElderRemove")
+				);
+			interaction.followUp({embeds: [confirmEmbed]});
+			return;
+		}
+		// Cancel the creation
+		sendErrorMessage(interaction.user, interaction.channel, guildElderRemoveModule.language, guildElderRemoveModule.get("elderRemoveCancelled"), true);
+	};
+}
+
+/**
+ * remove guild elder
+ * @param interaction
+ * @param {("fr"|"en")} language - Language to use in the response
+ * @param entity
+ */
+async function executeCommand(interaction: CommandInteraction, language: string, entity: Entity): Promise<void> {
+	const guild = await Guilds.getById(entity.Player.guildId);
+	const guildElderRemoveModule = Translations.getModule("commands.guildElderRemove", language);
+
+	if (guild.elderId === null) {
+		// trying to remove an elder that does not exist
+		sendErrorMessage(
+			interaction.user,
+			interaction.channel,
+			language,
+			guildElderRemoveModule.get("noElderToRemove"),
+			false,
+			interaction
+		);
+		return;
+	}
+
+	const elderRemoveEmbed = new DraftBotValidateReactionMessage(interaction.user, getEndCallbackElderRemoveValidation(entity, guild, guildElderRemoveModule, interaction))
+		.formatAuthor(guildElderRemoveModule.get("elderRemoveTitle"), interaction.user)
+		.setDescription(
+			guildElderRemoveModule.format("elderRemove",
+				{
+					guildName: guild.name
+				}
+			)
+		) as DraftBotValidateReactionMessage;
+
+	await elderRemoveEmbed.reply(
+		interaction,
+		(collector) => BlockingUtils.blockPlayerWithCollector(entity.discordUserId, "guildElderRemove", collector)
+	);
+}
+
+export const commandInfo: ICommand = {
+	slashCommandBuilder: new SlashCommandBuilder()
+		.setName("guildelderremove")
+		.setDescription("Remove the elder of your guild"),
+	executeCommand,
+	requirements: {
+		disallowEffects: [Constants.EFFECT.BABY, Constants.EFFECT.DEAD],
+		guildRequired: true,
+		guildPermissions: Constants.GUILD.PERMISSION_LEVEL.CHIEF
+	},
+	mainGuildCommand: false
+};
