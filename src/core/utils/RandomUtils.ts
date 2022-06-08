8241589ca068b771bc64f4daeb06f5ada7618ddc
Romain NEGRO <romain.negro@etu.univ-smb.fr>
2022-06-08 15:01:47 +0200

    doc randint

--- src/core/utils/RandomUtils.ts
+++ src/core/utils/RandomUtils.ts
@@ -3,5 +3,11 @@ import {Random} from "random-js/dist/Random";
 export class RandomUtils {
 	public static draftbotRandom: Random = new (require("random-js")).Random();
 
+	/**
+	 * Generates a random number between min included and max excluded
+	 * @param min
+	 * @param max
+	 * @returns a random number between min included and max excluded
+	 */
 	public static randInt = (min: number, max: number) => RandomUtils.draftbotRandom.integer(min, max - 1);
 }
