a4423b12dc3987739d5b2c11ee54e6d5f0b7fe40
Romain NEGRO <romain.negro@etu.univ-smb.fr>
2022-06-22 23:58:33 +0200

    fix #1022 + #951 migration

--- database/migrations/026-fixes-3-0-0.sql
+++ database/migrations/026-fixes-3-0-0.sql
@@ -1,5 +1,7 @@
 -- Up
 
-UPDATE pet_entities SET lovepoints=0 WHERE lovepoints < 0;
+UPDATE pet_entities SET lovepoints = 0 WHERE lovepoints < 0;
+UPDATE player_missions_info SET campaignProgression = 1 WHERE campaignProgression = 2
+UPDATE players SET money = CEIL(money)
 
 -- Down
