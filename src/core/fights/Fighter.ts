bcad83444874dba74ba53731bd328d91e5acf176
Feiryn <98115666+feiryn@users.noreply.github.com>
2022-06-23 00:39:43 +0200

    Removed unnecessary await

--- src/core/fights/Fighter.ts
+++ src/core/fights/Fighter.ts
@@ -100,7 +100,7 @@ export class Fighter {
 		}
 		await this.entity.Player.drinkPotion();
 		await MissionsController.update(this.entity.discordUserId, channel, language, "drinkPotion");
-		const potionSlot = await this.entity.Player.getMainPotionSlot();
+		const potionSlot = this.entity.Player.getMainPotionSlot();
 		if (potionSlot) {
 			await MissionsController.update(this.entity.discordUserId, channel, language, "drinkPotionRarity", 1, { rarity: (await potionSlot.getItem()).rarity });
 		}
