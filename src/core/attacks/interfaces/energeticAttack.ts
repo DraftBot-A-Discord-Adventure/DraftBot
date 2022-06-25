bcdbf2d93780e7f8eb2a9fcca16feb870d5eb1e4
BastLast <bastien.last87@gmail.com>
2022-06-25 02:39:02 +0200

    Ajout de l'attaque énergique #299

--- src/core/attacks/interfaces/energeticAttack.ts
+++ src/core/attacks/interfaces/energeticAttack.ts
@@ -12,25 +12,19 @@ type statsInfo = { attackerStats: number[], defenderStats: number[], statsEffect
 export const fightActionInterface: IFightAction = {
 	use(sender: Fighter, receiver: Fighter, language: string): string {
 		const initialDamage = FightActionController.getAttackDamage(this.getStatsInfo(sender, receiver), sender.getPlayerLevel(), this.getAttackInfo());
-		const damageDealt = FightActionController.applySecondaryEffects(initialDamage, 5, 10);
-
+		const damageDealt = FightActionController.applySecondaryEffects(initialDamage, 5, 5);
 		receiver.stats.fightPoints -= damageDealt;
 
-		let sideEffects = "";
 		const attackTranslationModule = Translations.getModule("commands.fight", language);
 
-
-		// 25% chance to lower the target's defense by 10%
-		if (Math.random() < 0.25) {
-			const reductionAmont = 10;
-			receiver.stats.defense = Math.round(receiver.stats.defense - receiver.stats.defense * reductionAmont / 100);
-			sideEffects = attackTranslationModule.format("actions.sideEffects.defense", {
-				adversary: FightConstants.TARGET.OPPONENT,
-				operator: "-",
-				amount: reductionAmont
-			});
-		}
-
+		// half of the damage is converted to fight points
+		const healAmount = Math.round(damageDealt / 2);
+		sender.stats.fightPoints += healAmount;
+		const sideEffects = attackTranslationModule.format("actions.sideEffects.energy", {
+			adversary: FightConstants.TARGET.SELF,
+			operator: "+",
+			amount: healAmount
+		});
 
 		const attackStatus = this.getAttackStatus(damageDealt, initialDamage);
 		const chosenString = attackTranslationModule.getRandom(`actions.attacksResults.${attackStatus}`);
@@ -52,11 +46,11 @@ export const fightActionInterface: IFightAction = {
 	},
 
 	getName(): string {
-		return "piercingAttack";
+		return "energeticAttack";
 	},
 
 	getAttackInfo(): attackInfo {
-		return {minDamage: 50, averageDamage: 100, maxDamage: 125};
+		return {minDamage: 30, averageDamage: 60, maxDamage: 90};
 	},
 
 	getStatsInfo(sender: Fighter, receiver: Fighter): statsInfo {
@@ -70,8 +64,8 @@ export const fightActionInterface: IFightAction = {
 				receiver.stats.agility,
 				receiver.stats.speed
 			], statsEffect: [
-				0.6,
-				0.2,
+				0.7,
+				0.1,
 				0.2
 			]
 		};
