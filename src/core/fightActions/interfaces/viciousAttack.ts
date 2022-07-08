7454eeed5ceae96c0a1e0f9225dcd880b5f6a3b9
BastLast <bastien.last87@gmail.com>
2022-07-08 23:56:35 +0200

    Ajout de l'attaque fourbe #299

---
+++ src/core/fightActions/interfaces/viciousAttack.ts
@@ -0,0 +1,82 @@
+import {IFightAction} from "../IFightAction";
+import {Fighter} from "../../fights/Fighter";
+import {Translations} from "../../Translations";
+import {format} from "../../utils/StringFormatter";
+import {Data} from "../../Data";
+import {FightActionController} from "../FightActionController";
+import {FightConstants} from "../../constants/FightConstants";
+
+type attackInfo = { minDamage: number, averageDamage: number, maxDamage: number };
+type statsInfo = { attackerStats: number[], defenderStats: number[], statsEffect: number[] }
+
+export const fightActionInterface: IFightAction = {
+	use(sender: Fighter, receiver: Fighter, turn: number, language: string): string {
+		const initialDamage = FightActionController.getAttackDamage(this.getStatsInfo(sender, receiver), sender.getPlayerLevel(), this.getAttackInfo());
+		let damageDealt = FightActionController.applySecondaryEffects(initialDamage, 10, 10);
+
+		// plus l'attaque est utilisée et plus elle est utilisée tard et moins elle est efficace. (pénalité maximale de -80 %)
+		const count = sender.fightActionsHistory.filter(action => action === this.getName()).length + 2;
+		const ratio = (100 - turn * count) / 100;
+		damageDealt = Math.round(ratio < 0.2 ? 0.2 * damageDealt : damageDealt * ratio);
+
+		const attackTranslationModule = Translations.getModule("commands.fight", language);
+
+		damageDealt = Math.round(damageDealt);
+		receiver.stats.fightPoints -= damageDealt;
+
+		const attackStatus = this.getAttackStatus(damageDealt, initialDamage);
+		const chosenString = attackTranslationModule.getRandom(`actions.attacksResults.${attackStatus}`);
+		return format(chosenString, {
+			attack: Translations.getModule("fightactions." + this.getName(), language)
+				.get("name")
+				.toLowerCase()
+		}) + Translations.getModule("commands.fight", language).format("actions.damages", {
+			damages: damageDealt
+		});
+	},
+
+	toString(language: string): string {
+		return Translations.getModule(`fightactions.${this.getName()}`, language).get("name");
+	},
+
+	getEmoji(): string {
+		return Data.getModule(`fightactions.${this.getName()}`).getString("emote");
+	},
+
+	getName(): string {
+		return "viciousAttack";
+	},
+
+	getAttackInfo(): attackInfo {
+		return {minDamage: 50, averageDamage: 200, maxDamage: 350};
+	},
+
+	getStatsInfo(sender: Fighter, receiver: Fighter): statsInfo {
+		return {
+			attackerStats: [
+				300,
+				sender.stats.attack,
+				sender.stats.agility,
+				sender.stats.speed
+			], defenderStats: [
+				sender.stats.fightPoints,
+				receiver.stats.defense,
+				receiver.stats.agility,
+				receiver.stats.speed
+			], statsEffect: [
+				0.5,
+				0.2,
+				0.1,
+				0.2
+			]
+		};
+	},
+
+	getAttackStatus(damageDealt: number, initialDamage: number) {
+		return damageDealt > initialDamage
+			? FightConstants.ATTACK_STATUS.CRITICAL
+			: damageDealt < initialDamage * 0.7
+				? FightConstants.ATTACK_STATUS.MISSED
+				: FightConstants.ATTACK_STATUS.NORMAL;
+	}
+};
