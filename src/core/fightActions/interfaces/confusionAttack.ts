ef159bda739340098bb4e22f23900882f849910d
BastLast <bastien.last87@gmail.com>
2022-07-10 16:57:59 +0200

    ajout de l'attaque troublante #299

---
+++ src/core/fightActions/interfaces/confusionAttack.ts
@@ -0,0 +1,82 @@
+import {IFightAction} from "../IFightAction";
+import {Fighter} from "../../fights/Fighter";
+import {Translations} from "../../Translations";
+import {format} from "../../utils/StringFormatter";
+import {Data} from "../../Data";
+import {FightActionController} from "../FightActionController";
+import {FightConstants} from "../../constants/FightConstants";
+import {FighterAlterationId} from "../../fights/FighterAlterationId";
+
+type attackInfo = { minDamage: number, averageDamage: number, maxDamage: number };
+type statsInfo = { attackerStats: number[], defenderStats: number[], statsEffect: number[] }
+
+export const fightActionInterface: IFightAction = {
+	use(sender: Fighter, receiver: Fighter, turn: number, language: string): string {
+		const initialDamage = FightActionController.getAttackDamage(this.getStatsInfo(sender, receiver), sender.getPlayerLevel(), this.getAttackInfo());
+		let damageDealt = FightActionController.applySecondaryEffects(initialDamage, 5, 10);
+
+		const attackTranslationModule = Translations.getModule("commands.fight", language);
+
+		let sideEffects = "";
+
+		const alteration = receiver.newAlteration(FighterAlterationId.CONFUSED);
+		if (alteration === FighterAlterationId.CONFUSED) {
+			sideEffects = attackTranslationModule.format("actions.sideEffects.newAlteration", {
+				adversary: FightConstants.TARGET.OPPONENT,
+				effect: attackTranslationModule.get("effects.confused").toLowerCase()
+			});
+		}
+
+		damageDealt = Math.round(damageDealt);
+		receiver.stats.fightPoints -= damageDealt;
+
+		const attackStatus = this.getAttackStatus(damageDealt, initialDamage);
+		const chosenString = attackTranslationModule.getRandom(`actions.attacksResults.${attackStatus}`);
+		return format(chosenString, {
+			attack: Translations.getModule("fightactions." + this.getName(), language)
+				.get("name")
+				.toLowerCase()
+		}) + sideEffects + Translations.getModule("commands.fight", language).format("actions.damages", {
+			damages: damageDealt
+		});
+	},
+
+	toString(language: string): string {
+		return Translations.getModule(`fightactions.${this.getName()}`, language).get("name");
+	},
+
+	getEmoji(): string {
+		return Data.getModule(`fightactions.${this.getName()}`).getString("emote");
+	},
+
+	getName(): string {
+		return "confusionAttack";
+	},
+
+	getAttackInfo(): attackInfo {
+		return {minDamage: 10, averageDamage: 20, maxDamage: 45};
+	},
+
+	getStatsInfo(sender: Fighter, receiver: Fighter): statsInfo {
+		return {
+			attackerStats: [
+				sender.stats.attack,
+				sender.stats.speed
+			], defenderStats: [
+				receiver.stats.defense,
+				receiver.stats.speed
+			], statsEffect: [
+				0.7,
+				0.3
+			]
+		};
+	},
+
+	getAttackStatus(damageDealt: number, initialDamage: number) {
+		return damageDealt > initialDamage
+			? FightConstants.ATTACK_STATUS.CRITICAL
+			: damageDealt < initialDamage
+				? FightConstants.ATTACK_STATUS.MISSED
+				: FightConstants.ATTACK_STATUS.NORMAL;
+	}
+};
