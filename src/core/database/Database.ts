72915f529453473f27721a4820b2ad716ffdf767
BastLast <bastien.last87@gmail.com>
2022-08-28 00:38:27 +0200

    bugfix génération de la database #546

--- src/core/database/Database.ts
+++ src/core/database/Database.ts
@@ -57,7 +57,7 @@ export abstract class Database {
 			password: botConfig.MARIADB_ROOT_PASSWORD
 		});
 		await mariadbConnection.execute(`CREATE DATABASE IF NOT EXISTS draftbot_${this.databaseName} CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;`);
-		await mariadbConnection.execute(`GRANT ALL PRIVILEGES ON draftbot_${this.databaseName}.* TO '${botConfig.MARIADB_USER}';`);
+		await mariadbConnection.execute(`GRANT ALL PRIVILEGES ON draftbot_${this.databaseName}.* TO '${botConfig.MARIADB_USER}'@${botConfig.MARIADB_HOST};`);
 		await mariadbConnection.end();
 
 		this.sequelize = new Sequelize(`draftbot_${this.databaseName}`, botConfig.MARIADB_USER, botConfig.MARIADB_PASSWORD, {
