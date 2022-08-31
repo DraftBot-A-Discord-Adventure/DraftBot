d5d11ff95a4f63aacbfbeae0ba3bae625f30cd7d
romain22222 <romrom22222@hotmail.fr>
2022-09-01 00:06:58 +0200

    Data.ts removal refactor part 6 : nettoyage d'items

--- src/core/constants/InventoryConstants.ts
+++ src/core/constants/InventoryConstants.ts
@@ -2,4 +2,6 @@ export abstract class InventoryConstants {
 	static readonly POTION_DEFAULT_ID = 0;
 
 	static readonly OBJECT_DEFAULT_ID = 0;
+
+	static readonly ITEMS_MAPPER = [1, 1.5, 2.1, 2.8, 3.6, 4.5, 5.5, 6.6, 6.7]
 }
