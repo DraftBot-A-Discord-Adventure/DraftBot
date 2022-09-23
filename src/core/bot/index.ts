7a2726b2c95fa26ae312718fde6317e9416b08e3
romain22222 <romrom22222@hotmail.fr>
2022-09-24 00:41:53 +0200

    intent clarification #1385

--- src/core/bot/index.ts
+++ src/core/bot/index.ts
@@ -68,11 +68,21 @@ async function main(): Promise<void> {
 			intents: [
 				IntentsBitField.Flags.Guilds, // We need it for roles
 				IntentsBitField.Flags.GuildMembers, // For tops
+				// IntentsBitField.Flags.GuildBans, // We do not need to ban anyone
+				// IntentsBitField.Flags.GuildEmojisAndStickers, // We do not need to manage emojis nor stickers
 				IntentsBitField.Flags.GuildIntegrations,
+				// IntentsBitField.Flags.GuildWebhooks, // We do not need webhook
+				// IntentsBitField.Flags.GuildInvites, // We do not need to manage guild invites
+				// IntentsBitField.Flags.GuildVoiceStates, // We do not need to manage vocals
+				// IntentsBitField.Flags.GuildPresences, // We do not need to manage presences
 				IntentsBitField.Flags.GuildMessages, // We need to receive, send, update and delete messages
 				IntentsBitField.Flags.GuildMessageReactions, // We need to add reactions
+				// IntentsBitField.Flags.GuildMessageTyping, // We do not need to see who's typing
 				IntentsBitField.Flags.DirectMessages, // We need to send and receive direct messages
 				IntentsBitField.Flags.DirectMessageReactions // We maybe need to receive direct messages reaction
+				// IntentsBitField.Flags.DirectMessageTyping, // We do not need to see who is currently writing to the bots dms
+				// IntentsBitField.Flags.MessageContent, // We do not need to manage other's message content
+				// IntentsBitField.Flags.GuildScheduledEvents // We do not need to see the guild's events
 			],
 			allowedMentions: {parse: ["users", "roles"]},
 			partials: [Partials.Message, Partials.Channel],
