c7856737edd1405d5a36e16f34ee15007c04d591
BastLast <lastaccount@fastmail.com>
2022-09-27 23:50:32 +0200

    guildCommand option translation #1375

--- src/commands/guild/GuildKickCommand.ts
+++ src/commands/guild/GuildKickCommand.ts
@@ -166,12 +166,24 @@ const currentCommandFrenchTranslations = Translations.getModule("commands.guildK
 const currentCommandEnglishTranslations = Translations.getModule("commands.guildKick", Constants.LANGUAGE.ENGLISH);
 export const commandInfo: ICommand = {
 	slashCommandBuilder: SlashCommandBuilderGenerator.generateBaseCommand(currentCommandFrenchTranslations, currentCommandEnglishTranslations)
-		.addUserOption(option => option.setName("user")
-			.setDescription("The user you want to kick of your guild")
+		.addUserOption(option => option.setName(currentCommandEnglishTranslations.get("optionUserName"))
+			.setNameLocalizations({
+				fr: currentCommandFrenchTranslations.get("optionUserName")
+			})
+			.setDescription(currentCommandEnglishTranslations.get("optionUserDescription"))
+			.setDescriptionLocalizations({
+				fr: currentCommandFrenchTranslations.get("optionUserDescription")
+			})
 			.setRequired(false)
 		)
-		.addNumberOption(option => option.setName("rank")
-			.setDescription("The rank of the player you want to kick of your guild")
+		.addNumberOption(option => option.setName(currentCommandEnglishTranslations.get("optionRankName"))
+			.setNameLocalizations({
+				fr: currentCommandFrenchTranslations.get("optionRankName")
+			})
+			.setDescription(currentCommandEnglishTranslations.get("optionRankDescription"))
+			.setDescriptionLocalizations({
+				fr: currentCommandFrenchTranslations.get("optionRankDescription")
+			})
 			.setRequired(false)
 		) as SlashCommandBuilder,
 	executeCommand,
