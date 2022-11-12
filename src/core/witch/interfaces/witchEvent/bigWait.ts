482dbbaea9a86ac2a2730a67a1b5e72208a116f0
Voltou <titou.lafond@gmail.com>
2022-11-13 00:00:22 +0100

    Ajout actions mini event sorcière #1330

---
+++ src/core/witch/interfaces/witchEvent/bigWait.ts
@@ -0,0 +1,35 @@
+import {WitchEvent} from "../../WitchEvent";
+import {Interaction} from "discord.js";
+import Player from "../../../database/game/models/Player";
+import {generateRandomPotion, giveItemToPlayer} from "../../../utils/ItemUtils";
+import {Constants} from "../../../Constants";
+import {InventorySlots} from "../../../database/game/models/InventorySlot";
+import {TravelTime} from "../../../maps/TravelTime";
+import {EffectsConstants} from "../../../constants/EffectsConstants";
+import {NumberChangeReason} from "../../../constants/LogsConstants";
+
+export default class BigWait extends WitchEvent {
+	async givePotion(interaction: Interaction, player: Player, language: string): Promise<void> {
+		const potionToGive = await generateRandomPotion(
+			Constants.ITEM_NATURE.NO_EFFECT);
+		await giveItemToPlayer(
+			player,
+			potionToGive,
+			language,
+			interaction.user,
+			interaction.channel,
+			await InventorySlots.getOfPlayer(player.id)
+		);
+	}
+
+	async giveEffect(player: Player): Promise<void> {
+		await TravelTime.applyEffect(
+			player,
+			EffectsConstants.EMOJI_TEXT.OCCUPIED,
+			60,
+			new Date(),
+			NumberChangeReason.SMALL_EVENT,
+			new Date()
+		);
+	}
+}
---
+++ src/core/witch/interfaces/witchEvent/crystalBall.ts
@@ -0,0 +1,37 @@
+import {WitchEvent} from "../../WitchEvent";
+import {Interaction} from "discord.js";
+import Player from "../../../database/game/models/Player";
+import {generateRandomPotion, giveItemToPlayer} from "../../../utils/ItemUtils";
+import {Constants} from "../../../Constants";
+import {InventorySlots} from "../../../database/game/models/InventorySlot";
+import {TravelTime} from "../../../maps/TravelTime";
+import {EffectsConstants} from "../../../constants/EffectsConstants";
+import {NumberChangeReason} from "../../../constants/LogsConstants";
+
+export default class CrystalBall extends WitchEvent {
+	async givePotion(interaction: Interaction, player: Player, language: string): Promise<void> {
+		const potionToGive = await generateRandomPotion(
+			Constants.ITEM_NATURE.TIME_SPEEDUP,
+			Constants.RARITY.RARE
+		);
+		await giveItemToPlayer(
+			player,
+			potionToGive,
+			language,
+			interaction.user,
+			interaction.channel,
+			await InventorySlots.getOfPlayer(player.id)
+		);
+	}
+
+	async giveEffect(player: Player): Promise<void> {
+		await TravelTime.applyEffect(
+			player,
+			EffectsConstants.EMOJI_TEXT.SCARED,
+			0,
+			new Date(),
+			NumberChangeReason.SMALL_EVENT,
+			new Date()
+		);
+	}
+}
