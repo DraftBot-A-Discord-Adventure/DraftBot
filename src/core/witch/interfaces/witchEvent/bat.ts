2050772eda10a092ed41f6a0fb33eda83dfa161e
BastLast <lastaccount@fastmail.com>
2022-11-12 20:36:05 +0100

    temp changes #1330

--- src/core/witch/interfaces/witchEvent/bat.ts
+++ src/core/witch/interfaces/witchEvent/bat.ts
@@ -2,17 +2,27 @@ import {WitchEvent} from "../../WitchEvent";
 import {RandomUtils} from "../../../utils/RandomUtils";
 import {Interaction} from "discord.js";
 import Player from "../../../database/game/models/Player";
-import {generateRandomPotion} from "../../../utils/ItemUtils";
+import {generateRandomPotion, giveItemToPlayer} from "../../../utils/ItemUtils";
 import {Constants} from "../../../Constants";
+import {InventorySlots} from "../../../database/game/models/InventorySlot";
 
 export default class Bat extends WitchEvent {
 	async givePotion(interaction: Interaction, player: Player, language: string): Promise<void> {
-		await player.giveItem(await generateRandomPotion(
+		const potionToGive = await generateRandomPotion(
 			RandomUtils.draftbotRandom.bool(0.625) ? Constants.ITEM_NATURE.SPEED : Constants.ITEM_NATURE.TIME_SPEEDUP,
-			Constants.RARITY.SPECIAL));
+			Constants.RARITY.SPECIAL);
+		await giveItemToPlayer(
+			player,
+			potionToGive,
+			language,
+			interaction.user,
+			interaction.channel,
+			await InventorySlots.getOfPlayer(player.id)
+		);
 	}
 
-	async giveEffect(interaction: Interaction, player: Player, language: string): Promise<void>{
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	async giveEffect(interaction: Interaction, player: Player, language: string): Promise<void> {
 		return await Promise.resolve();
 	}
 }
