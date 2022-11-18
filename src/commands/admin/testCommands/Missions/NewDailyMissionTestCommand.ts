52e9f2fcf1a5f8fffd955f4038fa447a70e3c7d9
Pietagorh <60353007+Pietagorh@users.noreply.github.com>
2022-11-18 14:46:07 +0100

    Fix #1591 (#1623)
    * Fix #1591
    
    * Deepsource fix
    
    * Deepsource fix 2 parce que j'suis trop con
    
    * Blblbl

--- src/commands/admin/testCommands/Missions/NewDailyMissionTestCommand.ts
+++ src/commands/admin/testCommands/Missions/NewDailyMissionTestCommand.ts
@@ -3,16 +3,15 @@ import {format} from "../../../../core/utils/StringFormatter";
 import {DraftBotMissionsMessageBuilder} from "../../../../core/messages/DraftBotMissionsMessage";
 import {Translations} from "../../../../core/Translations";
 import {getTomorrowMidnight} from "../../../../core/utils/TimeUtils";
-import PlayerMissionsInfo, {PlayerMissionsInfos} from "../../../../core/database/game/models/PlayerMissionsInfo";
-import {CommandInteraction} from "discord.js";
 import {ITestCommand} from "../../../../core/CommandsTest";
-import {Players} from "../../../../core/database/game/models/Player";
+import {Missions} from "../../../../core/database/game/models/Mission";
+import PlayerMissionsInfo from "../../../../core/database/game/models/PlayerMissionsInfo";
 
 export const commandInfo: ITestCommand = {
 	name: "newDailyMissions",
 	aliases: ["ndm"],
 	commandFormat: "",
-	messageWhenExecuted: "La mission quotidienne a changée !\n Mission : {mission}",
+	messageWhenExecuted: "La mission quotidienne a été changée !\n Mission : {mission}",
 	description: "Permet d'obtenir une nouvelle mission quotidienne",
 	commandTestShouldReply: true,
 	execute: null // defined later
@@ -22,18 +21,16 @@ export const commandInfo: ITestCommand = {
  * Set the weapon of the player
  * @return {String} - The successful message formatted
  */
-const newDailyMissionTestCommand = async (language: string, interaction: CommandInteraction): Promise<string> => {
-	const player = await Players.getByDiscordUserId(interaction.user.id);
-	const missionsInfo = await PlayerMissionsInfos.getOfPlayer(player.id);
+const newDailyMissionTestCommand = async (language: string): Promise<string> => {
 	const newDM = await DailyMissions.regenerateDailyMission();
 	await PlayerMissionsInfo.update({
 		dailyMissionNumberDone: 0,
-		lastDailyMissionCompleted: new Date(missionsInfo.lastDailyMissionCompleted.valueOf() - 86400000)
+		lastDailyMissionCompleted: new Date(0)
 	}, {where: {}});
 	return format(commandInfo.messageWhenExecuted, {
 		mission: DraftBotMissionsMessageBuilder.getMissionDisplay(
 			Translations.getModule("commands.missions", language),
-			await newDM.Mission.formatDescription(newDM.objective, newDM.variant, language, null),
+			await (await Missions.getById(newDM.missionId)).formatDescription(newDM.objective, newDM.variant, language, null),
 			getTomorrowMidnight(),
 			0,
 			newDM.objective
