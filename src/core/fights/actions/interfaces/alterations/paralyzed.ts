acba390453ad1d2b23f89948b4883833760ceecc
BastLast <lastaccount@fastmail.com>
2023-01-01 23:59:07 +0100

    suite d'équilibrage #1695

---
+++ src/core/fights/actions/interfaces/alterations/paralyzed.ts
@@ -0,0 +1,28 @@
+import {Fighter} from "../../../fighter/Fighter";
+import {Translations} from "../../../../Translations";
+import {FightAction} from "../../FightAction";
+import {FightActions} from "../../FightActions";
+
+export default class ParalyzedAlteration extends FightAction {
+	use(sender: Fighter, receiver: Fighter, turn: number, language: string): string {
+		sender.alterationTurn++;
+		const paralyzedTranslationModule = Translations.getModule(`fightactions.${this.name}`, language);
+		if (sender.alterationTurn > 2) { // this effect heals after two turns
+			sender.removeAlteration();
+			return paralyzedTranslationModule.get("inactive");
+		}
+
+		// 20% chance to not attack this turn
+		if (Math.random() < 0.2) {
+			sender.nextFightAction = FightActions.getNoAttack();
+			return paralyzedTranslationModule.get("noAttack");
+		}
+
+		if (!sender.hasSavedStats()) {
+			sender.saveStats();
+			sender.stats.speed = Math.round(0);
+			return paralyzedTranslationModule.get("new");
+		}
+		return paralyzedTranslationModule.get("active");
+	}
+}
