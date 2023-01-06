c1586a50848073bbe52ffc9bfa78047f9331de9c
BastLast <lastaccount@fastmail.com>
2023-01-06 01:03:46 +0100

    Ajout de l'attaque boomerang #1695

--- src/core/fights/actions/interfaces/boomerangAttack.ts
+++ src/core/fights/actions/interfaces/boomerangAttack.ts
@@ -4,48 +4,48 @@ import {format} from "../../../utils/StringFormatter";
 import {FightActionController} from "../FightActionController";
 import {PlayerFighter} from "../../fighter/PlayerFighter";
 import {attackInfo, FightAction, statsInfo} from "../FightAction";
+import {FightAlterations} from "../FightAlterations";
+import {FightConstants} from "../../../constants/FightConstants";
 
-export default class ViciousAttack extends FightAction {
+export default class BoomerangAttack extends FightAction {
 	use(sender: Fighter, receiver: Fighter, turn: number, language: string): string {
 		const initialDamage = FightActionController.getAttackDamage(this.getStatsInfo(sender, receiver), (sender as PlayerFighter).getPlayerLevel(), this.getAttackInfo());
-		let damageDealt = FightActionController.applySecondaryEffects(initialDamage, 10, 10);
-
-		// Plus l'attaque est utilisée et plus elle est utilisée tard et moins elle est efficace. (pénalité maximale de -70 %)
-		const ratio = (11 - turn * (sender.fightActionsHistory.filter(action => action instanceof ViciousAttack).length + 1)) / 10;
-		damageDealt = Math.round(ratio < 0.3 ? 0.3 * damageDealt : damageDealt * ratio);
+		const damageDealt = FightActionController.applySecondaryEffects(initialDamage, 15, 20);
 
 		const attackTranslationModule = Translations.getModule("commands.fight", language);
 
-		damageDealt = Math.round(damageDealt);
-		receiver.stats.fightPoints -= damageDealt;
+		let sideEffects = "";
 
+		const alteration = receiver.newAlteration(FightAlterations.TARGETED);
+		if (alteration === FightAlterations.TARGETED) {
+			sideEffects = attackTranslationModule.format("actions.sideEffects.newAlteration", {
+				adversary: FightConstants.TARGET.OPPONENT,
+				effect: attackTranslationModule.get("effects.targeted").toLowerCase()
+			});
+		}
+
+		receiver.stats.fightPoints -= damageDealt;
 		return format(attackTranslationModule.getRandom(`actions.attacksResults.${this.getAttackStatus(damageDealt, initialDamage)}`), {
 			attack: Translations.getModule(`fightactions.${this.name}`, language)
 				.get("name")
 				.toLowerCase()
-		}) + Translations.getModule("commands.fight", language).format("actions.damages", {
+		}) + sideEffects + Translations.getModule("commands.fight", language).format("actions.damages", {
 			damages: damageDealt
 		});
 	}
 
 	getAttackInfo(): attackInfo {
-		return {minDamage: 50, averageDamage: 200, maxDamage: 350};
+		return {minDamage: 45, averageDamage: 90, maxDamage: 150};
 	}
 
 	getStatsInfo(sender: Fighter, receiver: Fighter): statsInfo {
 		return {
 			attackerStats: [
-				300,
-				sender.stats.attack,
-				sender.stats.speed
+				sender.stats.attack
 			], defenderStats: [
-				sender.stats.fightPoints,
-				receiver.stats.defense,
-				receiver.stats.speed
+				receiver.stats.defense
 			], statsEffect: [
-				0.6,
-				0.2,
-				0.2
+				1
 			]
 		};
 	}
