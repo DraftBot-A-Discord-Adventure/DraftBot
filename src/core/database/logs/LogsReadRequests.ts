11c901f10316b2d89c50cd56c23f044ed116ef7a
Feiryn <98115666+feiryn@users.noreply.github.com>
2023-02-05 16:19:10 +0100

    BO3 #1568

--- src/core/database/logs/LogsReadRequests.ts
+++ src/core/database/logs/LogsReadRequests.ts
@@ -1,11 +1,13 @@
 import {LogsDailyPotions} from "./models/LogsDailyPotions";
 import {LogsClassicalShopBuyouts} from "./models/LogsClassicalShopBuyouts";
-import {Op} from "sequelize";
+import {HasOne, Op} from "sequelize";
 import {ShopItemType} from "../../constants/LogsConstants";
 import {LogsDatabase} from "./LogsDatabase";
 import {LogsPlayersPossibilities} from "./models/LogsPlayersPossibilities";
 import {LogsPossibilities} from "./models/LogsPossibilities";
 import {LogsPlayers} from "./models/LogsPlayers";
+import {getNextSundayMidnight} from "../../utils/TimeUtils";
+import {LogsFightsResults} from "./models/LogsFightsResults";
 
 /**
  * This class is used to read some information in the log database in case it is needed for gameplay purposes
@@ -77,5 +79,57 @@ export class LogsReadRequests {
 		});
 
 		return lastEvent ? new Date(lastEvent.date * 1000) : null;
+	}
+
+	/**
+	 * Get the fights of a player against another this week
+	 * @param playerDiscordId
+	 * @param opponentDiscordId
+	 */
+	static async getRankedFightsThisWeek(playerDiscordId: string, opponentDiscordId: string): Promise<{
+		notDraw: number,
+		draw: number
+	}> {
+		const fights = await LogsFightsResults.findAll({
+			where: {
+				[Op.or]: [
+					{
+						"$LogsPlayer1.discordId$": playerDiscordId,
+						"$LogsPlayer2.discordId$": opponentDiscordId
+					},
+					{
+						"$LogsPlayer1.discordId$": opponentDiscordId,
+						"$LogsPlayer2.discordId$": playerDiscordId
+					}
+				],
+				date: {
+					[Op.gt]: Math.floor((getNextSundayMidnight() - 7 * 24 * 60 * 60 * 1000) / 1000)
+				},
+				friendly: false
+			},
+			include: [{
+				model: LogsPlayers,
+				association: new HasOne(LogsFightsResults, LogsPlayers, { sourceKey: "player1Id", foreignKey: "id", as: "LogsPlayer1" })
+			}, {
+				model: LogsPlayers,
+				association: new HasOne(LogsFightsResults, LogsPlayers, { sourceKey: "player2Id", foreignKey: "id", as: "LogsPlayer2" })
+			}]
+		});
+
+		const ret = {
+			notDraw: 0,
+			draw: 0
+		};
+
+		for (const fight of fights) {
+			if (fight.winner === 0) {
+				ret.draw++;
+			}
+			else {
+				ret.notDraw++;
+			}
+		}
+
+		return ret;
 	}
 }
