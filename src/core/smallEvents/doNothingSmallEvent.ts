003af9ae4c94177d9fad0e23b49ee0aa5ab20955
Feiryn <98115666+feiryn@users.noreply.github.com>
2023-03-09 19:42:23 +0100

    Refactored doNothingSmallEvent

--- src/core/smallEvents/doNothingSmallEvent.ts
+++ src/core/smallEvents/doNothingSmallEvent.ts
@@ -1,29 +1,7 @@
-import {SmallEvent} from "./SmallEvent";
-import {CommandInteraction} from "discord.js";
-import {DraftBotEmbed} from "../messages/DraftBotEmbed";
-import {Translations} from "../Translations";
-import {Data} from "../Data";
-import Player from "../database/game/models/Player";
+import {SimpleTextSmallEvent} from "./SimpleTextSmallEvent";
 
-export const smallEvent: SmallEvent = {
-	/**
-	 * No restrictions on who can do it
-	 */
-	canBeExecuted(): Promise<boolean> {
-		return Promise.resolve(true);
-	},
+class DoNothingSmallEvent extends SimpleTextSmallEvent {
+	canBeExecuted = (): Promise<boolean> => Promise.resolve(true);
+}
 
-	/**
-	 * Do literally nothing, just shows the player he is doing its way
-	 * @param interaction
-	 * @param language
-	 * @param player
-	 * @param seEmbed
-	 */
-	async executeSmallEvent(interaction: CommandInteraction, language: string, player: Player, seEmbed: DraftBotEmbed): Promise<void> {
-		seEmbed.setDescription(
-			Data.getModule("smallEvents.doNothing").getString("emote") +
-			Translations.getModule("smallEvents.doNothing", language).getRandom("stories"));
-		await interaction.editReply({embeds: [seEmbed]});
-	}
-};
+export const smallEvent: DoNothingSmallEvent = new DoNothingSmallEvent("doNothing");
