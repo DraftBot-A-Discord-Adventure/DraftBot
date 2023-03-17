814bb3ac4f235db3e0875ddb4da6888593ecc2bd
BastLast <lastaccount@fastmail.com>
2023-03-17 18:49:19 +0100

    fix #1570

--- src/core/smallEvents/leagueRewardSmallEvent.ts
+++ src/core/smallEvents/leagueRewardSmallEvent.ts
@@ -1,9 +1,41 @@
 import {SmallEvent} from "./SmallEvent";
 import {CommandInteraction} from "discord.js";
 import {DraftBotEmbed} from "../messages/DraftBotEmbed";
-import {Translations} from "../Translations";
+import {TranslationModule, Translations} from "../Translations";
 import Player from "../database/game/models/Player";
+import League from "../database/game/models/League";
+import {getNextSaturdayMidnight, printTimeBeforeDate, todayIsSunday} from "../utils/TimeUtils";
+import {FightConstants} from "../constants/FightConstants";
 
+/**
+ * Load the description of the reward the player will have
+ * @param leagueRewardTranslationModule
+ * @param league
+ */
+function rewardListToString(leagueRewardTranslationModule: TranslationModule, league: League): string {
+	return leagueRewardTranslationModule.format("reward", {
+		money: league.getMoneyToAward(),
+		xp: league.getXPToAward()
+	});
+}
+
+async function generateEndMessage(leagueRewardTranslationModule: TranslationModule, player: Player, language: string) : Promise<string> {
+
+	if (todayIsSunday()) {
+		return leagueRewardTranslationModule.format("sunday", {});
+	}
+
+	const league = await player.getLeague();
+	const rewards = rewardListToString(leagueRewardTranslationModule, league);
+
+	return leagueRewardTranslationModule.format(
+		player.fightCountdown > FightConstants.FIGHT_COUNTDOWN_MAXIMAL_VALUE ? "notEnoughFight" : "endMessage",
+		{
+			league: league.toString(language),
+			rewards: rewards,
+			time: printTimeBeforeDate(getNextSaturdayMidnight())
+		});
+}
 
 export const smallEvent: SmallEvent = {
 	/**
@@ -21,8 +53,16 @@ export const smallEvent: SmallEvent = {
 	 * @param seEmbed
 	 */
 	async executeSmallEvent(interaction: CommandInteraction, language: string, player: Player, seEmbed: DraftBotEmbed): Promise<void> {
-		const playerLeague = player.getLeague();
-		const leagueRewardTranslationModule = Translations.getModule("smallEvents.leagueReward", language);
 
+		const leagueRewardTranslationModule = Translations.getModule("smallEvents.leagueReward", language);
+		const message = await generateEndMessage(leagueRewardTranslationModule, player, language);
+
+		seEmbed.setDescription(
+			seEmbed.data.description +
+			Translations.getModule("smallEventsIntros", language).getRandom("intro") +
+			leagueRewardTranslationModule.getRandom("intrigue") +
+			message
+		);
+		await interaction.editReply({embeds: [seEmbed]});
 	}
 };
