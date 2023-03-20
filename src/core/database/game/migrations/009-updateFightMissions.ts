54b5399ee9fa3c5d5948a8cc6e1f67a742498845
Feiryn <98115666+feiryn@users.noreply.github.com>
2023-03-20 01:13:34 +0100

    rankedFight and friendlyFight missions migrations down #1839

--- src/core/database/game/migrations/009-updateFightMissions.ts
+++ src/core/database/game/migrations/009-updateFightMissions.ts
@@ -40,5 +40,36 @@ export async function up({context}: { context: QueryInterface }): Promise<void> 
 }
 
 export async function down(): Promise<void> {
-	/* No down to do */
+	// Down campaign
+	await context.sequelize.query(`
+		UPDATE mission_slots
+		SET missionId = 'rankedFight',
+			missionObjective = 5,
+			gemsToWin = 4,
+			xpToWin = 250
+		WHERE missionId = 'reachGlory'
+		   AND expiresAt IS NULL
+           AND missionObjective = 100
+	`);
+	await context.sequelize.query(`
+		UPDATE mission_slots
+		SET missionId = 'rankedFight',
+			missionObjective = 10
+		WHERE missionId = 'anyFight'
+		  AND expiresAt IS NULL
+		  AND missionObjective = 5
+		  AND gemsToWin = 2
+		  AND xpToWin = 100
+	`);
+	await context.sequelize.query(`
+		UPDATE mission_slots
+		SET missionId = 'rankedFight',
+			missionObjective = 15,
+			xpToWin = 125
+		WHERE missionId = 'reachGlory'
+		   AND expiresAt IS NULL
+           AND missionObjective = 800
+	`);
+
+	// However we can't down secondary missions
 }
