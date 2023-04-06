8d752ff4e79b08e1750fcedb9e0c4618c089d260
BastLast <lastaccount@fastmail.com>
2023-04-06 23:30:04 +0200

    fix #1907

---
+++ src/commands/admin/testCommands/Player/changeClass.ts
@@ -0,0 +1,39 @@
+import {format} from "../../../../core/utils/StringFormatter";
+import {CommandInteraction} from "discord.js";
+import {Constants} from "../../../../core/Constants";
+import {ITestCommand} from "../../../../core/CommandsTest";
+import {Players} from "../../../../core/database/game/models/Player";
+import {draftBotInstance} from "../../../../core/bot";
+
+export const commandInfo: ITestCommand = {
+	name: "changeClass",
+	commandFormat: "<classId>",
+	typeWaited: {
+		classId: Constants.TEST_VAR_TYPES.INTEGER
+	},
+	messageWhenExecuted: "Vous avez maintenant la classe d'id : {classId} !",
+	description: "Change votre classe pour la classe d'id donnée.",
+	commandTestShouldReply: true,
+	execute: null // defined later
+};
+
+/**
+ * Add money to the player
+ * @param {("fr"|"en")} language - Language to use in the response
+ * @param interaction
+ * @param {String[]} args=[] - Additional arguments sent with the command
+ * @return {String} - The successful message formatted
+ */
+const changeClassTestCommand = async (language: string, interaction: CommandInteraction, args: string[]): Promise<string> => {
+	const [player] = await Players.getOrRegister(interaction.user.id);
+	const newClassId = parseInt(args[0], 10);
+	if (newClassId <= 0 || newClassId > 24) {
+		throw new Error("Erreur class : choisissez une class qui existe !");
+	}
+	player.class = newClassId;
+	draftBotInstance.logsDatabase.logPlayerClassChange(player.discordUserId, newClassId).then();
+	await player.save();
+	return format(commandInfo.messageWhenExecuted, {classId: newClassId});
+};
+
+commandInfo.execute = changeClassTestCommand;
