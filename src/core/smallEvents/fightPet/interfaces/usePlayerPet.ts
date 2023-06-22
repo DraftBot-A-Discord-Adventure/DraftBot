e25f5c9ce8fe30dc9a1ecf91caba100f96bde9a8
BastLast <lastaccount@fastmail.com>
2023-06-23 00:58:09 +0200

    more actions #1879

---
+++ src/core/smallEvents/fightPet/interfaces/usePlayerPet.ts
@@ -0,0 +1,20 @@
+import {FightPetAction} from "../FightPetAction";
+import Player from "../../../database/game/models/Player";
+import {FeralPet} from "../../../database/game/models/FeralPet";
+import {SmallEventConstants} from "../../../constants/SmallEventConstants";
+import {Pets} from "../../../database/game/models/Pet";
+import {RandomUtils} from "../../../utils/RandomUtils";
+
+/**
+ *  Allow the player to use his pet in the fight
+ */
+export default class UsePlayerPet extends FightPetAction {
+
+	public async applyOutcome(player: Player, feralPet: FeralPet): Promise<boolean> {
+		const playerPet = await Pets.getById(player.petId);
+		// chances are higher if the pet is rare
+		return RandomUtils.draftbotRandom.realZeroToOneInclusive() <= (
+			playerPet.rarity * SmallEventConstants.FIGHT_PET.USE_PLAYER_PET_PLAYER_PET_MULTIPLIER - feralPet.originalPet.rarity * SmallEventConstants.FIGHT_PET.USE_PLAYER_PET_FERAL_PET_MULTIPLIER
+		) / 100;
+	}
+}
