0259f1e2ae0dba5a30a956d6b111a444b3fd0f9e
Feiryn <98115666+feiryn@users.noreply.github.com>
2023-07-26 00:08:40 +0200

    Fix guild attack #2060

--- src/core/maps/MapCache.ts
+++ src/core/maps/MapCache.ts
@@ -1,6 +1,8 @@
 import {MapLink, MapLinks} from "../database/game/models/MapLink";
 import {MapConstants} from "../constants/MapConstants";
 import {RandomUtils} from "../utils/RandomUtils";
+import {LogsMapLinks} from "../database/logs/models/LogsMapLinks";
+import {Op, Sequelize} from "sequelize";
 
 export abstract class MapCache {
 	static boatEntryMapLinks: number[];
@@ -15,20 +17,39 @@ export abstract class MapCache {
 
 	static continentMapLinks: number[];
 
+	static logsPveIslandMapLinks: number[];
+
 	static async init(): Promise<void> {
 		// Boat links entry only
-		this.boatEntryMapLinks = (await MapLinks.getFromAttributeToAttribute(MapConstants.MAP_ATTRIBUTES.MAIN_CONTINENT, MapConstants.MAP_ATTRIBUTES.PVE_ISLAND_ENTRY))
+		const boatEntryMapLinksObjects = await MapLinks.getFromAttributeToAttribute(MapConstants.MAP_ATTRIBUTES.MAIN_CONTINENT, MapConstants.MAP_ATTRIBUTES.PVE_ISLAND_ENTRY);
+		this.boatEntryMapLinks = boatEntryMapLinksObjects
 			.map((mapLink) => mapLink.id);
 
 		// Boat links entry and exit
-		this.entryAndExitBoatMapLinks = (await MapLinks.getFromAttributeToAttribute(MapConstants.MAP_ATTRIBUTES.PVE_EXIT, MapConstants.MAP_ATTRIBUTES.CONTINENT1))
+		const entryAndExitBoatMapLinksObjects = await MapLinks.getFromAttributeToAttribute(MapConstants.MAP_ATTRIBUTES.PVE_EXIT, MapConstants.MAP_ATTRIBUTES.CONTINENT1);
+		this.entryAndExitBoatMapLinks = entryAndExitBoatMapLinksObjects
 			.map((mapLink) => mapLink.id)
 			.concat(this.boatEntryMapLinks);
 
 		// PVE island map links
-		this.pveIslandMapLinks = (await MapLinks.getFromAttributeToAttribute(MapConstants.MAP_ATTRIBUTES.PVE_ISLAND_ENTRY, MapConstants.MAP_ATTRIBUTES.PVE_ISLAND))
-			.concat(await MapLinks.getFromAttributeToAttribute(MapConstants.MAP_ATTRIBUTES.PVE_ISLAND, MapConstants.MAP_ATTRIBUTES.PVE_ISLAND))
+		const pveIslandMapLinksObjects = (await MapLinks.getFromAttributeToAttribute(MapConstants.MAP_ATTRIBUTES.PVE_ISLAND_ENTRY, MapConstants.MAP_ATTRIBUTES.PVE_ISLAND))
+			.concat(await MapLinks.getFromAttributeToAttribute(MapConstants.MAP_ATTRIBUTES.PVE_ISLAND, MapConstants.MAP_ATTRIBUTES.PVE_ISLAND));
+		this.pveIslandMapLinks = pveIslandMapLinksObjects
 			.map((pveLink) => pveLink.id);
+
+		// Get the logs equivalence
+		// First create a list of literal tuples (because I didn't find how to do it in sequelize)
+		const pveMapTuples = boatEntryMapLinksObjects
+			.concat(entryAndExitBoatMapLinksObjects)
+			.concat(pveIslandMapLinksObjects)
+			.map((mapLink) => Sequelize.literal(`(${mapLink.startMap},${mapLink.endMap})`));
+		this.logsPveIslandMapLinks = (await LogsMapLinks.findAll({
+			where: {
+				[Op.and]: [ // We need an and to be able to create a column with a custom name
+					Sequelize.where(Sequelize.literal("(start,end)"), { [Op.in]: pveMapTuples })
+				]
+			}
+		})).map((mapLink) => mapLink.id);
 
 		// All PVE links
 		this.allPveMapLinks = (await MapLinks.getFromAttributeToAttribute(MapConstants.MAP_ATTRIBUTES.MAIN_CONTINENT, MapConstants.MAP_ATTRIBUTES.PVE_ISLAND_ENTRY))
