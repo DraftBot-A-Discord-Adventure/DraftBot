dee654037262cc6ea33753e6767e8530b6f2aaf8
BastLast <lastaccount@fastmail.com>
2023-08-29 22:20:21 +0200

    small fixes #2115
    small fix
    
    small fix 2
    
    small fix 3

--- src/commands/admin/testCommands/Event/ListEventsTestCommand.ts
+++ src/commands/admin/testCommands/Event/ListEventsTestCommand.ts
@@ -15,13 +15,14 @@ export const commandInfo: ITestCommand = {
 };
 
 /**
- * Reset the player
+ * List all available events for each map
  * @param {("fr"|"en")} language - Language to use in the response
  * @param interaction
  * @return {String} - The successful message formatted
  */
-const destroyPlayerTestCommand = async (language: string, interaction: CommandInteraction): Promise<string> => {
+const listEventsTestCommand = async (language: string, interaction: CommandInteraction): Promise<string> => {
 	const [player] = await Players.getOrRegister(interaction.user.id);
+	let str = "";
 	// Let's display for every map all available events
 	for (const map of await Maps.getMaps()) {
 		const events = await BigEventsController.getAvailableEvents(map.id, player);
@@ -29,11 +30,18 @@ const destroyPlayerTestCommand = async (language: string, interaction: CommandIn
 		for (const event of events) {
 			eventsString += `${event.id}\n`;
 		}
-		interaction.channel.send({
-			content: `**${map.nameFr}**\n${eventsString}`
-		});
+		str += `**${map.nameFr}**\n${eventsString}\n`;
+		if (str.length > 1800) { // Just to avoid the 2000 char limit of discord
+			await interaction.channel.send({
+				content: str
+			});
+			str = "";
+		}
 	}
+	await interaction.channel.send({
+		content: str
+	});
 	return commandInfo.messageWhenExecuted;
 };
 
-commandInfo.execute = destroyPlayerTestCommand;
+commandInfo.execute = listEventsTestCommand;
