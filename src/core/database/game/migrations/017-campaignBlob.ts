80fd78a64f5517af71a59e37c050621eae64d3e1
romain22222 <romrom22222@hotmail.fr>
2023-10-18 00:09:48 +0200

    prefix: hardcoding the initial blob size to avoid problems with unmatched campaign size

--- src/core/database/game/migrations/017-campaignBlob.ts
+++ src/core/database/game/migrations/017-campaignBlob.ts
@@ -4,11 +4,7 @@ export async function up({context}: { context: QueryInterface }): Promise<void> 
 	await context.addColumn("player_missions_info", "campaignBlob", {
 		type: DataTypes.STRING
 	});
-	// Get how many campaign missions there are
-	const campaignMissions = require("../../../../../../resources/text/campaign.json").missions.length;
-	console.log(`There are ${campaignMissions} campaign missions`);
-	// Set the campaignBlob to a list of 1 of size campaignProgression for each player. The rest is a list of 0 of how many campaign missions there are
-	await context.sequelize.query(`UPDATE player_missions_info SET campaignBlob = CONCAT(REPEAT("1", campaignProgression-1), REPEAT("0", ${campaignMissions}-campaignProgression+1))`);
+	await context.sequelize.query("UPDATE player_missions_info SET campaignBlob = CONCAT(REPEAT(\"1\", campaignProgression-1), REPEAT(\"0\", 60-campaignProgression+1))");
 }
 
 export async function down({context}: { context: QueryInterface }): Promise<void> {
