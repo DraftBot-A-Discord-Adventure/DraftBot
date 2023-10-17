2f52fc37b580a96def43e3ef3d0d43217fc98c0c
romain22222 <romrom22222@hotmail.fr>
2023-10-17 22:55:44 +0200

    fix: turbo micro fix pour ceux à la mauvaise mission lors de la migration

--- src/core/database/game/GameDatabaseUtils.ts
+++ src/core/database/game/GameDatabaseUtils.ts
@@ -4,7 +4,7 @@ import {QueryInterface} from "sequelize";
  * Add a mission to the campaign
  */
 export async function addCampaignMission(context: QueryInterface, position: number): Promise<void> {
-	await context.sequelize.query(`UPDATE player_missions_info SET campaignProgression = campaignProgression + 1 WHERE campaignProgression > ${position}`);
+	await context.sequelize.query(`UPDATE player_missions_info SET campaignProgression = campaignProgression + 1 WHERE campaignProgression >= ${position}`);
 	await context.sequelize.query(`UPDATE player_missions_info SET campaignBlob = CONCAT(SUBSTR(campaignBlob, 1, ${position - 1}), "0", SUBSTR(campaignBlob, ${position}))`);
 }
 
