06f6aaca975ddc376f248bbbef9f413b5b5ec8de
romain22222 <romrom22222@hotmail.fr>
2023-10-25 17:04:45 +0200

    dbv5: some other fight attacks

--- src/interfaces/FightActionResult.ts
+++ src/interfaces/FightActionResult.ts
@@ -1,22 +1,30 @@
 import {FightStatModifierOperation} from "./FightStatModifierOperation";
 import {FightActionStatus} from "./FightActionStatus";
+import {FightAlterations} from "@Core/src/core/fights/actions/FightAlterations";
 
 export enum FightStatBuffed {
-    ATTACK,
-    DEFENSE,
-    SPEED
+	ATTACK,
+	DEFENSE,
+	SPEED,
+	BREATH,
 }
 
-interface FightActionBuff {
-    selfTarget: boolean,
-    stat: FightStatBuffed
-    operator: FightStatModifierOperation,
-    value: number
+export interface FightActionBuff {
+	selfTarget: boolean,
+	stat: FightStatBuffed
+	operator: FightStatModifierOperation,
+	value: number
 }
 
 export interface FightActionResult {
-    fail?: boolean,
-    buffs?: FightActionBuff[],
-    damages: number,
-    attackStatus: FightActionStatus
+	fail?: boolean,
+	buffs?: FightActionBuff[],
+	damages: number,
+	attackStatus: FightActionStatus,
+	alterations?: FightAlteration[]
+}
+
+export interface FightAlteration {
+	selfTarget: boolean,
+	alteration: (typeof FightAlterations)[keyof typeof FightAlterations]
 }
