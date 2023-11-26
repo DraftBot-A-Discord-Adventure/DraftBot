5b9bc79fa488d22cec439930ef2d57d87713edbd
Noelle-Ai <gerardelliot49@gmail.com>
2023-11-26 21:45:34 +0100

    Add Badge and Vote Command #2273

---
+++ src/packets/commands/CommandBadgePacket.ts
@@ -0,0 +1,7 @@
+import {DraftBotPacket} from "../DraftBotPacket";
+
+export interface CommandBadgePacketReq extends DraftBotPacket {
+}
+
+export interface CommandBadgePacketRes extends DraftBotPacket {
+}
---
+++ src/packets/commands/CommandVotePacket.ts
@@ -0,0 +1,7 @@
+import {DraftBotPacket} from "../DraftBotPacket";
+
+export interface CommandVotePacketReq extends DraftBotPacket {
+}
+
+export interface CommandVotePacketRes extends DraftBotPacket {
+}
