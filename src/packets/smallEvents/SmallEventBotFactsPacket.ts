78e1f4e52dde29c7bf94865329b0443349cc720a
Noelle-Ai <gerardelliot49@gmail.com>
2023-11-27 23:41:15 +0100

    dbv5: Refacto on SE JSONs and files #2318

--- src/packets/smallEvents/SmallEventBotFactsPacket.ts
+++ src/packets/smallEvents/SmallEventBotFactsPacket.ts
@@ -3,5 +3,5 @@ import {SmallEventPacket} from "./SmallEventPacket";
 export interface SmallEventBotFactsPacket extends SmallEventPacket {
     information: string,
     infoResult: number,
-    infoComplement: number
+    infoComplement?: number
 }
