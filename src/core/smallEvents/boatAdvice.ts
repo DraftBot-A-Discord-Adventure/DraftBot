5521f22acb26738ca2f8a93ba8a1e600ed58869c
Noelle-Ai <gerardelliot49@gmail.com>
2023-11-27 23:41:15 +0100

    dbv5: Refacto on SE JSONs and files #2318

--- src/core/smallEvents/boatAdvice.ts
+++ src/core/smallEvents/boatAdvice.ts
@@ -4,8 +4,8 @@ import {SmallEventBoatAdvicePacket} from "../../../../Lib/src/packets/smallEvent
 import {makePacket} from "../../../../Lib/src/packets/DraftBotPacket";
 
 export const smallEventFuncs: SmallEventFuncs = {
-    canBeExecuted: (player) => SmallEventConstants.DEFAULT_FUNCTIONS.CAN_BE_EXECUTED.onBoat(player),
-    executeSmallEvent: async (response, player): Promise<void> => {
-        response.push(makePacket<SmallEventBoatAdvicePacket>({}));
-    }
+	canBeExecuted: (player) => SmallEventConstants.DEFAULT_FUNCTIONS.CAN_BE_EXECUTED.onBoat(player),
+	executeSmallEvent: (response): void => {
+		response.push(makePacket<SmallEventBoatAdvicePacket>({}));
+	}
 };
--- src/core/smallEvents/botFacts.ts
+++ src/core/smallEvents/botFacts.ts
@@ -1,80 +1,80 @@
 import {SmallEventDataController, SmallEventFuncs} from "../../data/SmallEvent";
 import {SmallEventConstants} from "../constants/SmallEventConstants";
-import {SmallEventBoatAdvicePacket} from "../../../../Lib/src/packets/smallEvents/SmallEventBoatAdvicePacket";
 import {makePacket} from "../../../../Lib/src/packets/DraftBotPacket";
 import {RandomUtils} from "../utils/RandomUtils";
 import {Players} from "../database/game/models/Player";
 import {PetEntities} from "../database/game/models/PetEntity";
 import {Guilds} from "../database/game/models/Guild";
 import {ClassDataController} from "../../data/Class";
-import {readdir, readdirSync} from "fs";
+import {readdirSync} from "fs";
 import {SmallEventBotFactsPacket} from "../../../../Lib/src/packets/smallEvents/SmallEventBotFactsPacket";
 
 type BotFactsProperties = {
-    "possibleInfos": string[]
+    "properties": {
+		"possibleInfos": string[]
+	}
 }
 
 const getNbPlayersWithGivenClass = async (): Promise<[number, number]> => {
-    const classToCheck = ClassDataController.instance.getById(parseInt(RandomUtils.draftbotRandom.pick(readdirSync("resources/text/classes"))
-        .slice(0, -5), 10));
-    return [await Players.getNbPlayersWithClass(classToCheck), classToCheck.id];
+	const classToCheck = ClassDataController.instance.getById(parseInt(RandomUtils.draftbotRandom.pick(readdirSync("resources/text/classes")) // TODO: Rework, We musn't
+		.slice(0, -5), 10));
+	return [await Players.getNbPlayersWithClass(classToCheck), classToCheck.id];
 };
 
 export const smallEventFuncs: SmallEventFuncs = {
-    canBeExecuted: (player) => SmallEventConstants.DEFAULT_FUNCTIONS.CAN_BE_EXECUTED.onContinent(player),
-    executeSmallEvent: async (response, player): Promise<void> => {
-        const botFactsProperties = SmallEventDataController.instance.getById("botFacts").getProperties<BotFactsProperties>();
-        const information = RandomUtils.draftbotRandom.pick(Object.keys(botFactsProperties.possibleInfos));
-        let infoResult = 0;
-        let infoComplement: number = -1;
+	canBeExecuted: (player) => SmallEventConstants.DEFAULT_FUNCTIONS.CAN_BE_EXECUTED.onContinent(player),
+	executeSmallEvent: async (response, player): Promise<void> => {
+		const botFactsProperties = SmallEventDataController.instance.getById("botFacts").getProperties<BotFactsProperties>();
+		const information = RandomUtils.draftbotRandom.pick(Object.keys(botFactsProperties.properties.possibleInfos));
+		const packet: SmallEventBotFactsPacket = {information, infoResult: 0};
 
-        let array = [];
-        switch (information) {
-            case "nbMeanPoints":
-                infoResult = await Players.getNbMeanPoints();
-                break;
-            case "meanWeeklyScore":
-                infoResult = await Players.getMeanWeeklyScore();
-                break;
-            case "nbPlayersHaventStartedTheAdventure":
-                infoResult = await Players.getNbPlayersHaventStartedTheAdventure();
-                break;
-            case "levelMean":
-                infoResult = await Players.getLevelMean();
-                break;
-            case "nbMeanMoney":
-                infoResult = await Players.getNbMeanMoney();
-                break;
-            case "sumAllMoney":
-                infoResult = await Players.getSumAllMoney();
-                break;
-            case "richestPlayer":
-                infoResult = await Players.getRichestPlayer();
-                break;
-            case "trainedPets":
-                infoResult = await PetEntities.getNbTrainedPets();
-                break;
-            case "percentMalePets":
-                infoResult = Math.round(await PetEntities.getNbPetsGivenSex("m") / await PetEntities.getNbPets() * 10000) / 100;
-                break;
-            case "percentFemalePets":
-                infoResult = Math.round(await PetEntities.getNbPetsGivenSex("f") / await PetEntities.getNbPets() * 10000) / 100;
-                break;
-            case "guildLevelMean":
-                infoResult = await Guilds.getGuildLevelMean();
-                break;
-            case "feistyPets":
-                infoResult = await PetEntities.getNbFeistyPets();
-                break;
-            case "nbPlayersOnYourMap":
-                infoResult = await player.getNbPlayersOnYourMap();
-                break;
-            default:
-                array = await getNbPlayersWithGivenClass();
-                infoResult = array[0];
-                infoComplement = array[1];
-        }
+		let array = [];
+		switch (information) {
+		case "nbMeanPoints":
+			packet.infoResult = await Players.getNbMeanPoints();
+			break;
+		case "meanWeeklyScore":
+			packet.infoResult = await Players.getMeanWeeklyScore();
+			break;
+		case "nbPlayersHaventStartedTheAdventure":
+			packet.infoResult = await Players.getNbPlayersHaventStartedTheAdventure();
+			break;
+		case "levelMean":
+			packet.infoResult = await Players.getLevelMean();
+			break;
+		case "nbMeanMoney":
+			packet.infoResult = await Players.getNbMeanMoney();
+			break;
+		case "sumAllMoney":
+			packet.infoResult = await Players.getSumAllMoney();
+			break;
+		case "richestPlayer":
+			packet.infoResult = await Players.getRichestPlayer();
+			break;
+		case "trainedPets":
+			packet.infoResult = await PetEntities.getNbTrainedPets();
+			break;
+		case "percentMalePets":
+			packet.infoResult = Math.round(await PetEntities.getNbPetsGivenSex("m") / await PetEntities.getNbPets() * 10000) / 100;
+			break;
+		case "percentFemalePets":
+			packet.infoResult = Math.round(await PetEntities.getNbPetsGivenSex("f") / await PetEntities.getNbPets() * 10000) / 100;
+			break;
+		case "guildLevelMean":
+			packet.infoResult = await Guilds.getGuildLevelMean();
+			break;
+		case "feistyPets":
+			packet.infoResult = await PetEntities.getNbFeistyPets();
+			break;
+		case "nbPlayersOnYourMap":
+			packet.infoResult = await player.getNbPlayersOnYourMap();
+			break;
+		default:
+			array = await getNbPlayersWithGivenClass();
+			packet.infoResult = array[0];
+			packet.infoComplement = array[1];
+		}
 
-        response.push(makePacket<SmallEventBotFactsPacket>({information, infoResult, infoComplement}));
-    }
+		response.push(makePacket<SmallEventBotFactsPacket>(packet));
+	}
 };
