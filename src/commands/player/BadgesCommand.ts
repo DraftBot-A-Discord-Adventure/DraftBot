8a9d869479d58635eba190c74da9e1c194652e54
BastLast <lastaccount@fastmail.com>
2024-02-04 12:44:43 +0100

    add badge command embed
    DraftBot-A-Discord-Adventure/DraftBot#2273

--- src/commands/player/BadgesCommand.ts
+++ src/commands/player/BadgesCommand.ts
@@ -4,19 +4,29 @@ import {CommandBadgesPacketReq} from "../../../../Lib/src/packets/commands/Comma
 import {DraftbotInteraction} from "../../messages/DraftbotInteraction";
 import i18n from "../../translations/i18n";
 import {SlashCommandBuilderGenerator} from "../SlashCommandBuilderGenerator";
+import {DraftBotEmbed} from "../../messages/DraftBotEmbed";
 
 /**
  * Pings the bot, to check if it is alive and how well is it
  */
 async function getPacket(interaction: DraftbotInteraction): Promise<CommandBadgesPacketReq> {
-	const packet = makePacket(CommandBadgesPacketReq, {});
-	await interaction.reply({content: i18n.t("commands:badges.description")});
-	return packet;
+    const packet = makePacket(CommandBadgesPacketReq, {});
+    await interaction.reply({
+        embeds: [new DraftBotEmbed()
+            .setTitle(i18n.t("commands:badges.title", {
+                    lng: interaction.channel.language
+                }
+            ))
+            .setDescription(i18n.t("commands:badges.description", {
+                lng: interaction.channel.language
+            }))]
+    });
+    return packet;
 }
 
 export const commandInfo: ICommand = {
-	slashCommandBuilder: SlashCommandBuilderGenerator.generateBaseCommand("badges"),
-	getPacket,
-	requirements: {},
-	mainGuildCommand: false
+    slashCommandBuilder: SlashCommandBuilderGenerator.generateBaseCommand("badges"),
+    getPacket,
+    requirements: {},
+    mainGuildCommand: false
 };
