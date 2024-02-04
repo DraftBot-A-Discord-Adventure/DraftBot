62f71b28caa2835351ca4c086869feea9fe97b9d
BastLast <lastaccount@fastmail.com>
2024-02-04 01:26:29 +0100

    first draft badge command
    DraftBot-A-Discord-Adventure/DraftBot#2273

--- src/packets/commands/CommandBadgePacket.ts
+++
@@ -1,7 +0,0 @@
-import {DraftBotPacket} from "../DraftBotPacket";
-
-export class CommandBadgePacketReq extends DraftBotPacket {
-}
-
-export class CommandBadgePacketRes extends DraftBotPacket {
-}
---
+++ src/packets/commands/CommandBadgesPacket.ts
@@ -0,0 +1,7 @@
+import {DraftBotPacket} from "../DraftBotPacket";
+
+export class CommandBadgesPacketReq extends DraftBotPacket {
+}
+
+export class CommandBadgesPacketRes extends DraftBotPacket {
+}
