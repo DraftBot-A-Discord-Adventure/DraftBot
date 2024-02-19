5e03acb90dc241375a6d439c0e8188e9724820f7
BastLast <lastaccount@fastmail.com>
2024-02-19 23:10:07 +0100

    deletion of useless core code

--- Core/src/commands/player/BadgesCommand.ts
+++
@@ -1,8 +0,0 @@
-import {packetHandler} from "../../core/packetHandlers/PacketHandler";
-import {CommandBadgesPacketReq} from "../../../../Lib/src/packets/commands/CommandBadgesPacket";
-
-export default class BadgesCommand {
-	@packetHandler(CommandBadgesPacketReq)
-	execute(): void {
-	}
-}
--- Core/src/commands/player/IdeaCommand.ts
+++
@@ -1,7 +0,0 @@
-import {packetHandler} from "../../core/packetHandlers/PacketHandler";
-import {CommandIdeaPacketReq} from "../../../../Lib/src/packets/commands/CommandIdeaPacket";
-
-export default class IdeaCommand {
-	@packetHandler(CommandIdeaPacketReq)
-	execute(): void {}
-}
