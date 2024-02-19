50c56fb155a76451e85c935133b1647957c30cfd
BastLast <lastaccount@fastmail.com>
2024-02-19 22:07:20 +0100

    vote command #2273

---
+++ Discord/src/commands/player/VoteCommand.ts
@@ -0,0 +1,28 @@
+import {ICommand} from "../ICommand";
+import {DraftbotInteraction} from "../../messages/DraftbotInteraction";
+import i18n from "../../translations/i18n";
+import {SlashCommandBuilderGenerator} from "../SlashCommandBuilderGenerator";
+import {DraftBotEmbed} from "../../messages/DraftBotEmbed";
+
+/**
+ * Access information about badges
+ */
+async function getPacket(interaction: DraftbotInteraction): Promise<null> {
+	await interaction.reply({
+		embeds: [new DraftBotEmbed()
+			.setTitle(i18n.t("commands:vote.title", {
+				lng: interaction.channel.language
+			}))
+			.setDescription(i18n.t("commands:vote.description", {
+				lng: interaction.channel.language
+			}))]
+	});
+	return null;
+}
+
+export const commandInfo: ICommand = {
+	slashCommandBuilder: SlashCommandBuilderGenerator.generateBaseCommand("vote"),
+	getPacket,
+	requirements: {},
+	mainGuildCommand: false
+};
