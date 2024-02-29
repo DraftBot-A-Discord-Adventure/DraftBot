6835e20d8f2f1858897a83172244bfd959186f2b
BastLast <lastaccount@fastmail.com>
2024-02-29 23:57:55 +0100

    fix language used for mention of the bot

--- Core/src/core/database/game/migrations/019-v5.ts
+++ Core/src/core/database/game/migrations/019-v5.ts
@@ -29,7 +29,7 @@ clientSecret = "secret"
 
 	for (let i = 0; i < players.length; ++i) {
 		const player = players[i];
-		const user = await KeycloakUtils.getOrRegisterDiscordUser(config.keycloak, player.discordUserId as string, player.discordUserId as string, LANGUAGE.ENGLISH);
+		const user = await KeycloakUtils.getOrRegisterDiscordUser(config.keycloak, player.discordUserId as string, player.discordUserId as string, LANGUAGE.DEFAULT_LANGUAGE);
 		await context.sequelize.query(`UPDATE players SET discordUserId = "${user.id}" WHERE discordUserId = "${player.discordUserId}"`);
 		logsV5NewIds.set(player.discordUserId as string, user.id);
 	}
