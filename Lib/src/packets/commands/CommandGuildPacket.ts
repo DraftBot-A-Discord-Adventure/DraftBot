729463d10e14d55248ab212692a77d86997ab55f
BastLast <lastaccount@fastmail.com>
2024-03-09 16:19:53 +0100

    WIP guild command 2 #2273

--- Lib/src/packets/commands/CommandGuildPacket.ts
+++ Lib/src/packets/commands/CommandGuildPacket.ts
@@ -5,9 +5,42 @@ export class CommandGuildPacketReq extends DraftBotPacket {
 		rank?: number,
 		keycloakId?: string
 	};
-	guildName?: string;
+
+	askedGuildName?: string;
+}
+
+export interface GuildMemberPacket {
+	keycloakId: string;
+	rank: number,
+	score: number
+	islandStatus: {
+		isOnPveIsland: boolean,
+		isOnBoat: boolean,
+		isPveIslandAlly: boolean,
+		isInactive: boolean
+		cannotBeJoinedOnBoat: boolean
+	}
 }
 
 export class CommandGuildPacketRes extends DraftBotPacket {
 	foundGuild!: boolean;
+
+	data?: {
+		name: string,
+		description?: string,
+		chiefId: number,
+		elderId: number,
+		level: number,
+		experience: {
+			value: number,
+			max: number
+		},
+		rank: {
+			unranked: boolean,
+			rank: number,
+			numberOfGuilds: number,
+			score: number
+		},
+		members: GuildMemberPacket[]
+	};
 }
