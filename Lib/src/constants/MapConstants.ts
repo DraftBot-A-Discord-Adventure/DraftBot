972ccb7a10caf785601dac5335634ba9e2950d1a
BastLast <lastaccount@fastmail.com>
2024-03-10 13:06:43 +0100

    WIP guildCommand #2273

--- Lib/src/constants/MapConstants.ts
+++ Lib/src/constants/MapConstants.ts
@@ -1,4 +1,11 @@
 export abstract class MapConstants {
+
+	static readonly MAP_URL = "https://draftbot.com/public/ressources/map.jpg"; // Unused, but useful
+
+	static readonly MAP_URL_WITH_CURSOR = "https://draftbot.com/public/ressources/mapsCursed/{mapLink}map.jpg";
+
+	static readonly FORCED_MAPS_URL = "https://draftbot.com/public/ressources/maps/{name}.jpg";
+
 	static readonly WATER_MAP_LINKS = [
 		1, 2, 17, 20, 21, 22,
 		23, 24, 32, 33, 34, 36,
