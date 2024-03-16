0b3d724d5ce9e6910f56987a5d2524092c4085d6
BastLast <lastaccount@fastmail.com>
2024-03-17 00:34:31 +0100

    petCommand WIP #2273

--- Core/src/commands/pet/PetCommand.ts
+++ Core/src/commands/pet/PetCommand.ts
@@ -3,11 +3,8 @@ import {WebsocketClient} from "../../../../Lib/src/instances/WebsocketClient";
 import {DraftBotPacket, makePacket, PacketContext} from "../../../../Lib/src/packets/DraftBotPacket";
 import {Players} from "../../core/database/game/models/Player";
 import {CommandPetPacketReq, CommandPetPacketRes} from "../../../../Lib/src/packets/commands/CommandPetPacket";
-import {Maps} from "../../core/maps/Maps";
-import {MapCache} from "../../core/maps/MapCache";
-
 import {PetEntities} from "../../core/database/game/models/PetEntity";
-import {Pet, PetDataController} from "../../data/Pet";
+import {PetDataController} from "../../data/Pet";
 
 export default class PetCommand {
 	@packetHandler(CommandPetPacketReq)
@@ -15,13 +12,13 @@ export default class PetCommand {
 
 		const player = packet.askedPlayer.keycloakId ? await Players.getByKeycloakId(packet.askedPlayer.keycloakId) : await Players.getByRank(packet.askedPlayer.rank);
 		const pet = await PetEntities.getById(player.petId);
-		const petModel = PetDataController.instance.getById(pet.petId);
 		if (!pet) {
 			response.push(makePacket(CommandPetPacketRes, {
 				foundPet: false
 			}));
 		}
 		else {
+			const petModel = PetDataController.instance.getById(pet.petId);
 			response.push(makePacket(CommandPetPacketRes, {
 				foundPet: true,
 				data: {
