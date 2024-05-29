3c344b0ca22be5a2e945c23e339c078409d1d002
Feiryn <98115666+feiryn@users.noreply.github.com>
2024-05-29 09:39:37 +0200

    Fix declarations and imports

--- Core/src/commands/admin/testCommands/Map/SmallEventTestCommand.ts
+++ Core/src/commands/admin/testCommands/Map/SmallEventTestCommand.ts
@@ -1,10 +1,8 @@
 import {ExecuteTestCommandLike, ITestCommand, TypeKey} from "../../../../core/CommandsTest";
 import {SmallEventDataController} from "../../../../data/SmallEvent";
 import ReportCommand from "../../../player/ReportCommand";
-import {DraftBotPacket, makePacket, PacketContext} from "../../../../../../Lib/src/packets/DraftBotPacket";
-import {Client} from "../../../../../../Lib/src/instances/Client";
+import {makePacket} from "../../../../../../Lib/src/packets/DraftBotPacket";
 import {CommandReportPacketReq} from "../../../../../../Lib/src/packets/commands/CommandReportPacket";
-import {WebsocketClient} from "../../../../../../Lib/src/instances/WebsocketClient";
 
 const smallEventsKeys = SmallEventDataController.instance.getKeys();
 
@@ -28,7 +26,7 @@ const smallEventsKeysLower = smallEventsKeys.map(seName => seName.toLowerCase())
 /**
  * Force a small event with a given event name
  */
-const smallEventTestCommand: ExecuteTestCommandLike = async (player, args, response: DraftBotPacket[], context: PacketContext, client: WebsocketClient) => {
+const smallEventTestCommand: ExecuteTestCommandLike = async (player, args, response, context, client) => {
 	const keyPos = smallEventsKeysLower.indexOf(args[0].toLowerCase());
 	if (keyPos === -1) {
 		throw new Error(`Erreur smallEvent : le mini-event ${args[0]} n'existe pas. Veuillez vous référer à la commande "test help smallEvent" pour plus d'informations`);
