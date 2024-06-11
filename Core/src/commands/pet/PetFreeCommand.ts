407e529616fa4118497803f0a05cc893217c3add
BastLast <lastaccount@fastmail.com>
2024-06-11 23:31:44 +0200

    Petfree command work #2273

--- Core/src/commands/pet/PetFreeCommand.ts
+++ Core/src/commands/pet/PetFreeCommand.ts
@@ -1,34 +1,107 @@
 import {packetHandler} from "../../core/packetHandlers/PacketHandler";
 import {WebsocketClient} from "../../../../Lib/src/instances/WebsocketClient";
 import {DraftBotPacket, makePacket, PacketContext} from "../../../../Lib/src/packets/DraftBotPacket";
-import {Players} from "../../core/database/game/models/Player";
-import {PetEntities} from "../../core/database/game/models/PetEntity";
-import {PetDataController} from "../../data/Pet";
-import {CommandPetFreePacketReq} from "../../../../Lib/src/packets/commands/CommandPetFreePacket";
+import {Player, Players} from "../../core/database/game/models/Player";
+import {PetEntities, PetEntity} from "../../core/database/game/models/PetEntity";
+import {
+	CommandPetFreeAcceptPacketRes,
+	CommandPetFreePacketReq,
+	CommandPetFreePacketRes, CommandPetFreeRefusePacketRes
+} from "../../../../Lib/src/packets/commands/CommandPetFreePacket";
+import {BlockingUtils} from "../../core/utils/BlockingUtils";
+import {PetFreeConstants} from "../../../../Lib/src/constants/PetFreeConstants";
+import {EndCallback, ReactionCollectorInstance} from "../../core/utils/ReactionsCollector";
+import {ReactionCollectorAcceptReaction} from "../../../../Lib/src/packets/interaction/ReactionCollectorPacket";
+import {BlockingConstants} from "../../../../Lib/src/constants/BlockingConstants";
+import {ReactionCollectorPetFree} from "../../../../Lib/src/packets/interaction/ReactionCollectorPetFree";
+import {LogsDatabase} from "../../core/database/logs/LogsDatabase";
+import {NumberChangeReason} from "../../../../Lib/src/constants/LogsConstants";
+import Guild, {Guilds} from "../../core/database/game/models/Guild";
+import {GuildConstants} from "../../../../Lib/src/constants/GuildConstants";
+import {getFoodIndexOf} from "../../core/utils/FoodUtils";
+import {RandomUtils} from "../../core/utils/RandomUtils";
+import {Constants} from "../../../../Lib/src/constants/Constants";
 
 export default class PetFreeCommand {
-	@packetHandler(CommandPetFreePacketReq)
-	async execute(client: WebsocketClient, packet: CommandPetPacketReq, context: PacketContext, response: DraftBotPacket[]): Promise<void> {
 
-		const player = packet.askedPlayer.keycloakId ? await Players.getByKeycloakId(packet.askedPlayer.keycloakId) : await Players.getByRank(packet.askedPlayer.rank);
-		const pet = await PetEntities.getById(player.petId);
-		if (!pet) {
-			response.push(makePacket(CommandPetPacketRes, {
+	/**
+	 * Send the number of milliseconds that remain before the player is allowed to free his pet
+	 * (a player cannot free pets too often)
+	 * @param player
+	 */
+	getCooldownRemainingTimeMs(player: Player): number {
+		return PetFreeConstants.FREE_COOLDOWN - (new Date().valueOf() - player.lastPetFree.valueOf());
+	}
+
+	/**
+	 * Returns the amount of money the player is missing to free his pet (0 if the player has enough money)
+	 * @param player
+	 * @param playerPet
+	 */
+	getMissingMoneyToFreePet(player: Player, playerPet: PetEntity): number {
+		return playerPet.isFeisty() ? PetFreeConstants.FREE_FEISTY_COST - player.money : 0;
+	}
+
+	luckyMeat(guild: Guild, pPet: PetEntity): boolean {
+		return guild.carnivorousFood + 1 <= GuildConstants.MAX_PET_FOOD[getFoodIndexOf(Constants.PET_FOOD.CARNIVOROUS_FOOD)]
+			&& RandomUtils.draftbotRandom.realZeroToOneInclusive() <= PetFreeConstants.GIVE_MEAT_PROBABILITY
+			&& !pPet.isFeisty();
+	}
+
+	async acceptPetFree(player: Player, playerPet: PetEntity, response: DraftBotPacket[]): Promise<void> {
+		if (playerPet.isFeisty()) {
+			await player.addMoney({
+				amount: -PetFreeConstants.FREE_FEISTY_COST,
+				response,
+				reason: NumberChangeReason.PET_FREE
+			});
+		}
+
+		LogsDatabase.logPetFree(playerPet).then();
+
+		await playerPet.destroy();
+		player.petId = null;
+		player.lastPetFree = new Date();
+		await player.save();
+
+		let guild: Guild;
+		try {
+			guild = await Guilds.getById(player.guildId);
+		}
+		catch (error) {
+			guild = null;
+		}
+
+		const luckyMeat = this.luckyMeat(guild, playerPet);
+		if (guild !== null && luckyMeat) {
+			guild.carnivorousFood += PetFreeConstants.MEAT_GIVEN;
+			await guild.save();
+		}
+
+		response.push(makePacket(CommandPetFreeAcceptPacketRes, {
+			petId: playerPet.typeId,
+			petSex: playerPet.sex,
+			petNickname: playerPet.nickname,
+			freeCost: playerPet.isFeisty() ? PetFreeConstants.FREE_FEISTY_COST : 0,
+			luckyMeat
+		}));
+	}
+
+
+	@packetHandler(CommandPetFreePacketReq)
+	async execute(client: WebsocketClient, packet: CommandPetFreePacketReq, context: PacketContext, response: DraftBotPacket[]): Promise<void> {
+		const player = await Players.getByKeycloakId(packet.keycloakId);
+
+		if (BlockingUtils.appendBlockedPacket(player, response)) {
+			return;
+		}
+
+		const playerPet = await PetEntities.getById(player.petId);
+		if (!playerPet) {
+			response.push(makePacket(CommandPetFreePacketRes, {
 				foundPet: false
 			}));
-		}
-		else {
-			const petModel = PetDataController.instance.getById(pet.typeId);
-			response.push(makePacket(CommandPetPacketRes, {
-				foundPet: true,
-				data: {
-					nickname: pet.nickname,
-					petTypeId: petModel.id,
-					rarity: petModel.rarity,
-					sex: pet.sex,
-					loveLevel: pet.getLoveLevelNumber()
-				}
-			}));
+			return;
 		}
 	}
 }
