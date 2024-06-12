6346685d69504790cca069dbc2bdd1af0ec0950a
BastLast <lastaccount@fastmail.com>
2024-06-13 00:13:51 +0200

    Petfree command work #2273

--- Discord/src/commands/pet/PetFreeCommand.ts
+++ Discord/src/commands/pet/PetFreeCommand.ts
@@ -3,13 +3,15 @@ import {makePacket, PacketContext} from "../../../../Lib/src/packets/DraftBotPac
 import {DraftbotInteraction} from "../../messages/DraftbotInteraction";
 import i18n from "../../translations/i18n";
 import {SlashCommandBuilderGenerator} from "../SlashCommandBuilderGenerator";
-import {CommandPetFreePacketReq, CommandPetFreePacketRes} from "../../../../Lib/src/packets/commands/CommandPetFreePacket";
-import {DraftBotEmbed} from "../../messages/DraftBotEmbed";
+import {
+	CommandPetFreePacketReq,
+	CommandPetFreePacketRes
+} from "../../../../Lib/src/packets/commands/CommandPetFreePacket";
 import {DiscordCache} from "../../bot/DiscordCache";
 import {DraftBotErrorEmbed} from "../../messages/DraftBotErrorEmbed";
 import {KeycloakUser} from "../../../../Lib/src/keycloak/KeycloakUser";
 import {Effect} from "../../../../Lib/src/enums/Effect";
-import {PetData, PetUtils} from "../../utils/PetUtils";
+import {millisecondsToMinutes} from "../../../../Lib/src/utils/TimeUtils";
 
 /**
  * Destroy a pet forever... RIP
@@ -35,25 +37,33 @@ export async function handleCommandPetFreePacketRes(packet: CommandPetFreePacket
 			});
 			return;
 		}
-
-		const petData: PetData = packet.data!;
-
-		const PetCommandEmbed = new DraftBotEmbed()
-			.formatAuthor(
-				i18n.t("commands:pet.embedTitle", {
-					lng: interaction.userLanguage,
-					pseudo: interaction.user.username
-				}),
-				interaction.user
-			)
-			.setDescription(
-				PetUtils.petToString(interaction.userLanguage, petData)
-			);
-
-		await interaction.reply({
-			embeds: [PetCommandEmbed],
-			fetchReply: true
-		});
+		if (!packet.petCanBeFreed) {
+			if (packet.missingMoney! > 0) {
+				await interaction.reply({
+					embeds: [
+						new DraftBotErrorEmbed(
+							interaction.user,
+							interaction,
+							i18n.t("error:notEnoughMoney", {lng: interaction.userLanguage, money: packet.missingMoney})
+						)
+					]
+				});
+			}
+			else {
+				await interaction.reply({
+					embeds: [
+						new DraftBotErrorEmbed(
+							interaction.user,
+							interaction,
+							i18n.t("error:cooldownPetFree", {
+								lng: interaction.userLanguage,
+								remainingTime: millisecondsToMinutes(packet.cooldownRemainingTimeMs!)
+							})
+						)
+					]
+				});
+			}
+		}
 	}
 }
 
