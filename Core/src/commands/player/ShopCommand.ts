c2b0ffe440b34ddf896f4d7153a8bf215bcff421
Feiryn <98115666+feiryn@users.noreply.github.com>
2024-10-10 22:47:19 +0200

    Shop command WIP

---
+++ Core/src/commands/player/ShopCommand.ts
@@ -0,0 +1,330 @@
+import {packetHandler} from "../../core/packetHandlers/PacketHandler";
+import {WebsocketClient} from "../../../../Lib/src/instances/WebsocketClient";
+import {DraftBotPacket, makePacket, PacketContext} from "../../../../Lib/src/packets/DraftBotPacket";
+import {CommandShopPacketReq} from "../../../../Lib/src/packets/commands/CommandShopPacket";
+import {BlockingUtils} from "../../core/utils/BlockingUtils";
+import {Player, Players} from "../../core/database/game/models/Player";
+import {LogsReadRequests} from "../../core/database/logs/LogsReadRequests";
+import {ShopUtils} from "../../core/utils/ShopUtils";
+import {
+	CommandShopAlreadyHaveBadge,
+	CommandShopBadgeBought,
+	CommandShopBoughtTooMuchDailyPotions, CommandShopClosed,
+	CommandShopFullRegen,
+	CommandShopHealAlterationDone,
+	CommandShopNoAlterationToHeal,
+	CommandShopNoEnergyToHeal,
+	CommandShopTooManyEnergyBought, ReactionCollectorShop,
+	ShopCategory,
+	ShopItem
+} from "../../../../Lib/src/packets/interaction/ReactionCollectorShop";
+import {ShopConstants} from "../../../../Lib/src/constants/ShopConstants";
+import {getItemValue, giveItemToPlayer, giveRandomItem, toItemWithDetails} from "../../core/utils/ItemUtils";
+import {draftBotInstance} from "../../index";
+import {NumberChangeReason, ShopItemType} from "../../../../Lib/src/constants/LogsConstants";
+import {millisecondsToMinutes} from "../../../../Lib/src/utils/TimeUtils";
+import {Effect} from "../../../../Lib/src/enums/Effect";
+import {TravelTime} from "../../core/maps/TravelTime";
+import {MissionsController} from "../../core/missions/MissionsController";
+import {EntityConstants} from "../../../../Lib/src/constants/EntityConstants";
+import {BadgeConstants} from "../../../../Lib/src/constants/BadgeConstants";
+import {Potion, PotionDataController} from "../../data/Potion";
+import {Settings} from "../../core/database/game/models/Setting";
+import {InventorySlots} from "../../core/database/game/models/InventorySlot";
+import {InventoryInfos} from "../../core/database/game/models/InventoryInfo";
+import {ItemConstants} from "../../../../Lib/src/constants/ItemConstants";
+import {EndCallback, ReactionCollectorInstance} from "../../core/utils/ReactionsCollector";
+import {BlockingConstants} from "../../../../Lib/src/constants/BlockingConstants";
+import {
+	ReactionCollectorBuyCategorySlot,
+	ReactionCollectorBuyCategorySlotBuySuccess,
+	ReactionCollectorBuyCategorySlotCancelReaction,
+	ReactionCollectorBuyCategorySlotReaction
+} from "../../../../Lib/src/packets/interaction/ReactionCollectorBuyCategorySlot";
+
+/**
+ * Get the shop item for getting a random item
+ */
+function getRandomItemShopItem(): ShopItem {
+	return {
+		id: "randomItem",
+		price: ShopConstants.RANDOM_ITEM_PRICE,
+		amounts: [1],
+		buyCallback: async (context, response, playerId): Promise<boolean> => {
+			const player = await Players.getById(playerId);
+			await giveRandomItem(context, response, player);
+			draftBotInstance.logsDatabase.logClassicalShopBuyout(player.keycloakId, ShopItemType.RANDOM_ITEM).then();
+			return true;
+		}
+	};
+}
+
+/**
+ * Calculate the price for healing from an alteration
+ * @param player
+ */
+function calculateHealAlterationPrice(player: Player) :number {
+	let price = ShopConstants.ALTERATION_HEAL_BASE_PRICE;
+	const remainingTime = millisecondsToMinutes(player.effectRemainingTime());
+	// If remaining time is under one hour price become degressive until being divided by 8 at the 15 minutes marque then it no longer decrease
+	if (remainingTime < ShopConstants.MAX_REDUCTION_TIME) {
+		if (remainingTime <= ShopConstants.MIN_REDUCTION_TIME) {
+			price /= ShopConstants.MAX_PRICE_REDUCTION_DIVISOR;
+		}
+		else {
+			// Calculate the price reduction based on the remaining time
+			const priceDecreasePerMinute = (
+				ShopConstants.ALTERATION_HEAL_BASE_PRICE - ShopConstants.ALTERATION_HEAL_BASE_PRICE / ShopConstants.MAX_PRICE_REDUCTION_DIVISOR
+			) / (
+				ShopConstants.MAX_REDUCTION_TIME - ShopConstants.MIN_REDUCTION_TIME
+			);
+			price -= priceDecreasePerMinute * (ShopConstants.MAX_REDUCTION_TIME - remainingTime);
+		}
+	}
+	return Math.round(price);
+}
+
+/**
+ * Get the shop item for healing from an alteration
+ * @param player
+ */
+function getHealAlterationShopItem(player: Player): ShopItem {
+	const price = calculateHealAlterationPrice(player);
+	return {
+		id: "healAlteration",
+		price,
+		amounts: [1],
+		buyCallback: async (context, response, playerId): Promise<boolean> => {
+			const player = await Players.getById(playerId);
+			if (player.currentEffectFinished(new Date())) {
+				response.push(makePacket(CommandShopNoAlterationToHeal, {}));
+				return false;
+			}
+			if (player.effectId !== Effect.DEAD.id && player.effectId !== Effect.JAILED.id) {
+				await TravelTime.removeEffect(player, NumberChangeReason.SHOP);
+				await player.save();
+			}
+			await MissionsController.update(player, response, {missionId: "recoverAlteration"});
+			response.push(makePacket(CommandShopHealAlterationDone, {}));
+			draftBotInstance.logsDatabase.logClassicalShopBuyout(player.keycloakId, ShopItemType.ALTERATION_HEAL).then();
+			return true;
+		}
+	};
+}
+
+/**
+ * Get the shop item for recovering energy
+ * @param healEnergyAlreadyPurchased
+ */
+function getHealEnergyShopItem(healEnergyAlreadyPurchased: number): ShopItem {
+	return {
+		id: "healEnergy",
+		price: EntityConstants.HEAL_ENERGY_PRICE[healEnergyAlreadyPurchased > EntityConstants.HEAL_ENERGY_PRICE.length - 1 ? EntityConstants.HEAL_ENERGY_PRICE.length - 1 : healEnergyAlreadyPurchased],
+		amounts: [1],
+		buyCallback: async (context, response, playerId): Promise<boolean> => {
+			const player = await Players.getById(playerId);
+			if (healEnergyAlreadyPurchased > EntityConstants.HEAL_ENERGY_PRICE.length - 1) {
+				response.push(makePacket(CommandShopTooManyEnergyBought, {}));
+				return false;
+			}
+			if (player.fightPointsLost === 0) {
+				response.push(makePacket(CommandShopNoEnergyToHeal, {}));
+				return false;
+			}
+			player.setFightPointsLost(0, NumberChangeReason.SHOP);
+			await player.save();
+			draftBotInstance.logsDatabase.logClassicalShopBuyout(player.keycloakId, ShopItemType.ENERGY_HEAL).then();
+			return true;
+		}
+	};
+}
+
+/**
+ * Get the shop item for regenerating to full life
+ */
+function getRegenShopItem(): ShopItem {
+	return {
+		id: "regen",
+		price: ShopConstants.FULL_REGEN_PRICE,
+		amounts: [1],
+		buyCallback: async (context, response, playerId): Promise<boolean> => {
+			const player = await Players.getById(playerId);
+			await player.addHealth(player.getMaxHealth() - player.health, response, NumberChangeReason.SHOP, {
+				shouldPokeMission: true,
+				overHealCountsForMission: false
+			});
+			await player.save();
+			response.push(makePacket(CommandShopFullRegen, {}));
+			draftBotInstance.logsDatabase.logClassicalShopBuyout(player.keycloakId, ShopItemType.FULL_REGEN).then();
+			return true;
+		}
+	};
+}
+
+
+/**
+ * Get the shop item for the money mouth badge
+ */
+function getBadgeShopItem(): ShopItem {
+	return {
+		id: "moneyMouthBadge",
+		price: ShopConstants.MONEY_MOUTH_BADGE_PRICE,
+		amounts: [1],
+		buyCallback: async (context, response, playerId): Promise<boolean> => {
+			const player = await Players.getById(playerId);
+			if (player.hasBadge(BadgeConstants.RICH_PERSON)) {
+				response.push(makePacket(CommandShopAlreadyHaveBadge, {}));
+				return false;
+			}
+			player.addBadge(BadgeConstants.RICH_PERSON);
+			await player.save();
+			response.push(makePacket(CommandShopBadgeBought, {}));
+			draftBotInstance.logsDatabase.logClassicalShopBuyout(player.keycloakId, ShopItemType.BADGE).then();
+			return true;
+		}
+	};
+}
+
+/**
+ * Get the shop item for getting the daily potion
+ * @param potion
+ */
+function getDailyPotionShopItem(potion: Potion): ShopItem {
+	return {
+		id: "dailyPotion",
+		price: Math.round(getItemValue(potion) * 0.7),
+		amounts: [1],
+		buyCallback: async (context, response, playerId): Promise<boolean> => {
+			const player = await Players.getById(playerId);
+			const potionAlreadyPurchased = await LogsReadRequests.getAmountOfDailyPotionsBoughtByPlayer(player.keycloakId);
+			if (potionAlreadyPurchased >= ShopConstants.MAX_DAILY_POTION_BUYOUTS) {
+				response.push(makePacket(CommandShopBoughtTooMuchDailyPotions, {}));
+				return false;
+			}
+			await giveItemToPlayer(player, potion, context, response, await InventorySlots.getOfPlayer(player.id));
+			draftBotInstance.logsDatabase.logClassicalShopBuyout(player.keycloakId, ShopItemType.DAILY_POTION).then();
+			if (potionAlreadyPurchased === ShopConstants.MAX_DAILY_POTION_BUYOUTS - 1) {
+				await MissionsController.update(player, response, {missionId: "dailyPotionsStock"});
+			}
+			return true;
+		}
+	};
+}
+
+function getBuySlotExtensionShopItemCallback(playerId: number, price: number): EndCallback {
+	return async (collector, response): Promise<void> => {
+		const player = await Players.getById(playerId);
+		const reaction = collector.getFirstReaction();
+		if (!reaction || reaction.reaction.type === ReactionCollectorBuyCategorySlotCancelReaction.name) {
+			BlockingUtils.unblockPlayer(player.id, BlockingConstants.REASONS.SHOP);
+			response.push(makePacket(CommandShopClosed, {}));
+			return;
+		}
+
+		const invInfo = await InventoryInfos.getOfPlayer(player.id);
+		const category = (reaction.reaction.data as ReactionCollectorBuyCategorySlotReaction).categoryId;
+
+		await player.spendMoney({
+			amount: price,
+			response,
+			reason: NumberChangeReason.SHOP
+		});
+		await player.save();
+		invInfo.addSlotForCategory(category);
+		await invInfo.save();
+		response.push(makePacket(ReactionCollectorBuyCategorySlotBuySuccess, {}));
+
+		BlockingUtils.unblockPlayer(player.id, BlockingConstants.REASONS.SHOP);
+		draftBotInstance.logsDatabase.logClassicalShopBuyout(player.keycloakId, ShopItemType.SLOT_EXTENSION).then();
+	};
+}
+
+/**
+ * Get the shop item for extending your inventory
+ * @param player
+ */
+async function getSlotExtensionShopItem(player: Player): Promise<ShopItem | null> {
+	const invInfo = await InventoryInfos.getOfPlayer(player.id);
+	const availableCategories = [0, 1, 2, 3]
+		.filter(itemCategory => invInfo.slotLimitForCategory(itemCategory) < ItemConstants.SLOTS.LIMITS[itemCategory]);
+	if (availableCategories.length === 0) {
+		return null;
+	}
+	const totalSlots = invInfo.weaponSlots + invInfo.armorSlots
+		+ invInfo.potionSlots + invInfo.objectSlots;
+	const price = ItemConstants.SLOTS.PRICES[totalSlots - 4];
+	if (!price) {
+		return null;
+	}
+	return {
+		id: "inventoryExtension",
+		price,
+		amounts: [1],
+		buyCallback: (context, response): Promise<boolean> => {
+			const collector = new ReactionCollectorBuyCategorySlot(availableCategories, price);
+
+			const packet = new ReactionCollectorInstance(
+				collector,
+				context,
+				{
+					allowedPlayerKeycloakIds: [player.keycloakId]
+				},
+				getBuySlotExtensionShopItemCallback(player.id, price)
+			)
+				.block(player.id, BlockingConstants.REASONS.SHOP)
+				.build();
+
+			response.push(packet);
+
+			return Promise.resolve(false);
+		}
+	};
+}
+
+export default class ShopCommand {
+	@packetHandler(CommandShopPacketReq)
+	static async execute(
+		client: WebsocketClient,
+		packet: CommandShopPacketReq,
+		context: PacketContext,
+		response: DraftBotPacket[]
+	): Promise<void> {
+		const player = await Players.getByKeycloakId(context.keycloakId);
+
+		if (BlockingUtils.appendBlockedPacket(player, response)) {
+			return;
+		}
+
+		const healEnergyAlreadyPurchased = await LogsReadRequests.getAmountOfHealEnergyBoughtByPlayerThisWeek(player.keycloakId);
+		const potion = PotionDataController.instance.getById(await Settings.SHOP_POTION.getValue());
+
+		const shopCategories: ShopCategory[] = [{
+			id: "permanentItem",
+			items: [
+				getRandomItemShopItem(),
+				getHealAlterationShopItem(player),
+				getHealEnergyShopItem(healEnergyAlreadyPurchased),
+				getRegenShopItem(),
+				getBadgeShopItem()
+			]
+		}, {
+			id: "dailyPotion",
+			items: [getDailyPotionShopItem(potion)]
+		}];
+
+		const slotExtensionItem = await getSlotExtensionShopItem(player);
+		if (slotExtensionItem) {
+			shopCategories.push({
+				id: "inventory",
+				items: []
+			});
+		}
+
+		ShopUtils.sendShopCollector(new ReactionCollectorShop(
+			shopCategories,
+			player.money,
+			ShopConstants.MAX_DAILY_POTION_BUYOUTS - await LogsReadRequests.getAmountOfDailyPotionsBoughtByPlayer(player.keycloakId),
+			toItemWithDetails(potion)
+		), shopCategories, context, response, player);
+	}
+}
---
+++ Lib/src/packets/interaction/ReactionCollectorShop.ts
@@ -0,0 +1,133 @@
+import {
+	ReactionCollector,
+	ReactionCollectorCreationPacket, ReactionCollectorData,
+	ReactionCollectorReaction
+} from "./ReactionCollectorPacket";
+import {DraftBotPacket, PacketContext, PacketDirection, sendablePacket} from "../DraftBotPacket";
+import {ItemWithDetails} from "../../interfaces/ItemWithDetails";
+
+export interface ShopItem {
+	id: string;
+
+	price: number;
+
+	amounts: number[];
+
+	buyCallback: (context: PacketContext, response: DraftBotPacket[], playerId: number, amount: number) => Promise<boolean>;
+}
+
+export interface ShopCategory {
+	id: string;
+
+	items: ShopItem[];
+}
+
+@sendablePacket(PacketDirection.BACK_TO_FRONT)
+export class CommandShopClosed extends DraftBotPacket {
+}
+
+@sendablePacket(PacketDirection.BACK_TO_FRONT)
+export class CommandShopNoAlterationToHeal extends DraftBotPacket {
+}
+
+@sendablePacket(PacketDirection.BACK_TO_FRONT)
+export class CommandShopHealAlterationDone extends DraftBotPacket {
+}
+
+@sendablePacket(PacketDirection.BACK_TO_FRONT)
+export class CommandShopTooManyEnergyBought extends DraftBotPacket {
+}
+
+@sendablePacket(PacketDirection.BACK_TO_FRONT)
+export class CommandShopNoEnergyToHeal extends DraftBotPacket {
+}
+
+@sendablePacket(PacketDirection.BACK_TO_FRONT)
+export class CommandShopFullRegen extends DraftBotPacket {
+}
+
+@sendablePacket(PacketDirection.BACK_TO_FRONT)
+export class CommandShopAlreadyHaveBadge extends DraftBotPacket {
+}
+
+@sendablePacket(PacketDirection.BACK_TO_FRONT)
+export class CommandShopBadgeBought extends DraftBotPacket {
+}
+
+@sendablePacket(PacketDirection.BACK_TO_FRONT)
+export class CommandShopBoughtTooMuchDailyPotions extends DraftBotPacket {
+}
+
+@sendablePacket(PacketDirection.BACK_TO_FRONT)
+export class CommandShopNotEnoughMoney extends DraftBotPacket {
+	missingMoney!: number;
+}
+
+export class ReactionCollectorShopData extends ReactionCollectorData {
+	availableMoney!: number;
+
+	remainingPotions?: number;
+
+	dailyPotion?: ItemWithDetails;
+}
+
+export class ReactionCollectorShopItemReaction extends ReactionCollectorReaction {
+	shopCategoryId!: string;
+
+	shopItemId!: string;
+
+	price!: number;
+
+	amount!: number;
+}
+
+export class ReactionCollectorShopCloseReaction extends ReactionCollectorReaction {
+
+}
+
+export class ReactionCollectorShop extends ReactionCollector {
+	private readonly shopCategories!: ShopCategory[];
+
+	private readonly availableMoney!: number;
+
+	private readonly remainingPotions?: number;
+
+	private readonly dailyPotion?: ItemWithDetails;
+
+	constructor(shopCategories: ShopCategory[], availableMoney: number, remainingPotions: number | undefined, dailyPotion: ItemWithDetails | undefined) {
+		super();
+		this.shopCategories = shopCategories;
+		this.availableMoney = availableMoney;
+		this.remainingPotions = remainingPotions;
+		this.dailyPotion = dailyPotion;
+	}
+
+	creationPacket(id: string, endTime: number): ReactionCollectorCreationPacket {
+		const reactions = [];
+		for (const shopCategory of this.shopCategories) {
+			for (const shopItem of shopCategory.items) {
+				for (const amount of shopItem.amounts) {
+					reactions.push(this.buildReaction(ReactionCollectorShopItemReaction, {
+						shopCategoryId: shopCategory.id,
+						shopItemId: shopItem.id,
+						price: shopItem.price * amount,
+						amount: amount
+					}));
+				}
+			}
+		}
+
+		reactions.push(this.buildReaction(ReactionCollectorShopCloseReaction, {}));
+
+		return {
+			id,
+			endTime,
+			reactions,
+			data: this.buildData(ReactionCollectorShopData, {
+				availableMoney: this.availableMoney,
+				remainingPotions: this.remainingPotions,
+				dailyPotion: this.dailyPotion
+			})
+		};
+	}
+}
