29dfc3fe9d08c181a625d91a98032d8c869a7b03
Ntalcme <ntalcme@outlook.fr>
2024-10-26 23:11:56 +0200

    pet SmallEvent core #2318

--- Core/src/core/utils/FoodUtils.ts
+++ Core/src/core/utils/FoodUtils.ts
@@ -1,4 +1,10 @@
 import {Constants} from "../../../../Lib/src/constants/Constants";
+import {NumberChangeReason} from "../../../../Lib/src/constants/LogsConstants";
+import {Guilds} from "../database/game/models/Guild";
+import Player from "../database/game/models/Player";
+import {DraftBotPacket, makePacket} from "../../../../Lib/src/packets/DraftBotPacket";
+import {NoFoodSpaceInGuildPacket} from "../../../../Lib/src/packets/utils/NoFoodSpaceInGuildPacket";
+import {GiveFoodToGuildPacket} from "../../../../Lib/src/packets/utils/GiveFoodToGuildPacket";
 
 /**
  * Get the corresponding index in the constants of a given pet food
@@ -6,4 +12,16 @@ import {Constants} from "../../../../Lib/src/constants/Constants";
  */
 export function getFoodIndexOf(food: string): number {
 	return Constants.PET_FOOD_GUILD_SHOP.TYPE.indexOf(food);
+}
+
+export async function giveFoodToGuild(response: DraftBotPacket[],player:Player, selectedFood:string, quantity:number, reason:NumberChangeReason):Promise<void> {
+	const guild = await Guilds.getById(player.guildId);
+	const selectedFoodIndex = getFoodIndexOf(selectedFood);
+	if (guild.isStorageFullFor(selectedFood, quantity)) {
+		response.push(makePacket(NoFoodSpaceInGuildPacket,{}));
+		return;
+	}
+	guild.addFood(selectedFood, quantity, reason);
+	await guild.save();
+	response.push(makePacket(GiveFoodToGuildPacket,{quantity,selectedFoodIndex}));
 }
