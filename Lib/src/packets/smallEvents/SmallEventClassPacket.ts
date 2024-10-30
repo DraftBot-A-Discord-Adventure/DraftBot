2db8b0b73c9fb035fb769ad81fe0a690c5450380
Ntalcme <ntalcme@outlook.fr>
2024-10-30 01:16:16 +0100

    small fix #2318

--- Lib/src/packets/smallEvents/SmallEventClassPacket.ts
+++ Lib/src/packets/smallEvents/SmallEventClassPacket.ts
@@ -1,9 +1,10 @@
 import {PacketDirection, sendablePacket} from "../DraftBotPacket";
 import {SmallEventPacket} from "./SmallEventPacket";
+import {ClassKind} from "../../types/ClassKind";
 
 @sendablePacket(PacketDirection.BACK_TO_FRONT)
 export class SmallEventClassPacket extends SmallEventPacket {
-	classKind!: "basic" |"attack" | "defense" | "other";
+	classKind!: ClassKind;
 
 	interactionName!: string;
 
