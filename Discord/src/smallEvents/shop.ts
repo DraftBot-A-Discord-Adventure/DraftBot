627822c8aff3350eb750e1e36f30bc70c37ee08f
Ntalcme <ntalcme@outlook.fr>
2024-11-22 13:33:11 +0100

    WIP on front shop SE #2318

---
+++ Discord/src/smallEvents/shop.ts
@@ -0,0 +1,39 @@
+import {ReactionCollectorCreationPacket} from "../../../Lib/src/packets/interaction/ReactionCollectorPacket";
+import {PacketContext} from "../../../Lib/src/packets/DraftBotPacket";
+import {DiscordCache} from "../bot/DiscordCache";
+import {DraftbotSmallEventEmbed} from "../messages/DraftbotSmallEventEmbed";
+import {StringUtils} from "../utils/StringUtils";
+import {DiscordCollectorUtils} from "../utils/DiscordCollectorUtils";
+import {ReactionCollectorMerchantData} from "../../../Lib/src/packets/interaction/ReactionCollectorMerchant";
+import {RandomUtils} from "../../../Lib/src/utils/RandomUtils";
+import {KeycloakUtils} from "../../../Lib/src/keycloak/KeycloakUtils";
+import {keycloakConfig} from "../bot/DraftBotShard";
+import {SmallEventShopPacket} from "../../../Lib/src/packets/smallEvents/SmallEventShopPacket";
+
+export async function shopCollector(packet: ReactionCollectorCreationPacket, context: PacketContext): Promise<void> {
+	const interaction = DiscordCache.getInteraction(context.discord!.interaction)!;
+	const data = packet.data.data as ReactionCollectorMerchantData;
+	const gender = RandomUtils.draftbotRandom.bool() ? "m" : "f";
+	const name = StringUtils.getRandomTranslation("smallEvents:shop.names", interaction.userLanguage, {context: gender});
+
+	const embed = new DraftbotSmallEventEmbed(
+		"shop",
+		StringUtils.getRandomTranslation("smallEvents:shop.intro", interaction.userLanguage, {context: gender, name})
+		+ StringUtils.getRandomTranslation("smallEvents:shop.end", interaction.userLanguage, {
+			item: /* TODO: Renvoyer l'item avec ses stats etc */,
+			price: data.price,
+			type: data.item.category
+		}),
+		interaction.user,
+		interaction.userLanguage
+	);
+
+	await DiscordCollectorUtils.createAcceptRefuseCollector(interaction, embed, packet, context);
+}
+
+export async function shopResult(packet: SmallEventShopPacket, context: PacketContext): Promise<void> {
+	const user = (await KeycloakUtils.getUserByKeycloakId(keycloakConfig, context.keycloakId!))!;
+	const interaction = DiscordCache.getButtonInteraction(context.discord!.buttonInteraction!);
+
+	/* TODO : Renvoyer une réponse/Trouver les textes ?? */
+}
