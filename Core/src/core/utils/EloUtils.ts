7432a0c6ed717483824ecb19b2351edccc932fcf
BastLast <lastaccount@fastmail.com>
2025-01-16 23:19:22 +0100

    more fixes fight command WIP #2276

--- Core/src/core/utils/EloUtils.ts
+++ Core/src/core/utils/EloUtils.ts
@@ -35,7 +35,6 @@ export abstract class EloUtils {
 	 * @param kFactor The k factor of the player (see EloUtils.getKFactor)
 	 */
 	static calculateNewRating(playerRating: number, opponentRating: number, gameResult: EloGameResult, kFactor: number): number {
-		const oldElo = playerRating;
 		const newElo = Math.round(playerRating + kFactor * (gameResult - 1 / (1 + Math.pow(10, (opponentRating - playerRating) / 400))));
 		return newElo + Math.round((newElo - playerRating) * (1.49 - Math.tanh((playerRating - 502) / 140) / 2 - 0.87))
 	}
