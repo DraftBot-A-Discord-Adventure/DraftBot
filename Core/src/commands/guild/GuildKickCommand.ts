e804f3b336f873181fd9cdf92c6f247ff33f834d
Ntalcme <ntalcme@gmail.com>
2025-01-18 11:53:56 +0100

    add logs #2273

--- Core/src/commands/guild/GuildKickCommand.ts
+++ Core/src/commands/guild/GuildKickCommand.ts
@@ -28,6 +28,7 @@ async function acceptGuildKick(player: Player, kickedPlayer: Player, response: D
 	kickedPlayer.guildId = null;
 
 	if (guild.elderId === kickedPlayer.id) {
+		await draftBotInstance.logsDatabase.logGuildElderRemove(guild, guild.elderId);
 		guild.elderId = null;
 	}
 	await Promise.all([
