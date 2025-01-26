75edf6093d8c3daf5cd7fb944b31345a3e5278fc
BastLast <lastaccount@fastmail.com>
2025-01-26 19:07:49 +0100

    fightcommand intro first version #2276

--- Core/src/core/database/game/models/Player.ts
+++ Core/src/core/database/game/models/Player.ts
@@ -179,7 +179,7 @@ export class Player extends Model {
 	}
 
 	/**
-	 * Get the origin id of the playe²r
+	 * Get the origin id of the player
 	 */
 	public getPreviousMapId(): number {
 		const link = MapLinkDataController.instance.getById(this.mapLinkId);
@@ -262,6 +262,9 @@ export class Player extends Model {
 	 * Return the value of glory that is displayed to the users
 	 */
 	public getGloryPoints(): number {
+		console.log("Hello World");
+		console.log(this.attackGloryPoints);
+		console.log(this.defenseGloryPoints);
 		return this.attackGloryPoints + this.defenseGloryPoints;
 	}
 
@@ -826,6 +829,10 @@ export class Player extends Model {
 	 * Get the league of the player
 	 */
 	public getLeague(): League {
+		const banane = this.getGloryPoints();
+		console.log("this.getGloryPoints()", this.getGloryPoints());
+
+		console.log(banane);
 		return LeagueDataController.instance.getByGlory(this.getGloryPoints());
 	}
 
@@ -1451,7 +1458,7 @@ export class Players {
 						player.attackGloryPoints + FightConstants.ELO.MAX_ELO_GAP
 					]
 				},
-				level:  {[Op.gt]: FightConstants.REQUIRED_LEVEL}
+				level: {[Op.gt]: FightConstants.REQUIRED_LEVEL}
 			},
 			order: [
 				// Sort using the difference with the attack elo of the player
