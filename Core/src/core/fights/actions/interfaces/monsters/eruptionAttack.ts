43e3e7fe1bf485c4aa08e4b431c66ab39b76d0a8
BastLast <lastaccount@fastmail.com>
2025-01-30 23:24:07 +0100

    deleted weather mechanic

--- Core/src/core/fights/actions/interfaces/monsters/eruptionAttack.ts
+++ Core/src/core/fights/actions/interfaces/monsters/eruptionAttack.ts
@@ -1,9 +1,54 @@
-import {FightWeatherEnum} from "../../../FightWeather";
-import {defaultFightActionResult} from "../../../../../../../Lib/src/types/FightActionResult";
 import {FightActionFunc} from "../../../../../data/FightAction";
+import {simpleDamageFightAction} from "../../templates/SimpleDamageFightActionTemplate";
+import {attackInfo, FightActionController, statsInfo} from "../../FightActionController";
+import {FightAlterations} from "../../FightAlterations";
+import {Fighter} from "../../../fighter/Fighter";
 
-const use: FightActionFunc = (sender, _receiver, _fightAction, turn, fight) => {
-	fight.setWeather(FightWeatherEnum.FIRESTORM, turn, sender);
-	return defaultFightActionResult();
+const use: FightActionFunc = (sender, receiver, _fightAction) => {
+	const speedRatio = receiver.getSpeed() / sender.getSpeed();
+	const failureProbability = speedRatio < 3 ? 0 : 5 + Math.min(40, Math.pow(speedRatio - 3, 2) * 3);
+	const result = simpleDamageFightAction(
+		{
+			sender,
+			receiver
+		},
+		{
+			critical: 25,
+			failure: failureProbability
+		},
+		{
+			attackInfo: getAttackInfo(),
+			statsInfo: getStatsInfo(sender, receiver)
+		}
+	);
+
+	FightActionController.applyAlteration(result, {
+		selfTarget: false,
+		alteration: FightAlterations.BURNED
+	}, receiver);
+	return result;
 };
+
 export default use;
+
+function getAttackInfo(): attackInfo {
+	return {
+		minDamage: 50,
+		averageDamage: 90,
+		maxDamage: 170
+	};
+}
+
+function getStatsInfo(sender: Fighter, receiver: Fighter): statsInfo {
+	return {
+		attackerStats: [
+			sender.getAttack()
+		],
+		defenderStats: [
+			receiver.getSpeed()
+		],
+		statsEffect: [
+			1
+		]
+	};
+}
