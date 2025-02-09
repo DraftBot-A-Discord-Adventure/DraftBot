a7d111543402c175831a1b3b9454ccc80d428a9f
BastLast <lastaccount@fastmail.com>
2025-02-09 22:39:09 +0100

    Moved fight handlers to dedicated handler file #2276

--- Discord/src/packetHandlers/handlers/FightHandler.ts
+++ Discord/src/packetHandlers/handlers/FightHandler.ts
@@ -1,10 +1,35 @@
 import {packetHandler} from "../PacketHandler";
-import {FightIntroductionPacket} from "../../../../Lib/src/packets/fights/FightIntroductionPacket";
 import {PacketContext} from "../../../../Lib/src/packets/DraftBotPacket";
+import {
+	CommandFightIntroduceFightersPacket,
+	CommandFightOpponentsNotFoundPacket,
+	CommandFightRefusePacketRes, CommandFightStatusPacket
+} from "../../../../Lib/src/packets/commands/CommandFightPacket";
+import {
+	handleCommandFightIntroduceFightersRes,
+	handleCommandFightRefusePacketRes, handleCommandFightUpdateStatusRes
+} from "../../commands/player/FightCommand";
+import {handleClassicError} from "../../utils/ErrorUtils";
 
 export default class FightHandler {
-	@packetHandler(FightIntroductionPacket)
-	async fightIntroduction(_context: PacketContext, _packet: FightIntroductionPacket): Promise<void> {
-		// TODO
+	@packetHandler(CommandFightRefusePacketRes)
+	async refuseFight(context: PacketContext, _packet: CommandFightRefusePacketRes): Promise<void> {
+		await handleCommandFightRefusePacketRes(context);
 	}
+
+	@packetHandler(CommandFightOpponentsNotFoundPacket)
+	async opponentsNotFoundFight(context: PacketContext, _packet: CommandFightOpponentsNotFoundPacket): Promise<void> {
+		await handleClassicError(context, "commands:fight.opponentsNotFound");
+	}
+
+	@packetHandler(CommandFightIntroduceFightersPacket)
+	async introduceFighters(context: PacketContext, packet: CommandFightIntroduceFightersPacket): Promise<void> {
+		await handleCommandFightIntroduceFightersRes(packet, context);
+	}
+
+	@packetHandler(CommandFightStatusPacket)
+	async updateFightStatus(context: PacketContext, packet: CommandFightStatusPacket): Promise<void> {
+		await handleCommandFightUpdateStatusRes(packet, context);
+	}
+
 }
