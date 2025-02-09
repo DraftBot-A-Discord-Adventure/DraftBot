4b789248bec30d5f28e1d880990cfba703bbb0c8
BastLast <lastaccount@fastmail.com>
2025-02-09 17:14:54 +0100

    Wip first version of draftbotCachedMessage
    Co-authored-by: romain22222 <romrom22222@hotmail.fr>

---
+++ Discord/src/messages/DraftbotCachedMessage.ts
@@ -0,0 +1,72 @@
+import {Message} from "discord.js";
+import {DraftBotPacket, PacketContext} from "../../../Lib/src/packets/DraftBotPacket";
+import {minutesToMilliseconds} from "../../../Lib/src/utils/TimeUtils";
+import {DiscordCache} from "../bot/DiscordCache";
+import {OptionLike} from "./DraftbotInteraction";
+
+
+type CollectFunctionType = (packet: DraftBotPacket, context: PacketContext) => Promise<void>;
+
+export abstract class DraftbotCachedMessage {
+	// Function to call when you need to do something with the cached message
+	abstract collectFunction: CollectFunctionType;
+
+	// Duration of the message's cached life in minutes
+	abstract duration: number;
+
+	// Id of the original message
+	readonly originalMessageId: string;
+
+	// Message linked to this cached message
+	storedMessage?: Message;
+
+	constructor(originalMessageId: string) {
+		this.originalMessageId = originalMessageId;
+	}
+
+	async update(packet: DraftBotPacket, context: PacketContext): Promise<void> {
+		await this.collectFunction(packet, context);
+	}
+
+	async post(options: OptionLike): Promise<Message | null> {
+		if (this.storedMessage && this.storedMessage.editable) {
+			return await this.storedMessage.edit(options);
+		}
+		const mainMessage = DiscordCache.getInteraction(this.originalMessageId);
+		if (!mainMessage) {
+			return null;
+		}
+		const message = await mainMessage.channel.send(options) as Message;
+		this.storedMessage = message;
+		return message;
+	}
+}
+
+export class DraftbotCachedMessages {
+	static cachedMessages: Map<string, DraftbotCachedMessage> = new Map<string, DraftbotCachedMessage>();
+
+	static createCachedMessage(message: DraftbotCachedMessage): void {
+		DraftbotCachedMessages.cachedMessages.set(message.originalMessageId, message);
+		setTimeout(() => {
+			DraftbotCachedMessages.remove(message.originalMessageId);
+		}, minutesToMilliseconds(message.duration));
+	}
+
+	static remove(messageId: string): void {
+		DraftbotCachedMessages.cachedMessages.delete(messageId);
+	}
+
+	static get(messageId: string): DraftbotCachedMessage | undefined {
+		return DraftbotCachedMessages.cachedMessages.get(messageId);
+	}
+}
+
+// Je les mets ici pour le moment, mais faudra les déplacer
+export class DraftbotFightStatusCachedMessage extends DraftbotCachedMessage {
+	collectFunction = async (packet: NomDuPacketFightStatus, context: PacketContext) => {
+		// Actions à faire lors de la maj du message
+	};
+
+	duration = 30;
+}
+
