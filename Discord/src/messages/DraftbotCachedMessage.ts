5a66cfeb3849dd2c1a9b59e34aa7e8a60445fa01
romain22222 <romrom22222@hotmail.fr>
2025-02-12 22:56:18 +0100

    upgrade of DCMs

--- Discord/src/messages/DraftbotCachedMessage.ts
+++ Discord/src/messages/DraftbotCachedMessage.ts
@@ -1,50 +1,55 @@
-import {Message, MessageCreateOptions, MessageEditOptions, MessagePayload} from "discord.js";
+import {BaseMessageOptions, Message} from "discord.js";
 import {DraftBotPacket, PacketContext} from "../../../Lib/src/packets/DraftBotPacket";
 import {minutesToMilliseconds} from "../../../Lib/src/utils/TimeUtils";
 import {DiscordCache} from "../bot/DiscordCache";
-import {OptionLike} from "./DraftbotInteraction";
 
 export abstract class DraftbotCachedMessage<T extends DraftBotPacket = DraftBotPacket> {
-	// Function to call when you need to do something with the cached message
-	abstract updateMessage(packet: T, context: PacketContext): Promise<void>;
 
 	// Duration of the message's cached life in minutes
-	abstract duration: number;
+	abstract readonly duration: number;
 
 	// The id of the original message
 	readonly originalMessageId: string;
 
-	// The type of the message, each interaction can store one cached message per type
-	readonly type: string;
-
 	// Message linked to this cached message
 	storedMessage?: Message;
+
+	protected constructor(originalMessageId: string) {
+		this.originalMessageId = originalMessageId;
+	}
 
 	get cacheKey(): string {
 		return `${this.originalMessageId}-${this.type}`;
 	}
 
-	protected constructor(originalMessageId: string, type: string) {
-		this.originalMessageId = originalMessageId;
-		this.type = type;
-	}
+	// The type of the message, each interaction can store one cached message per type
+	abstract get type(): string;
+
+	// Function to call when you need to do something with the cached message
+	abstract updateMessage(packet: T, context: PacketContext): Promise<void>;
 
 	async update(packet: T, context: PacketContext): Promise<void> {
 		await this.updateMessage(packet, context);
 	}
 
-	async post(options: OptionLike): Promise<Message | null> {
+	async post(options: BaseMessageOptions): Promise<Message | null> {
 		if (this.storedMessage && this.storedMessage.editable) {
-			return await this.storedMessage.edit(options as string | MessageEditOptions | MessagePayload); // Todo: Check with romain for maybe better solution than casting here
+			return await this.storedMessage.edit(options);
 		}
 		const mainMessage = DiscordCache.getInteraction(this.originalMessageId);
 		if (!mainMessage) {
 			return null;
 		}
-		const message = await mainMessage.channel.send(options as string | MessageCreateOptions) as Message; // Todo: Check with romain for maybe better solution than casting here
+		const message = await mainMessage.channel.send(options) as Message;
 		this.storedMessage = message;
 		return message;
 	}
+}
+
+interface MessageLike<Message extends DraftbotCachedMessage> {
+	type: string;
+
+	new(originalMessageId: string): Message;
 }
 
 export class DraftbotCachedMessages {
@@ -61,7 +66,13 @@ export class DraftbotCachedMessages {
 		DraftbotCachedMessages.cachedMessages.delete(cacheKey);
 	}
 
-	static get(cacheKey: string): DraftbotCachedMessage | undefined {
-		return DraftbotCachedMessages.cachedMessages.get(cacheKey);
+	static getOrCreate<Packet extends DraftBotPacket, Message extends DraftbotCachedMessage<Packet>>(originalMessageId: string, MessageLike: MessageLike<Message>): Message {
+		const message = DraftbotCachedMessages.cachedMessages.get(`${originalMessageId}-${MessageLike.type}`);
+		if (!message) {
+			return new MessageLike(originalMessageId);
+		}
+		const instance = new MessageLike(message.originalMessageId);
+		Object.assign(instance, message);
+		return instance;
 	}
 }
