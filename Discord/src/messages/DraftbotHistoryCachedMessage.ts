93a7c1c606db514835e38fac3e8656c6973c276c
BastLast <lastaccount@fastmail.com>
2025-02-25 23:40:22 +0100

    alteration display of status #2276

--- Discord/src/messages/DraftbotHistoryCachedMessage.ts
+++ Discord/src/messages/DraftbotHistoryCachedMessage.ts
@@ -7,6 +7,9 @@ import i18n from "../translations/i18n";
 import {CommandFightHistoryItemPacket} from "../../../Lib/src/packets/fights/FightHistoryItemPacket";
 import {EmoteUtils} from "../utils/EmoteUtils";
 import {DraftBotIcons} from "../../../Lib/src/DraftBotIcons";
+import {FightAlterationState} from "../../../Lib/src/types/FightAlterationResult";
+import {FightActionStatus} from "../../../Lib/src/types/FightActionStatus";
+import {StringUtils} from "../utils/StringUtils";
 
 export class DraftbotHistoryCachedMessage extends DraftbotCachedMessage<CommandFightHistoryItemPacket> {
 	readonly duration = 30;
@@ -27,9 +30,60 @@ export class DraftbotHistoryCachedMessage extends DraftbotCachedMessage<CommandF
 			fighter: fighter
 		});
 
+		// First, display cases where the fightAction is an alteration
+		switch (packet.alterationStatus) {
+		case FightAlterationState.NEW:
+			newLine += i18n.t(`models:fight_actions.${packet.fightActionId}.new`, {
+				lng: interaction.userLanguage,
+				damage: packet.damageReceived
+			});
+			break;
+		case FightAlterationState.ACTIVE:
+			newLine += i18n.t(`models:fight_actions.${packet.fightActionId}.active`, {
+				lng: interaction.userLanguage,
+				damage: packet.damageReceived
+			});
+			break;
+		case FightAlterationState.NO_ACTION:
+			newLine += i18n.t(`models:fight_actions.${packet.fightActionId}.noAction`, {
+				lng: interaction.userLanguage
+			});
+			break;
+		case FightAlterationState.RANDOM_ACTION:
+			// Todo: implement this case
+			break;
+		case FightAlterationState.STOP:
+			newLine += i18n.t(`models:fight_actions.${packet.fightActionId}.stop`, {
+				lng: interaction.userLanguage,
+				damage: packet.damageReceived
+			});
+			break;
+		default:
+			// nothing to do, not an alteration
+			break;
+		}
+
+		// Second, display cases where the fightAction is an attack
+		let keyAttackResultsValue = "commands:fight.actions.attacksResults";
+		switch (packet.status) {
+		case FightActionStatus.CRITICAL:
+			keyAttackResultsValue += ".critical";
+			break;
+		case FightActionStatus.NORMAL:
+			keyAttackResultsValue += ".normal";
+			break;
+		case FightActionStatus.MISSED:
+			keyAttackResultsValue += ".missed";
+			break;
+		default:
+			break;
+		}
+		const attack =
+			newLine += StringUtils.getRandomTranslation(keyAttackResultsValue, interaction.userLanguage, {
+				attack:
+			});
 
 		const previousHistory = this.storedMessage?.content || "";
-
 		const history = `${previousHistory}\n${newLine}`;
 		await this.post({content: history});
 	};
