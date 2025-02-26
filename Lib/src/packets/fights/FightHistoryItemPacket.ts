53cce030ba1f093b9dbbd90650db30e6547531db
BastLast <lastaccount@fastmail.com>
2025-02-27 00:50:05 +0100

    Finished v1 of history display
    Co-authored-by: Ntalcme <ntalcme@gmail.com>

--- Lib/src/packets/fights/FightHistoryItemPacket.ts
+++ Lib/src/packets/fights/FightHistoryItemPacket.ts
@@ -8,24 +8,18 @@ export class CommandFightHistoryItemPacket extends DraftBotPacket {
 
 	fightActionId!: string;
 
-	// Contains the status of the alteration, is it new? is it removed?
-	alterationStatus?: string; // See constants in FightAlterationResult for values
+	status?: string; // See constants in FightAlterationResult or FightActionStatus for values
 
-	// Contains the result of the action, did it succeed? was it a critical hit?
-	status?: string; // See constants in FightActionStatus for values
-
-	damageDealt?: number; // Will be negative if we are healing the opponent
-
-	damageReceived?: number; // Will be negative if the player is healed
-
-	statsChangeDealt?: { // Stat change for the opponent in % (10 = 10%) can be negative
+	fightActionEffectDealt?: { // Stat change for the opponent in % (10 = 10%) can be negative
+		damages?: number;
 		defense?: number;
 		attack?: number;
 		speed?: number;
 		breath?: number; // This one is not in %
 	};
 
-	statsChangeReceived?: { // Stat change for the fighter in % (10 = 10%) can be negative
+	fightActionEffectReceived?: { // Stat change for the fighter in % (10 = 10%) can be negative
+		damages?: number;
 		defense?: number;
 		attack?: number;
 		speed?: number;
