eba571f8382b0742c64d86f6af67766e50e5ffcb
BastLast <lastaccount@fastmail.com>
2025-03-01 11:45:31 +0100

    fix resting display issues #2276

--- Lib/src/packets/fights/FightHistoryItemPacket.ts
+++ Lib/src/packets/fights/FightHistoryItemPacket.ts
@@ -8,6 +8,8 @@ export class CommandFightHistoryItemPacket extends DraftBotPacket {
 
 	fightActionId!: string;
 
+	customMessage?: boolean; // True if the fight action has an associated custom message instead of the default (only for attacks)
+
 	status?: string; // See constants in FightAlterationResult or FightActionStatus for values
 
 	fightActionEffectDealt?: { // Stat change for the opponent in % (10 = 10%) can be negative
