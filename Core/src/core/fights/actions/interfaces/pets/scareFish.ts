8e42ad2143861647895150d74297e639c6b08857
BastLast <lastaccount@fastmail.com>
2025-03-08 18:18:47 +0100

    pet assistance are now working correctly #2276

--- Core/src/core/fights/actions/interfaces/pets/scareFish.ts
+++ Core/src/core/fights/actions/interfaces/pets/scareFish.ts
@@ -1,8 +1,13 @@
 import {PetAssistanceResult, PetAssistanceState} from "../../../../../../../Lib/src/types/PetAssistanceResult";
 import {PetAssistanceFunc} from "../../../../../data/PetAssistance";
 
-const use: PetAssistanceFunc = (): PetAssistanceResult => ({
-	assistanceStatus: PetAssistanceState.GENERAL_EFFECT
-});
+const use: PetAssistanceFunc = (_sender, _receiver, _fightAction, turn): PetAssistanceResult | null => {
+	if (turn > 2) {
+		return null;
+	}
+	return {
+		assistanceStatus: PetAssistanceState.GENERAL_EFFECT
+	};
+};
 
 export default use;
