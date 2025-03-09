659385b4bd7ebff5229028b4bd313e0735896c53
BastLast <lastaccount@fastmail.com>
2025-03-09 19:34:22 +0100

    add more pet assists #2276 + small fixes

---
+++ Core/src/core/fights/actions/interfaces/pets/isUseless.ts
@@ -0,0 +1,14 @@
+import {PetAssistanceResult, PetAssistanceState} from "../../../../../../../Lib/src/types/PetAssistanceResult";
+import {PetAssistanceFunc} from "../../../../../data/PetAssistance";
+import {RandomUtils} from "../../../../../../../Lib/src/utils/RandomUtils";
+
+const use: PetAssistanceFunc = (_fighter, _opponent, _turn, _fightController): Promise<PetAssistanceResult | null> => {
+	if (RandomUtils.draftbotRandom.bool(0.9)) {
+		return null;
+	}
+	return Promise.resolve({
+		assistanceStatus: PetAssistanceState.GENERAL_EFFECT
+	});
+};
+
+export default use;
