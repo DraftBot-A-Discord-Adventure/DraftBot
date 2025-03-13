a63d0c88ce59575922a2817590497c8bdc584605
BastLast <lastaccount@fastmail.com>
2025-03-13 01:35:26 +0100

    add pet assist for 5 pets + fixes #2276

---
+++ Core/src/core/fights/actions/interfaces/pets/createBomb.ts
@@ -0,0 +1,49 @@
+import {Fighter} from "../../../fighter/Fighter";
+import {attackInfo, FightActionController, statsInfo} from "../../FightActionController";
+import {PetAssistanceFunc} from "../../../../../data/PetAssistance";
+import {PetAssistanceResult, PetAssistanceState} from "../../../../../../../Lib/src/types/PetAssistanceResult";
+
+function getAttackInfo(): attackInfo {
+	return {
+		minDamage: 50,
+		averageDamage: 130,
+		maxDamage: 180
+	};
+}
+
+function getStatsInfo(_sender: Fighter, receiver: Fighter): statsInfo {
+	return {
+		attackerStats: [
+			800
+		],
+		defenderStats: [
+			receiver.getDefense()
+		],
+		statsEffect: [
+			1
+		]
+	};
+}
+
+const use: PetAssistanceFunc = (fighter, opponent, turn, _fightController): Promise<PetAssistanceResult | null> => {
+	if (turn <= 2) {
+		return Promise.resolve({
+			assistanceStatus: PetAssistanceState.GENERAL_EFFECT
+		});
+	}
+	// Last turn of the fight, bomb explodes?
+	if (turn === 23 || turn === 24) {
+		if (fighter.getSpeed() < opponent.getSpeed()) {
+			return Promise.resolve({
+				assistanceStatus: PetAssistanceState.FAILURE
+			});
+		}
+		return Promise.resolve({
+			damages: FightActionController.getAttackDamage(getStatsInfo(fighter, opponent), fighter, getAttackInfo()),
+			assistanceStatus: PetAssistanceState.SUCCESS
+		});
+	}
+	return null;
+};
+
+export default use;
