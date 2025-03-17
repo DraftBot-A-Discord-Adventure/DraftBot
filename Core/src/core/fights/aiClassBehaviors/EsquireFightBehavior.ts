0ca12c7e14af6c209d8e54b96880d829c0d51c22
BastLast <lastaccount@fastmail.com>
2025-03-17 23:30:50 +0100

    fixes of big bugs with ai when two fights are happening at the same time #2276

--- Core/src/core/fights/aiClassBehaviors/EsquireFightBehavior.ts
+++ Core/src/core/fights/aiClassBehaviors/EsquireFightBehavior.ts
@@ -3,35 +3,42 @@ import { AiPlayerFighter } from "../fighter/AiPlayerFighter";
 import { FightView } from "../FightView";
 import { FightAction, FightActionDataController } from "../../../data/FightAction";
 import { FightConstants } from "../../../../../Lib/src/constants/FightConstants";
-import {PlayerFighter} from "../fighter/PlayerFighter";
-import {MonsterFighter} from "../fighter/MonsterFighter";
+import { PlayerFighter } from "../fighter/PlayerFighter";
+import { MonsterFighter } from "../fighter/MonsterFighter";
 
 class EsquireFightBehavior implements ClassBehavior {
-	private restCount = 0; // Track how many times we've rested
+	// WeakMap to store fighter-specific rest counts
+	private restCountMap = new WeakMap<AiPlayerFighter, number>();
 
 	chooseAction(me: AiPlayerFighter, fightView: FightView): FightAction {
 		const opponent = fightView.fightController.getDefendingFighter();
 		const currentRound = fightView.fightController.turn;
 
+		// Get fighter-specific rest count or initialize to 0 if not set
+		let restCount = this.restCountMap.get(me) || 0;
+
 		// Initialize defense tracking on first round
 		if (currentRound <= 1) {
-			this.restCount = 0; // Reset rest counter at the beginning of a fight
+			restCount = 0; // Reset rest counter at the beginning of a fight
+			this.restCountMap.set(me, restCount);
 		}
 
 		// ENDGAME STRATEGY: Try to force a draw if victory seems impossible
 		// Still rest even if we've done it 4 times, because the goal is to stall
 		if (me.getEnergy() < 75 && opponent.getEnergy() > 200) {
-			this.restCount++;
+			restCount++;
+			this.restCountMap.set(me, restCount);
 			return FightActionDataController.instance.getById(FightConstants.FIGHT_ACTIONS.PLAYER.RESTING);
 		}
 
 		// REST WHEN NEEDED: Not enough breath for actions (only if we haven't rested 4 times)
-		if (me.getBreath() < 2 && this.restCount < 4) {
-			this.restCount++;
+		if (me.getBreath() < 2 && restCount < 4) {
+			restCount++;
+			this.restCountMap.set(me, restCount);
 			return FightActionDataController.instance.getById(FightConstants.FIGHT_ACTIONS.PLAYER.RESTING);
 		}
 
-		return simpleOrQuickAttack(me, opponent);
+		return simpleOrQuickAttack(me, opponent, restCount);
 	}
 }
 
@@ -39,15 +46,20 @@ class EsquireFightBehavior implements ClassBehavior {
  * Choose between a simple or quick attack based on speed comparison and breath
  * @param me
  * @param opponent
+ * @param restCount Optional rest count for decision-making
  */
-export function simpleOrQuickAttack(me: AiPlayerFighter, opponent: PlayerFighter | MonsterFighter | AiPlayerFighter): FightAction {
+export function simpleOrQuickAttack(
+	me: AiPlayerFighter,
+	opponent: PlayerFighter | MonsterFighter | AiPlayerFighter,
+	restCount?: number
+): FightAction {
 	// Other attacks based on speed comparison
 	const mySpeed = me.getSpeed();
 	const opponentSpeed = opponent.getSpeed();
 
 	// If my speed is greater than the opponent's speed, use a quick attack otherwise, use a simple attack
 	// If we're very low on breath but have already rested 4 times, still try to attack
-	if (mySpeed > opponentSpeed && (me.getBreath() >= 3 || this.restCount >= 4)) {
+	if (mySpeed > opponentSpeed && (me.getBreath() >= 3 || restCount !== undefined && restCount >= 4)) {
 		return FightActionDataController.instance.getById(FightConstants.FIGHT_ACTIONS.PLAYER.QUICK_ATTACK);
 	}
 	return FightActionDataController.instance.getById(FightConstants.FIGHT_ACTIONS.PLAYER.SIMPLE_ATTACK);
