fe06424b3f811abac74fd5a2f4f8f8a797435528
BastLast <lastaccount@fastmail.com>
2025-03-17 23:30:50 +0100

    fixes of big bugs with ai when two fights are happening at the same time #2276

--- Core/src/core/fights/aiClassBehaviors/GunnerFightBehavior.ts
+++ Core/src/core/fights/aiClassBehaviors/GunnerFightBehavior.ts
@@ -1,37 +1,43 @@
-import { ClassBehavior } from "../AiBehaviorManager";
-import { AiPlayerFighter } from "../fighter/AiPlayerFighter";
-import { FightView } from "../FightView";
-import { FightAction, FightActionDataController } from "../../../data/FightAction";
-import { FightConstants } from "../../../../../Lib/src/constants/FightConstants";
+import {ClassBehavior} from "../AiBehaviorManager";
+import {AiPlayerFighter} from "../fighter/AiPlayerFighter";
+import {FightView} from "../FightView";
+import {FightAction, FightActionDataController} from "../../../data/FightAction";
+import {FightConstants} from "../../../../../Lib/src/constants/FightConstants";
 
 class GunnerFightBehavior implements ClassBehavior {
+	// WeakMaps to store fighter-specific state
+	private isGoingForChainedCanonAttackMap = new WeakMap<AiPlayerFighter, boolean>();
 
-	private isGoingForChainedCanonAttack = false;
-
-	private canonAttackUsed = 0;
+	private canonAttackUsedMap = new WeakMap<AiPlayerFighter, number>();
 
 	chooseAction(me: AiPlayerFighter, fightView: FightView): FightAction {
 		const opponent = fightView.fightController.getDefendingFighter();
 
+		// Get a fighter-specific state or initialize if not set
+		let isGoingForChainedCanonAttack = this.isGoingForChainedCanonAttackMap.get(me) || false;
+		let canonAttackUsed = this.canonAttackUsedMap.get(me) || 0;
+
 		// Use canon attack again if used last turn to get 1.5x damage
 		if (
 			fightView.fightController.turn > 2
-			&& this.isGoingForChainedCanonAttack
+			&& isGoingForChainedCanonAttack
 			&& me.getBreath() >= FightActionDataController.getFightActionBreathCost(FightConstants.FIGHT_ACTIONS.PLAYER.CANON_ATTACK)
-			&& this.canonAttackUsed <= 2
+			&& canonAttackUsed <= 2
 		) {
-			this.canonAttackUsed++;
+			canonAttackUsed++;
+			this.canonAttackUsedMap.set(me, canonAttackUsed);
 			return FightActionDataController.instance.getById(FightConstants.FIGHT_ACTIONS.PLAYER.CANON_ATTACK);
 		}
 
-		//clear the chained canon attack flag if 3 canon attacks have been used (or 2 and not enough breath to continue)
+		// Clear the chained canon attack flag if 3 canon attacks have been used (or 2 and not enough breath to continue)
 		if (
-			this.isGoingForChainedCanonAttack
-			&& (this.canonAttackUsed >= 3
+			isGoingForChainedCanonAttack
+			&& (canonAttackUsed >= 3
 				|| me.getBreath() < FightActionDataController.getFightActionBreathCost(FightConstants.FIGHT_ACTIONS.PLAYER.CANON_ATTACK)
-				&& this.canonAttackUsed === 2)
+				&& canonAttackUsed === 2)
 		) {
-			this.isGoingForChainedCanonAttack = false;
+			isGoingForChainedCanonAttack = false;
+			this.isGoingForChainedCanonAttackMap.set(me, false);
 		}
 
 		// If opponent is very low health, finish them with any attack
@@ -39,15 +45,14 @@ class GunnerFightBehavior implements ClassBehavior {
 			// Quick Attack is good for finishing off enemies
 			if (me.getBreath() >= FightActionDataController.getFightActionBreathCost(FightConstants.FIGHT_ACTIONS.PLAYER.QUICK_ATTACK)) {
 				return FightActionDataController.instance.getById(FightConstants.FIGHT_ACTIONS.PLAYER.QUICK_ATTACK);
-			} else {
-				return FightActionDataController.instance.getById(FightConstants.FIGHT_ACTIONS.PLAYER.SABOTAGE_ATTACK);
 			}
+			return FightActionDataController.instance.getById(FightConstants.FIGHT_ACTIONS.PLAYER.SABOTAGE_ATTACK);
 		}
 
-		// Play boomerang when possible if opponent has no alteration
+		// Play boomerang when possible if the opponent has no alteration
 		if (
 			!opponent.hasFightAlteration()
-			&& !this.isGoingForChainedCanonAttack
+			&& !isGoingForChainedCanonAttack
 			&& me.getBreath() >= FightActionDataController.getFightActionBreathCost(FightConstants.FIGHT_ACTIONS.PLAYER.BOOMERANG_ATTACK)
 		) {
 			return FightActionDataController.instance.getById(FightConstants.FIGHT_ACTIONS.PLAYER.BOOMERANG_ATTACK);
@@ -55,23 +60,26 @@ class GunnerFightBehavior implements ClassBehavior {
 
 		// After a boomerang, decide to focus on canon attack strategy we have enough breath, use canon attack
 		if (
-			!this.isGoingForChainedCanonAttack
-			&& this.canonAttackUsed === 0
+			!isGoingForChainedCanonAttack
+			&& canonAttackUsed === 0
 			&& opponent.getEnergy() > 400
 			&& opponent.hasFightAlteration()
 			&& me.getBreath() >= FightActionDataController.getFightActionBreathCost(FightConstants.FIGHT_ACTIONS.PLAYER.CANON_ATTACK)
 		) {
-			this.isGoingForChainedCanonAttack = true;
-			// we need to have enough breath to use canon attack twice in a raw at minimum
+			isGoingForChainedCanonAttack = true;
+			this.isGoingForChainedCanonAttackMap.set(me, true);
+
+			// We need to have enough breath to use canon attack twice in a raw at minimum
 			if (me.getBreath() >= FightActionDataController.getFightActionBreathCost(FightConstants.FIGHT_ACTIONS.PLAYER.CANON_ATTACK) + 2) {
-				this.canonAttackUsed++;
+				canonAttackUsed++;
+				this.canonAttackUsedMap.set(me, canonAttackUsed);
 				return FightActionDataController.instance.getById(FightConstants.FIGHT_ACTIONS.PLAYER.CANON_ATTACK);
 			}
 		}
 
-		// If opponent has more speed or close to it and we have enough breath, use intense attack
+		// If the opponent has higher speed or close to it, and we have enough breath, use intense attack
 		if (
-			!this.isGoingForChainedCanonAttack
+			!isGoingForChainedCanonAttack
 			&& opponent.getSpeed() > me.getSpeed() * 0.8
 			&& opponent.getEnergy() > 200
 			&& me.getBreath() >= FightActionDataController.getFightActionBreathCost(FightConstants.FIGHT_ACTIONS.PLAYER.INTENSE_ATTACK)
@@ -79,7 +87,7 @@ class GunnerFightBehavior implements ClassBehavior {
 			return FightActionDataController.instance.getById(FightConstants.FIGHT_ACTIONS.PLAYER.INTENSE_ATTACK);
 		}
 
-		// If opponent has more speed or close to it and we don't have breath for intense, use sabotage attack
+		// If the opponent has higher speed or close to it, and we don't have breath for intense, use sabotage attack
 		if (
 			opponent.getSpeed() > me.getSpeed() * 0.8
 			&& me.getBreath() >= FightActionDataController.getFightActionBreathCost(FightConstants.FIGHT_ACTIONS.PLAYER.SABOTAGE_ATTACK)
--- Core/src/core/fights/aiClassBehaviors/InfantrymanFightBehavior.ts
+++ Core/src/core/fights/aiClassBehaviors/InfantrymanFightBehavior.ts
@@ -1,47 +1,49 @@
-import { ClassBehavior } from "../AiBehaviorManager";
-import { AiPlayerFighter } from "../fighter/AiPlayerFighter";
-import { FightView } from "../FightView";
-import { FightAction, FightActionDataController } from "../../../data/FightAction";
-import { FightConstants } from "../../../../../Lib/src/constants/FightConstants";
-import { PlayerFighter } from "../fighter/PlayerFighter";
-import { ClassConstants } from "../../../../../Lib/src/constants/ClassConstants";
-import { RandomUtils } from "../../../../../Lib/src/utils/RandomUtils";
+import {ClassBehavior} from "../AiBehaviorManager";
+import {AiPlayerFighter} from "../fighter/AiPlayerFighter";
+import {FightView} from "../FightView";
+import {FightAction, FightActionDataController} from "../../../data/FightAction";
+import {FightConstants} from "../../../../../Lib/src/constants/FightConstants";
+import {PlayerFighter} from "../fighter/PlayerFighter";
+import {ClassConstants} from "../../../../../Lib/src/constants/ClassConstants";
+import {RandomUtils} from "../../../../../Lib/src/utils/RandomUtils";
 
 class InfantryManFightBehavior implements ClassBehavior {
 
-	private powerfulAttacksUsed = 0;
+	private powerfulAttacksUsedMap = new WeakMap<AiPlayerFighter, number>();
 
 	chooseAction(me: AiPlayerFighter, fightView: FightView): FightAction {
+
+		const powerfulAttacksUsed = this.powerfulAttacksUsedMap.get(me) || 0;
 		const opponent = fightView.fightController.getDefendingFighter() as PlayerFighter | AiPlayerFighter; // AI will never fight monsters
 
-		// If opponent is a mage, or a gunner with a lot of breath and we are not protected, use protection
+		// If the opponent is a mage, or a gunner with a lot of breath, and we are not protected, use protection
 		if (
 			(opponent.player.class === ClassConstants.CLASSES_ID.MYSTIC_MAGE
-				|| (
-					opponent.player.class === ClassConstants.CLASSES_ID.GUNNER
-					&& opponent.getBreath() > 4
-				))
+				||
+				opponent.player.class === ClassConstants.CLASSES_ID.GUNNER
+				&& opponent.getBreath() > 4
+			)
 			&& !me.hasFightAlteration()
-			&& opponent.getEnergy() > 100 // don't use this if the opponent is about to die
+			&& opponent.getEnergy() > 100 // Don't use this if the opponent is about to die
 		) {
 			return FightActionDataController.instance.getById(FightConstants.FIGHT_ACTIONS.PLAYER.PROTECTION);
 		}
 
-		// priority is to use powerful attacks
+		// Priority is to use powerful attacks
 		if (
-			this.powerfulAttacksUsed <= 2
-			&& RandomUtils.draftbotRandom.bool(0.9) // add a bit of randomness here to avoid being too predictable
+			powerfulAttacksUsed <= 2
+			&& RandomUtils.draftbotRandom.bool(0.9) // Add a bit of randomness here to avoid being too predictable
 			&& me.getBreath() >= FightActionDataController.getFightActionBreathCost(FightConstants.FIGHT_ACTIONS.PLAYER.POWERFUL_ATTACK)
 		) {
-			this.powerfulAttacksUsed++;
+			this.powerfulAttacksUsedMap.set(me, powerfulAttacksUsed + 1);
 			return FightActionDataController.instance.getById(FightConstants.FIGHT_ACTIONS.PLAYER.POWERFUL_ATTACK);
 		}
 
 		// Use charging attack if the opponent is not a knight and we have enough breath
 		if (
-			(fightView.fightController.turn > 11 // except if we are late fight
-				|| this.powerfulAttacksUsed > 2) // priority to the powerful attacks
-			&& me.getEnergy() > 300 // don't use it if we are about to die
+			(fightView.fightController.turn > 11 // Except if we are a late fight
+				|| powerfulAttacksUsed > 2) // Priority to the powerful attacks
+			&& me.getEnergy() > 300 // Don't use it if we are about to die
 			&& (opponent.player.class !== ClassConstants.CLASSES_ID.MYSTIC_MAGE
 				|| me.hasFightAlteration()
 			)
@@ -57,7 +59,7 @@ class InfantryManFightBehavior implements ClassBehavior {
 			(opponent.getDefense() > me.getDefense()
 				|| RandomUtils.draftbotRandom.bool(0.2))
 			&& (opponent.getSpeed() < me.getSpeed() * 1.2
-				|| this.powerfulAttacksUsed > 2)  // don't use it if the opponent is way faster to save breath for powerful
+				|| powerfulAttacksUsed > 2) // Don't use it if the opponent is way faster to save breath for powerful
 			&& me.getBreath() >= FightActionDataController.getFightActionBreathCost(FightConstants.FIGHT_ACTIONS.PLAYER.PIERCING_ATTACK)
 		) {
 			return FightActionDataController.instance.getById(FightConstants.FIGHT_ACTIONS.PLAYER.PIERCING_ATTACK);
