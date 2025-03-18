6943e9583646f8bf0a67fcd35214ed7f62cf2ead
Feiryn <98115666+feiryn@users.noreply.github.com>
2025-03-19 00:26:21 +0100

    Add doc to getCurrentInteraction function

--- Discord/src/utils/MessagesUtils.ts
+++ Discord/src/utils/MessagesUtils.ts
@@ -3,6 +3,13 @@ import {DiscordCache} from "../bot/DiscordCache";
 import {PacketContext} from "../../../Lib/src/packets/DraftBotPacket";
 
 export abstract class MessagesUtils {
+	/**
+	 * Get the current interaction from the context and cast it to DraftbotInteraction.
+	 * It selects either the string select menu interaction, the button interaction or the interaction depending on which one is present in the context.
+	 * The order of priority is string select menu interaction > button interaction > interaction.
+	 * The interaction is retrieved from the DiscordCache.
+	 * @param context
+	 */
 	static getCurrentInteraction(context: PacketContext): DraftbotInteraction {
 		if (context.discord!.stringSelectMenuInteraction) {
 			const stringSelectMenuInteraction = DiscordCache.getStringSelectMenuInteraction(context.discord!.stringSelectMenuInteraction);
