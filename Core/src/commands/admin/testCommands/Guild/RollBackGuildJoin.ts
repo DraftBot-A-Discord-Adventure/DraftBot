ed22286215553c39d2cf0bc11c0917b3f126d50c
ntalcme <ntalcme@gmail.com>
2025-03-21 20:53:46 +0100

    squel on RollBackGuildJoin.ts #2750

---
+++ Core/src/commands/admin/testCommands/Guild/RollBackGuildJoin.ts
@@ -0,0 +1,7 @@
+import {ITestCommand} from "../../../../core/CommandsTest";
+
+export const commandInfo: ITestCommand = {
+	name: "rollbackguildjoin",
+	aliases: ["rgj"],
+	description: "Recule d'une semaine notre date d'adhésion à une guilde."
+};
