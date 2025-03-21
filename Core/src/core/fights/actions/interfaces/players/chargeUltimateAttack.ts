3281c334106634470d73f9264b05fd6c9b36a89e
BastLast <lastaccount@fastmail.com>
2025-03-21 01:51:33 +0100

    small balancing changes#2276

--- Core/src/core/fights/actions/interfaces/players/chargeUltimateAttack.ts
+++ Core/src/core/fights/actions/interfaces/players/chargeUltimateAttack.ts
@@ -1,8 +1,9 @@
 import {FightActionDataController, FightActionFunc} from "../../../../../data/FightAction";
 import {fightActionResultFromSuccessTest} from "../../../../../../../Lib/src/types/FightActionResult";
+import {FightConstants} from "../../../../../../../Lib/src/constants/FightConstants";
 
 const use: FightActionFunc = (sender) => {
-	const usedUltimateAttacks = sender.fightActionsHistory.filter(action => action === this).length;
+	const usedUltimateAttacks = sender.fightActionsHistory.filter(action => action.id === FightConstants.FIGHT_ACTIONS.PLAYER.ULTIMATE_ATTACK).length;
 	if (usedUltimateAttacks === 0) {
 		// Set the next fight action of the sender to be the ultimate attack
 		sender.nextFightAction = FightActionDataController.instance.getById("ultimateAttack");
