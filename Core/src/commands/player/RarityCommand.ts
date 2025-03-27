b1a7000f2d0f7e8e52373fe7536260e7170f4e14
romain22222 <romrom22222@hotmail.fr>
2025-03-27 20:29:37 +0100

    refactor command report

--- Core/src/commands/player/RarityCommand.ts
+++ Core/src/commands/player/RarityCommand.ts
@@ -12,14 +12,17 @@ export default class RarityCommand {
 		const maxValue = ItemConstants.RARITY.GENERATOR.MAX_VALUE;
 		const raritiesGenerator = ItemConstants.RARITY.GENERATOR.VALUES;
 		response.push(makePacket(CommandRarityPacketRes, {
-			common: raritiesGenerator[0] * 100 / maxValue,
-			uncommon: (raritiesGenerator[1] - raritiesGenerator[0]) * 100 / maxValue,
-			exotic: (raritiesGenerator[2] - raritiesGenerator[1]) * 100 / maxValue,
-			rare: (raritiesGenerator[3] - raritiesGenerator[2]) * 100 / maxValue,
-			special: (raritiesGenerator[4] - raritiesGenerator[3]) * 100 / maxValue,
-			epic: (raritiesGenerator[5] - raritiesGenerator[4]) * 100 / maxValue,
-			legendary: (raritiesGenerator[6] - raritiesGenerator[5]) * 100 / maxValue,
-			unique: (maxValue - raritiesGenerator[6]) * 100 / maxValue
+			rarities: [
+				0,
+				raritiesGenerator[0] * 100 / maxValue,
+				(raritiesGenerator[1] - raritiesGenerator[0]) * 100 / maxValue,
+				(raritiesGenerator[2] - raritiesGenerator[1]) * 100 / maxValue,
+				(raritiesGenerator[3] - raritiesGenerator[2]) * 100 / maxValue,
+				(raritiesGenerator[4] - raritiesGenerator[3]) * 100 / maxValue,
+				(raritiesGenerator[5] - raritiesGenerator[4]) * 100 / maxValue,
+				(raritiesGenerator[6] - raritiesGenerator[5]) * 100 / maxValue,
+				(maxValue - raritiesGenerator[6]) * 100 / maxValue
+			]
 		}));
 	}
 }
