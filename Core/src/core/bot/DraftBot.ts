b1404538640c12d0042843f388fdfc8b73ede5f4
Feiryn <98115666+feiryn@users.noreply.github.com>
2025-04-01 17:14:57 +0200

    Fix deepsource

--- Core/src/core/bot/DraftBot.ts
+++ Core/src/core/bot/DraftBot.ts
@@ -223,10 +223,10 @@ export class DraftBot {
 		await Player.update(
 			{
 				defenseGloryPoints: Sequelize.literal(
-					"defenseGloryPoints + LEAST(" + FightConstants.ATTACK_GLORY_TO_DEFENSE_GLORY_EACH_WEEK + ", attackGloryPoints)"
+					`defenseGloryPoints + LEAST(${FightConstants.ATTACK_GLORY_TO_DEFENSE_GLORY_EACH_WEEK}, attackGloryPoints)`
 				),
 				attackGloryPoints: Sequelize.literal(
-					"attackGloryPoints - LEAST(" + FightConstants.ATTACK_GLORY_TO_DEFENSE_GLORY_EACH_WEEK + ", attackGloryPoints)"
+					`attackGloryPoints - LEAST(${FightConstants.ATTACK_GLORY_TO_DEFENSE_GLORY_EACH_WEEK}, attackGloryPoints)`
 				)
 			},
 			{where: {attackGloryPoints: {[Op.gt]: 0}}}
