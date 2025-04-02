d8f03d490a92f948843dc8d3c5a724ac47de4fd3
Feiryn <98115666+feiryn@users.noreply.github.com>
2025-04-03 00:10:05 +0200

    More eslint fixes

--- Discord/eslint.config.mjs
+++ Discord/eslint.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 
-const {defineConfig, globalIgnores} = require("eslint/config");
-const typescriptEslintPlugin = require("@typescript-eslint/eslint-plugin");
-const typescriptEslintParser = require("@typescript-eslint/parser");
-const jsdoc = require("eslint-plugin-jsdoc");
+import {defineConfig, globalIgnores} from "eslint/config";
+import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
+import typescriptEslintParser from "@typescript-eslint/parser";
+import jsdoc from "eslint-plugin-jsdoc";
 import stylistic from "@stylistic/eslint-plugin";
 import customRules from "../eslint-rules.mjs";
 
-module.exports = defineConfig([
+export default defineConfig([
 	{
 		languageOptions: {
 			ecmaVersion: 2022,
