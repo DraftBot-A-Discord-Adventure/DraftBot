3071ff01281b81ab6be152ac5ba95caf88f2bbdc
bastlast <lastaccount@fastmail.com>
2025-04-06 00:15:10 +0200

    eslint fix #2813

--- Core/src/core/utils/PetUtils.ts
+++ Core/src/core/utils/PetUtils.ts
@@ -2,10 +2,10 @@ import { PetConstants } from "../../../../Lib/src/constants/PetConstants";
 
 export abstract class PetUtils {
 	/**
- * Get age context depending on the id of the pet
- * @param age - the id of the pet
- * @returns a string context that can be used to get more precise translations
- */
+	 * Get age context depending on the id of the pet
+	 * @param age - the id of the pet
+	 * @returns a string context that can be used to get more precise translations
+	 */
 	static getAgeCategory(age: number): string {
 		return age <= PetConstants.PET_AGE_GROUPS_THRESHOLDS.ANCESTOR
 			? PetConstants.PET_AGE_GROUP_NAMES.ANCESTOR
