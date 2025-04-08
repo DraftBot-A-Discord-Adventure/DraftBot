cf350a66582a02e8bc843f280900dc8ea30854c9
BastLast <lastaccount@fastmail.com>
2025-04-09 00:45:45 +0200

    fix mult fight action #2896

--- Core/src/core/fights/actions/interfaces/alterations/concentrated.ts
+++ Core/src/core/fights/actions/interfaces/alterations/concentrated.ts
@@ -8,24 +8,17 @@ import {
 } from "../../../FightController";
 
 const use: FightAlterationFunc = (affected, fightAlteration) => {
-	if (affected.alterationTurn > 2) { // This effect heals after two turns
-		affected.removeAttackModifiers(fightAlteration);
-		affected.removeSpeedModifiers(fightAlteration);
+	if (affected.alterationTurn > 1) { // This effect only lasts one turn
 		return defaultHealFightAlterationResult(affected);
 	}
 	const result = defaultFightAlterationResult();
 	if (!affected.hasAttackModifier(fightAlteration)) {
 		FightActionController.applyBuff(result, {
 			selfTarget: true,
-			stat: FightStatBuffed.SPEED,
+			stat: FightStatBuffed.DAMAGE_BOOST,
 			operator: FightStatModifierOperation.MULTIPLIER,
-			value: 2
-		}, affected, fightAlteration);
-		FightActionController.applyBuff(result, {
-			selfTarget: true,
-			stat: FightStatBuffed.ATTACK,
-			operator: FightStatModifierOperation.MULTIPLIER,
-			value: 2
+			duration: 1,
+			value: 1.6
 		}, affected, fightAlteration);
 		result.state = FightAlterationState.NEW;
 	}
