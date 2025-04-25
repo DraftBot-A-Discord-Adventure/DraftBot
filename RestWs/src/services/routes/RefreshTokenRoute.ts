7aedfa40cd76ee632085c0924527af1b65efffc0
Feiryn <98115666+feiryn@users.noreply.github.com>
2025-04-25 18:08:12 +0200

    Base of Rest API / Websocket middleware #2791

---
+++ RestWs/src/services/routes/RefreshTokenRoute.ts
@@ -0,0 +1,49 @@
+import { FastifyInstance } from "fastify";
+import { DraftBotLogger } from "../../../../Lib/src/logs/DraftBotLogger";
+import { keycloakConfig } from "../../index";
+import { KeycloakUtils } from "../../../../Lib/src/keycloak/KeycloakUtils";
+import { getRequestLoggerMetadata } from "../RestApi";
+
+export function setupRefreshTokenRoute(server: FastifyInstance): void {
+	server.post("/refresh-token", async (request, reply) => {
+		try {
+			DraftBotLogger.debug("Refresh token request received", {
+				...getRequestLoggerMetadata(request)
+			});
+
+			// URI naming convention
+			/* eslint-disable-next-line camelcase */
+			const { refresh_token } = request.body as { refresh_token: string };
+
+			// Check if the refresh token is provided
+			/* eslint-disable-next-line camelcase */
+			if (!refresh_token) {
+				reply.status(400).send({ error: "Token is required" });
+			}
+
+			// Try to refresh the token
+			const resRefresh = await KeycloakUtils.refreshUserToken(keycloakConfig, refresh_token);
+
+			// Check if the refresh was successful
+			if (resRefresh.isError) {
+				// As it can be caused by a user trying to refresh a token manually, we log it as a warning instead of an error
+				DraftBotLogger.warn("Failed to refresh token", {
+					apiReturn: resRefresh,
+					...getRequestLoggerMetadata(request)
+				});
+				reply.status(resRefresh.status).send(resRefresh.payload);
+				return;
+			}
+
+			// Send the new token to the user
+			reply.send(resRefresh.payload);
+		}
+		catch (error) {
+			DraftBotLogger.errorWithObj("Error during refresh token", {
+				error,
+				...getRequestLoggerMetadata(request)
+			});
+			reply.status(500).send({ error: "Internal server error" });
+		}
+	});
+}
