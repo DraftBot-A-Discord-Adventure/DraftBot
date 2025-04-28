b84e00fdfbf6b037b730cd907a9492851eb099bb
romain22222 <romrom22222@hotmail.fr>
2025-04-28 15:25:15 +0200

    fix #3209

--- Discord/src/commands/player/SwitchCommand.ts
+++ Discord/src/commands/player/SwitchCommand.ts
@@ -1,30 +1,20 @@
-import { ICommand } from "../ICommand";
-import {
-	makePacket, PacketContext
-} from "../../../../Lib/src/packets/DraftBotPacket";
-import { SlashCommandBuilderGenerator } from "../SlashCommandBuilderGenerator";
-import { DiscordCache } from "../../bot/DiscordCache";
+import {ICommand} from "../ICommand";
+import {makePacket, PacketContext} from "../../../../Lib/src/packets/DraftBotPacket";
+import {SlashCommandBuilderGenerator} from "../SlashCommandBuilderGenerator";
+import {DiscordCache} from "../../bot/DiscordCache";
 import i18n from "../../translations/i18n";
-import { DraftbotInteraction } from "../../messages/DraftbotInteraction";
-import {
-	CommandSwitchPacketReq, CommandSwitchSuccess
-} from "../../../../Lib/src/packets/commands/CommandSwitchPacket";
-import { DraftBotEmbed } from "../../messages/DraftBotEmbed";
-import { ReactionCollectorCreationPacket } from "../../../../Lib/src/packets/interaction/ReactionCollectorPacket";
-import { DiscordCollectorUtils } from "../../utils/DiscordCollectorUtils";
-import {
-	ReactionCollectorSwitchItemCloseReaction,
-	ReactionCollectorSwitchItemReaction
-} from "../../../../Lib/src/packets/interaction/ReactionCollectorSwitchItem";
-import { DiscordItemUtils } from "../../utils/DiscordItemUtils";
-import {
-	MainItemDisplayPacket,
-	SupportItemDisplayPacket
-} from "../../../../Lib/src/packets/commands/CommandInventoryPacket";
-import { Language } from "../../../../Lib/src/Language";
-import { EmbedField } from "discord.js";
-import { ReactionCollectorReturnTypeOrNull } from "../../packetHandlers/handlers/ReactionCollectorHandlers";
-import { escapeUsername } from "../../utils/StringUtils";
+import {DraftbotInteraction} from "../../messages/DraftbotInteraction";
+import {CommandSwitchPacketReq, CommandSwitchSuccess} from "../../../../Lib/src/packets/commands/CommandSwitchPacket";
+import {DraftBotEmbed} from "../../messages/DraftBotEmbed";
+import {ReactionCollectorCreationPacket} from "../../../../Lib/src/packets/interaction/ReactionCollectorPacket";
+import {DiscordCollectorUtils, SEND_POLITICS} from "../../utils/DiscordCollectorUtils";
+import {ReactionCollectorSwitchItemCloseReaction, ReactionCollectorSwitchItemReaction} from "../../../../Lib/src/packets/interaction/ReactionCollectorSwitchItem";
+import {DiscordItemUtils} from "../../utils/DiscordItemUtils";
+import {MainItemDisplayPacket, SupportItemDisplayPacket} from "../../../../Lib/src/packets/commands/CommandInventoryPacket";
+import {Language} from "../../../../Lib/src/Language";
+import {EmbedField} from "discord.js";
+import {ReactionCollectorReturnTypeOrNull} from "../../packetHandlers/handlers/ReactionCollectorHandlers";
+import {escapeUsername} from "../../utils/StringUtils";
 
 /**
  * Get the switch command packet
@@ -69,14 +59,14 @@ export async function handleItemSwitch(packet: CommandSwitchSuccess, context: Pa
  */
 function getFielder(itemCategory: number): ((displayPacket: MainItemDisplayPacket, lng: Language) => EmbedField) | ((displayPacket: SupportItemDisplayPacket, lng: Language) => EmbedField) {
 	switch (itemCategory) {
-		case 0:
-			return DiscordItemUtils.getWeaponField;
-		case 1:
-			return DiscordItemUtils.getArmorField;
-		case 2:
-			return DiscordItemUtils.getPotionField;
-		default:
-			return DiscordItemUtils.getObjectField;
+	case 0:
+		return DiscordItemUtils.getWeaponField;
+	case 1:
+		return DiscordItemUtils.getArmorField;
+	case 2:
+		return DiscordItemUtils.getPotionField;
+	default:
+		return DiscordItemUtils.getObjectField;
 	}
 }
 
@@ -91,21 +81,23 @@ export async function switchItemCollector(context: PacketContext, packet: Reacti
 			lng,
 			pseudo: escapeUsername(interaction.user.displayName)
 		}), interaction.user)
-		.setDescription(`${i18n.t("commands:switch.switchSelectionDescription", { lng })}\n\n`);
+		.setDescription(`${i18n.t("commands:switch.switchSelectionDescription", {lng})}\n\n`);
 	const reactions: ReactionCollectorSwitchItemReaction[] = packet.reactions
 		.map(reaction => reaction.data as ReactionCollectorSwitchItemReaction)
 		.filter(reaction => reaction.item);
-	return await DiscordCollectorUtils.createChoiceListCollector(
-		interaction,
-		embed,
+	return await DiscordCollectorUtils.createChoiceListCollector(interaction, {
 		packet,
-		context,
-		reactions.map(reaction => getFielder(reaction.item.itemCategory)(reaction.item as MainItemDisplayPacket & SupportItemDisplayPacket, lng).value),
-		{
+		context
+	}, {
+		embed,
+		items: reactions.map(reaction => getFielder(reaction.item.itemCategory)(reaction.item as MainItemDisplayPacket & SupportItemDisplayPacket, lng).value)
+	}, {
+		refuse: {
 			can: true,
 			reactionIndex: packet.reactions.findIndex(reaction => reaction.type === ReactionCollectorSwitchItemCloseReaction.name)
-		}
-	);
+		},
+		sendManners: SEND_POLITICS.EDIT_REPLY_OR_FOLLOWUP
+	});
 }
 
 export const commandInfo: ICommand = {
