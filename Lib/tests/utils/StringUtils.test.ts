f5b583cc8a43090327c958c88dcadce5b28ebc63
BastLast <lastaccount@fastmail.com>
2025-04-28 22:01:12 +0200

    review comments

--- Lib/tests/utils/StringUtils.test.ts
+++ Lib/tests/utils/StringUtils.test.ts
@@ -1,5 +1,4 @@
 import { describe, it, expect } from 'vitest';
-import { StringConstants } from "../../src/constants/StringConstants";
 import { progressBar } from "../../src/utils/StringUtils";
 
 describe('progressBar', () => {
