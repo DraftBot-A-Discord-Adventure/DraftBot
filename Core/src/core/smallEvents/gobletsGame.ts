136f3d1bc795e84945c8fac1d420d94f6ef833bc
BastLast <lastaccount@fastmail.com>
2025-05-05 00:31:21 +0200

    Refactor advice message for clarity and improve array formatting in gobletsGame

--- Core/src/core/smallEvents/gobletsGame.ts
+++ Core/src/core/smallEvents/gobletsGame.ts
@@ -78,7 +78,8 @@ export const smallEventFuncs: SmallEventFuncs = {
 		const origin = player.getPreviousMap();
 		return Maps.isOnContinent(player)
 			&& ![destination.id, origin.id].some(mapId =>
-				[MapConstants.LOCATIONS_IDS.ROAD_OF_WONDERS,
+				[
+					MapConstants.LOCATIONS_IDS.ROAD_OF_WONDERS,
 					MapConstants.LOCATIONS_IDS.MARSHY_ROAD,
 					MapConstants.LOCATIONS_IDS.MOUNT_CELESTRUM
 				].includes(mapId));
