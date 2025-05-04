be2b888ea95e557a06aca848056e83b4d151c3c6
BastLast <lastaccount@fastmail.com>
2025-05-05 00:33:35 +0200

    fix #3253 unicorn power crashing fight
    fix rainbowPower logic to handle null last fight actions correctly
    
    fix rainbowPower logic to correctly handle undefined last fight actions

--- Core/src/core/fights/actions/interfaces/pets/rainbowPower.ts
+++ Core/src/core/fights/actions/interfaces/pets/rainbowPower.ts
@@ -14,7 +14,8 @@ const use: PetAssistanceFunc = (fighter, opponent, turn, _fightController): Prom
 		return null;
 	}
 
-	if (opponent.getLastFightActionUsed().type !== FightActionType.MAGIC && fighter.getLastFightActionUsed().type !== FightActionType.MAGIC) {
+	if ((!opponent.getLastFightActionUsed() || opponent.getLastFightActionUsed()?.type !== FightActionType.MAGIC)
+		&& (!fighter.getLastFightActionUsed() || fighter.getLastFightActionUsed()?.type !== FightActionType.MAGIC)) {
 		return null;
 	}
 
