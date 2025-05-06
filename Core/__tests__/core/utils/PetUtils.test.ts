d022dbcde3fd1649cdc259c113c3cd8b7962a2c9
Electek <154330555+Electek@users.noreply.github.com>
2025-05-06 11:37:44 +0200

    premier test PetUtils

---
+++ Core/__tests__/core/utils/PetUtils.test.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest';
+import {PetUtils} from "../../../src/core/utils/PetUtils";
+
+describe('PetUtils', () => {
+	describe('getAgeCategory', () => {
+		it('should get correct category for correct age', () => {
+			expect(PetUtils.getAgeCategory(1)).toBe("ANCESTOR");
+		});
+	});
+});
