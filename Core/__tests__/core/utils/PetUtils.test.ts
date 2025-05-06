999dff4e0aa37312e87f4ef1e7e89591ab857644
Electek <154330555+Electek@users.noreply.github.com>
2025-05-06 11:47:36 +0200

    add more test for PetUtils

--- Core/__tests__/core/utils/PetUtils.test.ts
+++ Core/__tests__/core/utils/PetUtils.test.ts
@@ -5,6 +5,10 @@ describe('PetUtils', () => {
 	describe('getAgeCategory', () => {
 		it('should get correct category for correct age', () => {
 			expect(PetUtils.getAgeCategory(1)).toBe("ancestor");
+			expect(PetUtils.getAgeCategory(500)).toBe("veryOld");
+			expect(PetUtils.getAgeCategory(4000)).toBe("old");
+			expect(PetUtils.getAgeCategory(10000)).toBe("adult");
+			expect(PetUtils.getAgeCategory(30000)).toBe("other");
 		});
 	});
 });
