01048d2641cd203cf2a69838e43940d1cc0aa2bd
Electek <154330555+Electek@users.noreply.github.com>
2025-05-07 12:22:39 +0200

    correction Food Utils test

--- Core/__tests__/core/utils/Foodutils.test.ts
+++ Core/__tests__/core/utils/Foodutils.test.ts
@@ -4,10 +4,10 @@ import {FoodUtils} from "../../../src/core/utils/FoodUtils";
 describe('FoodUtils', () => {
 	describe('getFoodIndexOf', () => {
 		it('should correctly compute the interval value for a given percentage', () => {
-			expect(FoodUtils.getFoodIndexOf(commonFood)).toBe(0);
-			expect(FoodUtils.getFoodIndexOf(carnivorousFood)).toBe(1);
-			expect(FoodUtils.getFoodIndexOf(herbivorousFood)).toBe(2);
-			expect(FoodUtils.getFoodIndexOf(ultimateFood)).toBe(3);
+			expect(FoodUtils.getFoodIndexOf("commonFood")).toBe(0);
+			expect(FoodUtils.getFoodIndexOf("carnivorousFood")).toBe(1);
+			expect(FoodUtils.getFoodIndexOf("herbivorousFood")).toBe(2);
+			expect(FoodUtils.getFoodIndexOf("ultimateFood")).toBe(3);
 		});
 	});
 });
