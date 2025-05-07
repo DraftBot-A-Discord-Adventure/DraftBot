3be9d352f95ec6a31d468b8056cfa940e6147aa2
Electek <154330555+Electek@users.noreply.github.com>
2025-05-07 11:45:45 +0200

    Guild Utils test

---
+++ Core/__tests__/core/utils/GuildUtils.test.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest';
+import {GuildUtils} from "../../../src/core/utils/GuildUtils";
+
+describe('GuildUtils', () => {
+	describe('calculateAmountOfXPToAdd', () => {
+		it('should return XP within expected range for a given cost', () => {
+			expect(GuildUtils.calculateAmountOfXPToAdd(1000)).GreaterThanOrEqual(50);
+			expect(GuildUtils.calculateAmountOfXPToAdd(1000)).toBeLessThanOrEqual(450);
+		});
+	});
+});
