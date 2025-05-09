7d3779f4c0daa6e645353cf7544837096eb43e58
Electek <154330555+Electek@users.noreply.github.com>
2025-05-09 12:27:54 +0200

    ajout de la fonction calculateNewRating dans Elo Utils test #numérodelissue

--- Core/__tests__/core/utils/EloUtils.test.ts
+++ Core/__tests__/core/utils/EloUtils.test.ts
@@ -12,5 +12,12 @@ describe('MathUtils', () => {
 		expect(EloUtils.getKFactor(mockPlayer2)).toBe(24);
 		expect(EloUtils.getKFactor(mockPlayer3)).toBe(16);
         });
+        
+    describe('calculateNewRating', () => {
+        it('should return the correct K factor', () => {
+		expect(EloUtils.calculateNewRating(1500, 1500, 1, 32)).toBe(1536);
+		expect(EloUtils.calculateNewRating(2200, 2200, 1, 24)).toBe(2213);
+		expect(EloUtils.calculateNewRating(2500, 2500, 1, 16)).toBe(2509);
+        });
     });
 });
