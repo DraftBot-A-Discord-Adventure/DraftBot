6bf9e96ac994866b48e7c87f59d0dc9922b0fd49
Electek <154330555+Electek@users.noreply.github.com>
2025-05-09 11:38:38 +0200

    encore plus de correction sur Food Utils test

--- Core/__tests__/core/utils/FoodUtils.test.ts
+++ Core/__tests__/core/utils/FoodUtils.test.ts
@@ -4,10 +4,10 @@ import {getFoodIndexOf } from "../../../src/core/utils/FoodUtils";
 describe('FoodUtils', () => {
 	describe('getFoodIndexOf', () => {
 		it('should correctly index the type of food', () => {
-			expect(getFoodIndexOf("commonFood")).toBe(1);
-			expect(getFoodIndexOf("carnivorousFood")).toBe(2);
-			expect(getFoodIndexOf("herbivorousFood")).toBe(3);
-			expect(getFoodIndexOf("ultimateFood")).toBe(4);
+			expect(getFoodIndexOf("commonFood")).toBe(0);
+			expect(getFoodIndexOf("carnivorousFood")).toBe(1);
+			expect(getFoodIndexOf("herbivorousFood")).toBe(2);
+			expect(getFoodIndexOf("ultimateFood")).toBe(3);
 		});
 	});
 });
