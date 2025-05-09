1e4c95e3d874d85bca4de3e4dd2bda6eede36520
Electek <154330555+Electek@users.noreply.github.com>
2025-05-09 12:33:29 +0200

    correction des parrenthese Elo Utils #3285

--- Core/__tests__/core/utils/EloUtils.test.ts
+++ Core/__tests__/core/utils/EloUtils.test.ts
@@ -1,23 +1,24 @@
 import { describe, it, expect } from 'vitest';
-import {EloUtils} from "../../../src/core/utils/EloUtils";
+import { EloUtils } from "../../../src/core/utils/EloUtils";
 
 describe('MathUtils', () => {
     describe('getKFactor', () => {
         it('should return the correct K factor', () => {
-		const mockPlayer1 = { getGloryPoints: () => 1500 } as any;
-		const mockPlayer2 = { getGloryPoints: () => 2200 } as any;
-		const mockPlayer3 = { getGloryPoints: () => 2500 } as any;
+            const mockPlayer1 = { getGloryPoints: () => 1500 } as any;
+            const mockPlayer2 = { getGloryPoints: () => 2200 } as any;
+            const mockPlayer3 = { getGloryPoints: () => 2500 } as any;
 
-		expect(EloUtils.getKFactor(mockPlayer1)).toBe(32);
-		expect(EloUtils.getKFactor(mockPlayer2)).toBe(24);
-		expect(EloUtils.getKFactor(mockPlayer3)).toBe(16);
+            expect(EloUtils.getKFactor(mockPlayer1)).toBe(32);
+            expect(EloUtils.getKFactor(mockPlayer2)).toBe(24);
+            expect(EloUtils.getKFactor(mockPlayer3)).toBe(16);
         });
-        
+    });
+
     describe('calculateNewRating', () => {
         it('should return the correct K factor', () => {
-		expect(EloUtils.calculateNewRating(1500, 1500, 1, 32)).toBe(1536);
-		expect(EloUtils.calculateNewRating(2200, 2200, 1, 24)).toBe(2213);
-		expect(EloUtils.calculateNewRating(2500, 2500, 1, 16)).toBe(2509);
+            expect(EloUtils.calculateNewRating(1500, 1500, 1, 32)).toBe(1536);
+            expect(EloUtils.calculateNewRating(2200, 2200, 1, 24)).toBe(2213);
+            expect(EloUtils.calculateNewRating(2500, 2500, 1, 16)).toBe(2509);
         });
     });
 });
