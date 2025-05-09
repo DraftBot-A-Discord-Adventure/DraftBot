4003e8eb190bd53e82897b2d4096d8ff233cf633
Electek <154330555+Electek@users.noreply.github.com>
2025-05-09 12:04:40 +0200

    deuxieme Elo Utils test

--- Core/__tests__/core/utils/EloUtils.test.ts
+++ Core/__tests__/core/utils/EloUtils.test.ts
@@ -4,9 +4,13 @@ import {EloUtils} from "../../../src/core/utils/EloUtils";
 describe('MathUtils', () => {
     describe('getKFactor', () => {
         it('should return the correct K factor', () => {
-            expect(EloUtils.getKFactor(1000)).toBe(32);
-            expect(EloUtils.getKFactor(2200)).toBe(24);
-            expect(EloUtils.getKFactor(2500)).toBe(16);
+		const mockPlayer1 = { getGloryPoints: () => 1500 } as any;
+		const mockPlayer2 = { getGloryPoints: () => 2200 } as any;
+		const mockPlayer3 = { getGloryPoints: () => 2500 } as any;
+
+		expect(EloUtils.getKFactor(mockPlayer1)).toBe(32);
+		expect(EloUtils.getKFactor(mockPlayer2)).toBe(24);
+		expect(EloUtils.getKFactor(mockPlayer3)).toBe(16);
         });
     });
 });
