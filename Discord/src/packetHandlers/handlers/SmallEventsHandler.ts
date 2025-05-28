940d1570fbddc03d53e0e9b3fe02aee9cf41a24c
ntalcme <ntalcme@gmail.com>
2025-05-28 15:21:48 +0200

    create function getRandomSmallEventEnd and add it to se dwarf #3054

--- Discord/src/packetHandlers/handlers/SmallEventsHandler.ts
+++ Discord/src/packetHandlers/handlers/SmallEventsHandler.ts
@@ -96,6 +96,10 @@ export function getRandomSmallEventIntro(language: Language): string {
 	return StringUtils.getRandomTranslation("smallEvents:intro", language);
 }
 
+export function getRandomSmallEventEnd(language: Language): string {
+	return StringUtils.getRandomTranslation("smallEvents:end", language);
+}
+
 export default class SmallEventsHandler {
 	@packetHandler(SmallEventAdvanceTimePacket)
 	async smallEventAdvanceTime(context: PacketContext, packet: SmallEventAdvanceTimePacket): Promise<void> {
@@ -991,12 +995,14 @@ export default class SmallEventsHandler {
 					"dwarfPetFan",
 					StringUtils.getRandomTranslation("smallEvents:dwarfPetFan.intro", lng)
 					+ StringUtils.getRandomTranslation(`smallEvents:dwarfPetFan.${keyStory}`, lng, {
+						context: packet.playerHavePet ? packet.petSex : "m",
 						pet: packet.playerHavePet && !packet.arePetsAllSeen ? PetUtils.petToShortString(lng, packet.petNickname, packet.petTypeId!, packet.petSex!) : "",
 						reward: i18n.t(`smallEvents:dwarfPetFan.reward.${keyReward}`, {
 							lng,
 							amount: packet.amount
 						})
-					}),
+					})
+					+ getRandomSmallEventEnd(lng),
 					interaction.user,
 					lng
 				)
