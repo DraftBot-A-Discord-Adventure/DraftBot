faa32d66ff75792095999aa1ef3c9107e3072799
BastLast <lastaccount@fastmail.com>
2025-05-29 18:52:27 +0200

    Add call pack and pack attack implementations with associated data
    #3392

---
+++ Core/src/core/fights/actions/interfaces/monsters/packAttack.ts
@@ -0,0 +1,46 @@
+import {
+	FightActionDataController, FightActionFunc
+} from "../../../../../data/FightAction";
+import { Fighter } from "../../../fighter/Fighter";
+import { simpleDamageFightAction } from "../../templates/SimpleDamageFightActionTemplate";
+import {
+	attackInfo, statsInfo
+} from "../../FightActionController";
+import { RandomUtils } from "../../../../../../../Lib/src/utils/RandomUtils";
+
+const use: FightActionFunc = (sender, receiver) => {
+	const nbOfMonsters = RandomUtils.draftbotRandom.integer(2, 5);
+	const result = simpleDamageFightAction(
+		{
+			sender, receiver
+		},
+		{
+			critical: 5, failure: 0
+		},
+		{
+			attackInfo: getAttackInfo(nbOfMonsters), statsInfo: getStatsInfo(sender, receiver, nbOfMonsters)
+		}
+	);
+	if (nbOfMonsters <= 3) {
+		sender.nextFightAction = FightActionDataController.instance.getById("packAttack");
+	}
+	return result;
+};
+
+function getAttackInfo(nbOfMonsters: number): attackInfo {
+	return {
+		minDamage: 5 * nbOfMonsters,
+		averageDamage: 30 * nbOfMonsters,
+		maxDamage: 45 * nbOfMonsters
+	};
+}
+
+function getStatsInfo(sender: Fighter, receiver: Fighter, nbOfMonsters: number): statsInfo {
+	return {
+		attackerStats: [sender.getAttack() * nbOfMonsters],
+		defenderStats: [receiver.getDefense() / nbOfMonsters],
+		statsEffect: [1]
+	};
+}
+
+export default use;
