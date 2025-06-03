6627e9c5957313c6055c0f817c091c9236482a50
BastLast <lastaccount@fastmail.com>
2025-06-04 01:12:24 +0200

    final version sentences mini event #3359

--- Core/src/core/fights/actions/interfaces/monsters/chargeClubSmashAttack.ts
+++ Core/src/core/fights/actions/interfaces/monsters/chargeClubSmashAttack.ts
@@ -3,10 +3,11 @@ import {
 	FightActionDataController, FightActionFunc
 } from "../../../../../data/FightAction";
 import { defaultFightActionResult } from "../../../../../../../Lib/src/types/FightActionResult";
+import { FightConstants } from "../../../../../../../Lib/src/constants/FightConstants";
 
 const use: FightActionFunc = (sender: Fighter) => {
 	// Set the next fight action of the sender to be the club smash attack
-	sender.nextFightAction = FightActionDataController.instance.getById("clubSmashAttack");
+	sender.nextFightAction = FightActionDataController.instance.getById(FightConstants.FIGHT_ACTIONS.MONSTER.CLUB_SMASH_ATTACK);
 	return defaultFightActionResult();
 };
 
