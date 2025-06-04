59aa15db1e8a8bd40e211476d83aa5fe5b54eebb
BastLast <lastaccount@fastmail.com>
2025-06-04 11:26:12 +0200

    fix review comments #3360

--- Discord/src/smallEvents/infoFight.ts
+++ Discord/src/smallEvents/infoFight.ts
@@ -2,14 +2,12 @@ import { PacketContext } from "../../../Lib/src/packets/DraftBotPacket";
 import { DiscordCache } from "../bot/DiscordCache";
 import { DraftbotSmallEventEmbed } from "../messages/DraftbotSmallEventEmbed";
 import { StringUtils } from "../utils/StringUtils";
-import { RandomUtils } from "../../../Lib/src/utils/RandomUtils";
 
 export async function infoFightResult(context: PacketContext): Promise<void> {
 	const interaction = DiscordCache.getInteraction(context.discord!.interaction);
 	const lng = interaction!.userLanguage;
 	const intro = StringUtils.getRandomTranslation("smallEvents:infoFight.intro", lng);
-	const typeOfActionExplained = RandomUtils.draftbotRandom.pick(["fightActions"]);
-	const description = StringUtils.getRandomTranslation(`smallEvents:infoFight.${typeOfActionExplained}`, lng);
+	const description = StringUtils.getRandomTranslation(`smallEvents:infoFight.fightActions`, lng);
 	await interaction?.editReply({
 		embeds: [
 			new DraftbotSmallEventEmbed(
